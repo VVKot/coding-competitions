add6adcb4d6c9e8f4bc59e750b852b918a8ca9af
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-09 15:53:03 +0200

    feat: Improve 122. Faster than 100% of solutions

--- problems/122-best-time-to-by-and-sell-stock-ii.py
+++ problems/122-best-time-to-by-and-sell-stock-ii.py
@@ -1,18 +1,7 @@
 class Solution(object):
     def maxProfit(self, prices):
-        if not prices:
-            return 0
         max_profit = 0
-        min_price = prices[0]
-        max_price = prices[0]
-        for i, price in enumerate(prices):
-            # if price dropped or we reached the end of the period
-            if price < max_price or i == len(prices) - 1:
-                # call max to account for end of the period case
-                max_profit += max(max_price, price) - min_price
-                # we want to start over as history is no longer relevant at this point - we sold stock
-                max_price = min_price = price
-                continue
-            min_price = min(min_price, price)
-            max_price = max(max_price, price)
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                max_profit += prices[i] - prices[i-1]
         return max_profit
