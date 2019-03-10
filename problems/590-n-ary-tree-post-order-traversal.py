784b055e96db2bee2aad8107ac9c76b9acce2de7
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-10 09:02:11 +0200

    feat: Add iterative solution for 590

--- problems/590-n-ary-tree-post-order-traversal.py
+++ problems/590-n-ary-tree-post-order-traversal.py
@@ -7,10 +7,23 @@ class Node(object):
 """
 class Solution(object):
     def postorder(self, root):
+        # recursive
+        # result = []
+        # if not root:
+        #     return result
+        # for child in root.children:
+        #     result += self.postorder(child)
+        # result.append(root.val)
+        # return result
+
+        # iterative
         result = []
         if not root:
             return result
-        for child in root.children:
-            result += self.postorder(child)
-        result.append(root.val)
-        return result
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            result.append(curr.val)
+            stack.extend(curr.children)
+
+        return result[::-1]
