b078dae5b9dae6b2d4381587d8efa44b9b91aa26
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-13 21:45:21 +0200

    feat: Make solution for 695 iterative

--- problems/695-max-area-of-island.py
+++ problems/695-max-area-of-island.py
@@ -1,23 +1,21 @@
 class Solution:
-    def get_area(self, grid, x, y):
+    def maxAreaOfIsland(self, grid):
         grid_y = len(grid)
         grid_x = len(grid[0])
         result = 0
-        if x < 0 or x == grid_x or y < 0 or y == grid_y or grid[y][x] == 0:
-            return result
-        grid[y][x] = 0
-        result += self.get_area(grid, x+1, y)
-        result += self.get_area(grid, x-1, y)
-        result += self.get_area(grid, x, y+1)
-        result += self.get_area(grid, x, y-1)
-        result += 1
-        return result
 
-    def maxAreaOfIsland(self, grid):
-        max_island = 0
-        for y, row in enumerate(grid):
-            for x, val in enumerate(row):
-                if val == 1:
-                    curr_island = self.get_area(grid, x, y)
-                    max_island = max(max_island, curr_island)
-        return max_island
+        for y in range(grid_y):
+            for x in range(grid_x):
+                if grid[y][x] == 1:
+                    grid[y][x] = 0
+                    adj_nodes = [(y, x)]
+
+                    for r, c in adj_nodes:
+                        for node_y, node_x in (r+1, c), (r-1, c), (r, c+1), (r, c-1):
+                            if 0 <= node_y < grid_y and 0 <= node_x < grid_x and grid[node_y][node_x] == 1:
+                                grid[node_y][node_x] = 0
+                                adj_nodes.append((node_y, node_x))
+
+                    result = max(result, len(adj_nodes))
+
+        return result
