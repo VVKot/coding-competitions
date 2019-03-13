650989df3d9d81c32d9d27c039b5ef18729cbcea
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-13 20:43:15 +0200

    feat: Make solution for 462 a 2-liner

--- problems/462-minimal-moves-to-equal-array-elements-ii.py
+++ problems/462-minimal-moves-to-equal-array-elements-ii.py
@@ -1,10 +1,4 @@
 class Solution:
     def minMoves2(self, nums):
-        result = 0
-        if not nums:
-            return result
         nums = sorted(nums)
-        mid = len(nums) // 2
-        for n in nums:
-            result += abs(n - nums[mid])
-        return result
+        return sum(abs(nums[len(nums) // 2] - i) for i in nums)
