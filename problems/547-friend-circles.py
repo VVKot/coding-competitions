e74ef7427ccb909cbd855fd8f09b9178bf5dc749
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-15 20:49:30 +0200

    feat: Make 547 iterative

--- problems/547-friend-circles.py
+++ problems/547-friend-circles.py
@@ -1,16 +1,16 @@
 class Solution:
     def findCircleNum(self, M):
         N = len(M)
-        visited = set()
-
-        def dfs(friend):
-            for i, is_friend in enumerate(M[friend]):
-                if is_friend and i not in visited:
-                    visited.add(i)
-                    dfs(i)
         result = 0
+        visited = set()
         for i in range(N):
             if i not in visited:
-                dfs(i)
+                stack = [i]
+                while stack:
+                    curr = stack.pop()
+                    if curr not in visited:
+                        visited.add(curr)
+                        stack.extend(i for i, is_friend in enumerate(
+                            M[curr]) if i not in visited and is_friend)
                 result += 1
         return result
