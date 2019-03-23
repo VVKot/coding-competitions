08f0138715a70269f2183487c1c166257234d4c6
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-23 14:31:20 +0200

    feat: Add iterative solution for 841

---
+++ problems/841-keys-and-rooms.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        stack = [0]
+        visited = set(stack)
+        while stack:
+            curr = stack.pop()
+            for room in rooms[curr]:
+                if room not in visited:
+                    stack.append(room)
+                    visited.add(room)
+                    if len(visited) == len(rooms): return True
+        return len(visited) == len(rooms)
