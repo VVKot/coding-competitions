8dc489d7654d2a7c4a73a27b5ee3a47a3abe0605
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-27 20:16:36 +0200

    feat: Add XOR solution

--- problems/693-binary-number-with-alternating-bits.py
+++ problems/693-binary-number-with-alternating-bits.py
@@ -1,12 +1,6 @@
 class Solution:
-	def hasAlternatingBits(self, n):
-		if not n:
-			return False
-		prev = None
-		while n:
-			if prev is not None and prev == n & 1:
-				return False
-			else:
-				prev = n & 1
-				n >>= 1
-		return True
+    def hasAlternatingBits(self, n):
+        if not n:
+            return False
+        num = n ^ (n >> 1)
+        return not (num & (num + 1))
