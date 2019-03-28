26986ba85617d58841a96440fbdbe9693823e39e
VVKot <volodymyr.kot.ua@gmail.com>
2019-03-28 05:58:55 +0200

    feat: Add solution for 703

---
+++ problems/703_kth_largest_element_in_stream.py
@@ -0,0 +1,17 @@
+import heapq
+
+
+class KthLargest:
+    def __init__(self, k, nums):
+        self.nums = nums
+        self.k = k
+        heapq.heapify(self.nums)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
+
+    def add(self, val):
+        if len(self.nums) < self.k:
+            heapq.heappush(self.nums, val)
+        else:
+            heapq.heappushpop(self.nums, val)
+        return self.nums[0]
