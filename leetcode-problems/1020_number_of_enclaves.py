0aaeee8bc6f320271966ff77d3fc8a482a077f62
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-09 20:08:11 +0300

    refactor: Refactor solution for 1020

--- leetcode-problems/1020_number_of_enclaves.py
+++ leetcode-problems/1020_number_of_enclaves.py
@@ -1,7 +1,16 @@
 class Solution:
-    def numEnclaves(self, A):
-        if not any(A):
-            return 0
+    def dfs(self, A, y, x):
+        rows = len(A)
+        cols = len(A[0])
+        stack = [(y, x)]
+        while stack:
+            yy, xx = stack.pop()
+            A[yy][xx] = 0
+            for r, c in [(yy-1, xx), (yy+1, xx), (yy, xx-1), (yy, xx+1)]:
+                if 0 <= r < rows and 0 <= c < cols and A[r][c]:
+                    stack.append((r, c))
+
+    def find_borders(self, A):
         rows = len(A)
         cols = len(A[0])
         borders = []
@@ -11,13 +20,13 @@ class Solution:
         for y in range(rows):
             borders.append((y, 0))
             borders.append((y, cols-1))
+        return borders
+
+    def numEnclaves(self, A):
+        if not any(A):
+            return 0
+        borders = self.find_borders(A)
         for y, x in borders:
-            if A[y][x] and (y == 0 or x == 0 or y == rows-1 or x == cols-1):
-                stack = [(y, x)]
-                while stack:
-                    yy, xx = stack.pop()
-                    A[yy][xx] = 0
-                    for r, c in [(yy-1, xx), (yy+1, xx), (yy, xx-1), (yy, xx+1)]:
-                        if 0 <= r < rows and 0 <= c < cols and A[r][c]:
-                            stack.append((r, c))
+            if A[y][x]:
+                self.dfs(A, y, x)
         return sum(sum(row) for row in A)
