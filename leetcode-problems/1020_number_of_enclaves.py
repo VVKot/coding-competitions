7fc63cd2926e66045e147e6f1f3d4b3b01e40225
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-09 07:37:27 +0300

    feat: only iterate over borders in 1020

--- leetcode-problems/1020_number_of_enclaves.py
+++ leetcode-problems/1020_number_of_enclaves.py
@@ -4,14 +4,20 @@ class Solution:
             return 0
         rows = len(A)
         cols = len(A[0])
+        borders = []
+        for x in range(cols):
+            borders.append((0, x))
+            borders.append((rows-1, x))
         for y in range(rows):
-            for x in range(cols):
-                if A[y][x] and (y == 0 or x == 0 or y == rows-1 or x == cols-1):
-                    stack = [(y, x)]
-                    while stack:
-                        yy, xx = stack.pop()
-                        A[yy][xx] = 0
-                        for r, c in [(yy-1, xx), (yy+1, xx), (yy, xx-1), (yy, xx+1)]:
-                            if 0 <= r < rows and 0 <= c < cols and A[r][c]:
-                                stack.append((r, c))
+            borders.append((y, 0))
+            borders.append((y, cols-1))
+        for y, x in borders:
+            if A[y][x] and (y == 0 or x == 0 or y == rows-1 or x == cols-1):
+                stack = [(y, x)]
+                while stack:
+                    yy, xx = stack.pop()
+                    A[yy][xx] = 0
+                    for r, c in [(yy-1, xx), (yy+1, xx), (yy, xx-1), (yy, xx+1)]:
+                        if 0 <= r < rows and 0 <= c < cols and A[r][c]:
+                            stack.append((r, c))
         return sum(sum(row) for row in A)
