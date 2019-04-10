513a36a7e1928eaba2f051f19215957f21052974
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-10 20:52:11 +0300

    refactor: fix variable names

--- leetcode-problems/295_find_median_from_data_stream.py
+++ leetcode-problems/295_find_median_from_data_stream.py
@@ -7,22 +7,12 @@ class MedianFinder:
 
     def addNum(self, num):
         if len(self.min_heap) == len(self.max_heap):
-            heappush(self.max_heap, -heappushpop(self.min_heap, -num))
+            heappush(self.min_heap, -heappushpop(self.max_heap, -num))
         else:
-            heappush(self.min_heap, -heappushpop(self.max_heap, num))
+            heappush(self.max_heap, -heappushpop(self.min_heap, num))
 
     def findMedian(self):
         if len(self.min_heap) == len(self.max_heap):
-            return float(self.max_heap[0] - self.min_heap[0]) / 2.0
+            return float(self.min_heap[0] - self.max_heap[0]) / 2.0
         else:
-            return float(self.max_heap[0])
-
-obj = MedianFinder()
-obj.addNum(1)
-assert obj.findMedian() == 1.0
-obj.addNum(2)
-assert obj.findMedian() == 1.5
-obj.addNum(3)
-assert obj.findMedian() == 2.0
-obj.addNum(4)
-assert obj.findMedian() == 2.5
+            return float(self.min_heap[0])
