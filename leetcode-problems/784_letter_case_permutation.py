24ac47506c7abf1636d296c4fbb291dff0837763
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-14 20:54:17 +0300

    feat: add more consice solution

--- leetcode-problems/784_letter_case_permutation.py
+++ leetcode-problems/784_letter_case_permutation.py
@@ -1,10 +1,19 @@
-class Solution:
-    def letterCasePermutation(self, S):
-        result = ['']
-        for ch in S:
-            if ch.isalpha():
-                result = [
-                    pre+suf for pre in result for suf in [ch.upper(), ch.lower()]]
-            else:
-                result = [pre+ch for pre in result]
-        return result
+from collections import defaultdict
+from itertools import product
+
+
+class Solution(object):
+
+    def pyramidTransition(self, bottom, allowed):
+        f = defaultdict(lambda: defaultdict(list))
+        for a, b, c in allowed:
+            f[a][b].append(c)
+
+        def pyramid(bottom):
+            if len(bottom) == 1:
+                return True
+            for i in product(*(f[a][b] for a, b in zip(bottom, bottom[1:]))):
+                if pyramid(i):
+                    return True
+            return False
+        return pyramid(bottom)
