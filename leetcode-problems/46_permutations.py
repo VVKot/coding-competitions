b0b9ec4ae6c2ac41f2301de93662f998721d9b7f
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-14 15:20:06 +0300

    feat: add solution for 46

---
+++ leetcode-problems/46_permutations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def permute(self, nums):
+        result = []
+
+        def helper(curr, rest):
+            if not rest:
+                result.append(curr)
+            else:
+                for i, num in enumerate(rest):
+                    helper(curr + [num], rest[:i] + rest[i+1])
+        helper([], nums)
+        return result
