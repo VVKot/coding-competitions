feca016b0a44bc616aa081a963905a9989816e4e
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-19 18:56:23 +0300

    feat: add solution without auxiliary function

--- leetcode_problems/43_multiply_strings.py
+++ leetcode_problems/43_multiply_strings.py
@@ -6,30 +6,21 @@ class Solution(object):
     def my_int(self, num):
         return ord(num) - ord('0')
 
-    def add(self, num1, num2):
-        carry = 0
-        result = []
-        for i, j in zip_longest(num1, num2, fillvalue=0):
-            carry, curr = divmod(i + j + carry, 10)
-            result.append(curr)
-        if carry != 0:
-            result.append(carry)
-        return result
-
     def multiply(self, num1, num2):
-        result = []
-        for i, n1 in enumerate(reversed(num1)):
-            temp = [0] * i
-            carry = 0
-            for n2 in reversed(num2):
-                carry, curr = divmod(self.my_int(n1) * self.my_int(n2) + carry, 10)
-                temp.append(curr)
-            if carry != 0:
-                temp.append(carry)
-            result = self.add(result, temp)
-        while len(result) > 1:
-            if result[-1] == 0:
-                result.pop()
-            else:
-                break
-        return "".join(str(x) for x in reversed(result))
+        M = len(num1)
+        N = len(num2)
+        nums = [0] * (M + N)
+        for i in reversed(range(M)):
+            for j in reversed(range(N)):
+                low = i + j + 1
+                high = i + j
+                n1 = self.my_int(num1[i])
+                n2 = self.my_int(num2[j])
+                carry, curr = divmod(n1 * n2 + nums[low], 10)
+                nums[low] = curr
+                nums[high] += carry
+        result = ""
+        for num in nums:
+            if result or num:
+                result += str(num)
+        return result if result else "0"
