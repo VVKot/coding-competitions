3fbc931d923a73b77dfebaa759294651c3b29f5a
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-20 08:05:45 +0300

    refactor: extract method

--- pramp/find_duplicates.py
+++ pramp/find_duplicates.py
@@ -23,23 +23,28 @@ def get_duplicates_bs(large, small):
     return result
 
 
+def get_duplicates(arr1, arr2):
+    result = []
+    p1, p2 = 0, 0
+    len1, len2 = len(arr1), len(arr2)
+    while p1 < len1 and p2 < len2:
+        curr1, curr2 = arr1[p1], arr2[p2]
+        if curr1 == curr2:
+            result.append(curr1)
+            p1 += 1
+            p2 += 1
+        elif curr1 < curr2:
+            p1 += 1
+        else:
+            p2 += 1
+    return result
+
+
 def find_duplicates(arr1, arr2):
     len1 = len(arr1)
     len2 = len(arr2)
     # determine if we should use binary search or regular search
     if math.log(len2, 2) * len1 > len2 + len1:
-        result = []
-        p1, p2 = 0, 0
-        while p1 < len1 and p2 < len2:
-            curr1, curr2 = arr1[p1], arr2[p2]
-            if curr1 == curr2:
-                result.append(curr1)
-                p1 += 1
-                p2 += 1
-            elif curr1 < curr2:
-                p1 += 1
-            else:
-                p2 += 1
-        return result
+        return get_duplicates(arr1, arr2)
     else:
         return get_duplicates_bs(arr2, arr1)
