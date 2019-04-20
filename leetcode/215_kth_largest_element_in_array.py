4a42b9c6ed2b60a367b024077a30171c218a338d
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-20 15:57:00 +0300

    feat: add partitioning solution for 215

--- leetcode/215_kth_largest_element_in_array.py
+++ leetcode/215_kth_largest_element_in_array.py
@@ -1,12 +1,32 @@
-class Solution:
+class Solution(object):
+
+    def get_pivot(self, left, right):
+        return (left + right) // 2
+
+    def partition(self, nums, pivot_index, left, right):
+        pivot = nums[pivot_index]
+        nums[-1], nums[pivot_index] = nums[pivot_index], nums[-1]
+        read, write = left, left
+        while read <= right:
+            if nums[read] >= pivot:
+                read += 1
+                continue
+            if read != write:
+                nums[read], nums[write] = nums[write], nums[read]
+            read += 1
+            write += 1
+        nums[write], nums[-1] = nums[-1], nums[write]
+        return write, nums
+
     def findKthLargest(self, nums, k):
-        pivot = nums[k//2]
-        left = [l for l in nums if l < pivot]
-        equal = [e for e in nums if e == pivot]
-        right = [r for r in nums if r > pivot]
-        if k <= len(right):
-            return self.findKthLargest(right, k)
-        elif (k - len(right)) <= len(equal):
-            return equal[0]
-        else:
-            return self.findKthLargest(left, k - len(right) - len(equal))
+        n = len(nums)
+        left, right, target = 0, n - 1, n - k
+        while True:  # TODO check
+            pivot_index = self.get_pivot(left, right)
+            final, nums = self.partition(nums, pivot_index, left, right)
+            if final == target:
+                return nums[final]
+            if final > target:
+                right = final - 1
+            else:
+                left = final + 1
