be311d1566df90e7dfc89a4186a6b17c3a4f932e
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-21 20:36:15 +0300

    docs: add comment for explanation

--- leetcode/78_subsets.py
+++ leetcode/78_subsets.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def subsets(self, nums):
         res = []
-        nums.sort()
-        #0, 1, 2, 4, 8, 16
-        for i in range(1<<len(nums)):# 0b1000 -> base 10 ->8
+        for i in range(1 << len(nums)):
             tmp = []
-            for j in range(len(nums)): # 0, 1, 2
-                if i & 1 << j:  # if i 
+            for j in range(len(nums)):
+                if i & 1 << j:
                     tmp.append(nums[j])
             res.append(tmp)
         return res
