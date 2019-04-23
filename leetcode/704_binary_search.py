b0b8b550b380578a7a4a55d401f72df62c81626f
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-23 20:26:20 +0300

    feat: add solution for 704

---
+++ leetcode/704_binary_search.py
@@ -0,0 +1,15 @@
+class Solution(object):
+
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left+right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
