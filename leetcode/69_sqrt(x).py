84e0f542b55f78fa81465e5ca107b44b8a908b18
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-24 05:52:52 +0300

    feat: add Newton solution

--- leetcode/69_sqrt(x).py
+++ leetcode/69_sqrt(x).py
@@ -1,23 +1,9 @@
 class Solution:
 
-    EPS = 0.00001
-
     def mySqrt(self, x):
-        if x == 0 or x == 1:
+        if not x:
             return x
-        left, right = 0, x
-        if x < 1:
-            right = 1
-        while left < right:
-            mid = (left + right) / 2.0
-            error = mid ** 2 - x
-            if abs(error) < Solution.EPS:
-                return int(mid)
-            if error > 0:
-                right = mid
-            else:
-                left = mid
-        return -1
-
-s = Solution()
-s.mySqrt(5)
+        r = x
+        while r*r > x:
+            r = (r + x//r) // 2
+        return int(r)
