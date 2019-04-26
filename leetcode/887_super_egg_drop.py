2c715b704c97d8ce708f3889dc5884d9d6eeb662
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-26 06:02:55 +0300

    feat: add O(K*logN) solution

--- leetcode/887_super_egg_drop.py
+++ leetcode/887_super_egg_drop.py
@@ -1,12 +1,9 @@
 class Solution:
 
     def superEggDrop(self, K, N):
-        cache = [[0] * (N+1) for _ in range(K+1)]
-        cache[1] = list(range(N+1))
-        for i in range(2, K+1):
-            for j in range(1, N+1):
-                cache[i][j] = float('inf')
-                for m in range(1, j+1):
-                    prev = max(cache[i-1][m-1], cache[i][j-m]) + 1
-                    cache[i][j] = min(prev, cache[i][j])
-        return cache[-1][-1]
+        cache = [[0] * (K+1) for _ in range(N+1)]
+        for m in range(1, N+1):
+            for k in range(1, K+1):
+                cache[m][k] = 1 + cache[m-1][k-1] + cache[m-1][k]
+            if cache[m][-1] >= N:
+                return m
