041e739bb278b4fe14d20901d3e95097af80e3a4
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-28 09:06:57 +0300

    feat: add iterative solution for 100

--- leetcode/100_same_tree.py
+++ leetcode/100_same_tree.py
@@ -1,13 +1,13 @@
 class Solution:
     def isSameTree(self, p, q):
-        if not p:
-            if q:
-                return False
+        stack = [(p, q)]
+        while stack:
+            n1, n2 = stack.pop()
+            if n1 and n2 and n1.val == n2.val:
+                stack.append((n1.right, n2.right))
+                stack.append((n1.left, n2.left))
+            elif not n1 and not n2:
+                continue
             else:
-                return True
-        else:
-            if not q:
                 return False
-        if p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return True
