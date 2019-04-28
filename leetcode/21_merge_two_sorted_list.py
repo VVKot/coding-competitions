73d55a4dac474c46af224c1ad276cffab8020f72
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-28 16:19:06 +0300

    docs: fix linting error

--- leetcode/21_merge_two_sorted_list.py
+++ leetcode/21_merge_two_sorted_list.py
@@ -1,8 +1,8 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
