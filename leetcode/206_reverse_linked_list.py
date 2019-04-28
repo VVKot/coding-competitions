b504a8fce2fd3968a14679bf42f646c1e72e3370
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-28 21:51:50 +0300

    feat: add iterative solution

--- leetcode/206_reverse_linked_list.py
+++ leetcode/206_reverse_linked_list.py
@@ -1,6 +1,15 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution:
-    def reverseList(self, head):
-        cur, prev = head, None
-        while cur:
-            cur.next, prev, cur = prev, cur, cur.next
+    def reverseList(self, head: ListNode) -> Optional[ListNode]:
+        prev = None
+        while head:
+            prev, head.next, head = head, prev, head.next
         return prev
