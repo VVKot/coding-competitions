76a3d67d90006b35dc91f043b3f590dc150dd74b
VVKot <volodymyr.kot.ua@gmail.com>
2019-04-29 20:25:30 +0300

    feat: add Counter solution

---
+++ leetcode/169_majority_element.py
@@ -0,0 +1,8 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+
+    def majorityElement(self, nums: List[int]) -> int:
+        return Counter(nums).most_common()[0][0]
