93e4f9d31cc3f5eabce52f9ca2194d5b0eaf2a7d
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-02 05:35:05 +0300

    feat: add rotation solution for 542

--- leetcode/542_01_matrix.py
+++ leetcode/542_01_matrix.py
@@ -1,39 +1,13 @@
-from collections import deque
+from typing import List
 
 
 class Solution:
-    def multihead_bfs_update(self, matrix, heads):
-        rows = len(matrix)
-        cols = len(matrix[0])
-        stack = deque([(y, x, 2) for y, x in heads])
-        while stack:
-            y, x, val = stack.popleft()
-            near = self.get_near(y, x)
-            for r, c in near:
-                if 0 <= r < rows and 0 <= c < cols:
-                    if (r, c) not in heads and matrix[r][c] == 1:
-                        matrix[r][c] = val
-                        stack.append((r, c, val+1))
 
-    def get_bfs_heads(self, matrix):
-        heads = set()
-        rows = len(matrix)
-        cols = len(matrix[0])
-        for y, row in enumerate(matrix):
-            for x, num in enumerate(row):
-                if not num or (y, x) in heads:
-                    continue
-                for r, c in self.get_near(y, x):
-                    if 0 <= r < rows and 0 <= c < cols:
-                        if not matrix[r][c]:
-                            heads.add((y, x))
-                            break
-        return heads
-
-    def get_near(self, y, x):
-        return [(y, x+1), (y, x-1), (y+1, x), (y-1, x)]
-
-    def updateMatrix(self, matrix):
-        heads = self.get_bfs_heads(matrix)
-        self.multihead_bfs_update(matrix, heads)
-        return matrix
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        answer = [[10000 * x for x in row] for row in matrix]
+        for _ in range(4):
+            for row in answer:
+                for j in range(1, len(row)):
+                    row[j] = min(row[j], row[j-1] + 1)
+            answer = list(map(list, zip(*answer[::-1])))
+        return answer
