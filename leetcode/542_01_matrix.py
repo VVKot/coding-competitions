9d27e6ee56d9e131fa6dbeb1f8f94049d1bc7ef5
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-02 20:31:46 +0300

    feat: make solution O(1) space

--- leetcode/542_01_matrix.py
+++ leetcode/542_01_matrix.py
@@ -4,10 +4,10 @@ from typing import List
 class Solution:
 
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
-        answer = [[10000 * x for x in row] for row in matrix]
+        matrix = [[10000 * x for x in row] for row in matrix]
         for _ in range(4):
-            for row in answer:
+            for row in matrix:
                 for j in range(1, len(row)):
                     row[j] = min(row[j], row[j-1] + 1)
-            answer = list(map(list, zip(*answer[::-1])))
-        return answer
+            matrix = list(map(list, zip(*matrix[::-1])))
+        return matrix
