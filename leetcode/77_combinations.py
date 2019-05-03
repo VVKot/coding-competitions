327809e5a1c4b2f968c6d49863de7818f5beab1e
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-03 07:48:05 +0300

    feat: add iterative solution

--- leetcode/77_combinations.py
+++ leetcode/77_combinations.py
@@ -4,13 +4,8 @@ from typing import List
 class Solution:
 
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-
-        def backtrack(comb, start):
-            if len(comb) == k:
-                result.append(comb)
-                return
-            for i in range(start, n+1):
-                backtrack(comb + [i], i+1)
-        backtrack([], 1)
-        return result
+        combs = [[]]  # type: List[List[int]]
+        for j in range(k, 0, -1):
+            combs = [
+                [i] + c for c in combs for i in range(j, c[0] if c else n+1)]
+        return combs
