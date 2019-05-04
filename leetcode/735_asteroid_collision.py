9b4654d15926bc79948894e0ad8d65600fe74072
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-04 08:03:18 +0300

    docs: add typings

--- leetcode/735_asteroid_collision.py
+++ leetcode/735_asteroid_collision.py
@@ -1,22 +1,25 @@
+from typing import List
+
+
 class Solution:
-    def collapseAsteroids(self, asts, new_):
+    def collapseAsteroids(self, asts: List[int], new_: int) -> List[int]:
         while True:
             if not asts or asts[-1] < 0:
                 asts.append(new_)
                 break
-            last = asts[-1] 
+            last = asts[-1]
             if last == -new_:
                 asts.pop()
                 break
             elif last > -new_:
                 break
             else:
-                asts.pop()	
-        return asts 
+                asts.pop()
+        return asts
 
-    def asteroidCollision(self, ast):
-        result = []
-        for a in ast:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        result = []  # type: List[int]
+        for a in asteroids:
             if result and result[-1] > 0 and a < 0:
                 result = self.collapseAsteroids(result, a)
             else:
