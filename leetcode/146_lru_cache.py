bfa82d534d550a9d5c2b13a6737aa564e9debee2
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-06 20:55:39 +0300

    docs: add typings

--- leetcode/146_lru_cache.py
+++ leetcode/146_lru_cache.py
@@ -2,20 +2,24 @@ from collections import OrderedDict
 
 
 class LRUCache:
-    def __init__(self, Capacity):
-        self.capacity = Capacity
-        self.cache = OrderedDict()
 
-    def get(self, key):
-        if key not in self.cache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()  # type: OrderedDict[int, int]
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key, last=True)
+            return self.cache[key]
+        else:
             return -1
-        value = self.cache[key]
-        self.cache.move_to_end(key)
-        return value
 
-    def put(self, key, value):
+    def put(self, key: int, value: int) -> None:
         if key in self.cache:
             del self.cache[key]
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
+            self.cap += 1
+        if not self.cap:
             self.cache.popitem(last=False)
+        else:
+            self.cap -= 1
+        self.cache[key] = value
