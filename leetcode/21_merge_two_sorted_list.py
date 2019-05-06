d4e6719ee777733cf7318011131ed6ca7b1a18df
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-06 20:17:28 +0300

    docs: add typings

--- leetcode/21_merge_two_sorted_list.py
+++ leetcode/21_merge_two_sorted_list.py
@@ -1,19 +1,19 @@
-class ListNode(object):
+class ListNode:
+
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        head = curr = ListNode(0)
+class Solution:
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        temp = head = ListNode(-1)
         while l1 and l2:
             if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
+                head.next, l1 = l1, l1.next
             else:
-                curr.next = l2
-                l2 = l2.next
-            curr = curr.next
-        curr.next = l1 or l2
-        return head.next
+                head.next, l2 = l2, l2.next
+            head = head.next
+        head.next = l1 or l2
+        return temp.next
