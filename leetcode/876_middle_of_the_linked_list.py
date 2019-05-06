b7a465c7928f765b1404a8f8b6bfbdaf1ada9ba5
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-06 21:28:20 +0300

    feat: add two pointers solution

--- leetcode/876_middle_of_the_linked_list.py
+++ leetcode/876_middle_of_the_linked_list.py
@@ -1,12 +1,15 @@
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution:
-    def middleNode(self, head):
-        list_len = 0
-        old_head = head
-        while head:
-            list_len += 1
-            head = head.next
-            middle_index = list_len // 2
-        while middle_index != 0:
-            middle_index -= 1
-            old_head = old_head.next
-        return old_head
+
+    def middleNode(self, head: ListNode) -> ListNode:
+        fast, slow = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
