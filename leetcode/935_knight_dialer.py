afa29f1b7f4dced56deeda13f3c934719921ca2f
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-07 07:14:11 +0300

    feat: use list in place of dicts

--- leetcode/935_knight_dialer.py
+++ leetcode/935_knight_dialer.py
@@ -1,17 +1,18 @@
-from typing import Dict, List
-
-
 class Solution:
 
+    NEARS = {0: [4, 6], 1: [6, 8], 2: [7, 9], 3: [4, 8],
+             4: [0, 3, 9], 5: [], 6: [0, 1, 7], 7: [2, 6],
+             8: [1, 3], 9: [2, 4]}
+
+    def get_near(self, num):
+        return self.NEARS[num]
+
     def knightDialer(self, N: int) -> int:
-        num_dials = {i: 1 for i in range(10)}
-        near = {0: [4, 6], 1: [6, 8], 2: [7, 9], 3: [4, 8],
-                4: [0, 3, 9], 5: [], 6: [0, 1, 7], 7: [2, 6],
-                8: [1, 3], 9: [2, 4]}  # type: Dict[int, List[int]]
+        dial_size = 10
+        old = [1] * dial_size
+        new = [1] * dial_size
         for _ in range(N-1):
-            dials = dict(num_dials)
-            for i in range(10):
-                near_sum = sum(num_dials[j] for j in near[i])
-                dials[i] = near_sum
-            num_dials = dials
-        return sum(num_dials.values()) % (10**9 + 7)
+            for i in range(dial_size):
+                new[i] = sum(old[j] for j in self.get_near(i))
+            old = new[::]
+        return sum(old) % (10**9 + 7)
