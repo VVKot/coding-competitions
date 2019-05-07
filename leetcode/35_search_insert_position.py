70d41685d2ca57de04641923de8ad284b94a0beb
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-07 07:25:36 +0300

    docs: add typings

--- leetcode/35_search_insert_position.py
+++ leetcode/35_search_insert_position.py
@@ -1,12 +1,13 @@
+from typing import List
+
+
 class Solution:
-    def searchInsert(self, nums, target):
-        left = 0
-        right = len(nums) - 1
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
         while left <= right:
-            mid = left + (right - left) // 2
-            mid_value = nums[mid]
-            if mid_value >= target:
-                right = mid - 1
-            else:
+            mid = (left + right) // 2
+            if nums[mid] < target:
                 left = mid + 1
+            else:
+                right = mid - 1
         return left
