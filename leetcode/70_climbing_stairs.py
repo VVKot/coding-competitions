1649d21322e5e6c9315f3718312f5bcd8b1b1980
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-07 07:36:46 +0300

    docs: add typings

--- leetcode/70_climbing_stairs.py
+++ leetcode/70_climbing_stairs.py
@@ -1,6 +1,6 @@
 class Solution:
-    def climbStairs(self, n):
-        first = second = 1
+    def climbStairs(self, n: int) -> int:
+        first, second = 1, 1
         for _ in range(n):
             first, second = second, first + second
         return first
