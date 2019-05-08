cc4008afd60a9c1d24d1f25ad039979a3a6fdb71
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-08 06:18:28 +0300

    feat: add one-liner

--- leetcode/832_flipping_an_image.py
+++ leetcode/832_flipping_an_image.py
@@ -3,15 +3,4 @@ from typing import List
 
 class Solution:
     def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
-        A = self.get_flipped_image(A)
-        A = self.get_inverted_image(A)
-        return A
-
-    def get_flipped_image(self, A):
-        return [list(reversed(line)) for line in A]
-
-    def get_inverted_image(self, A):
-        return [self.get_reversed_line(line) for line in A]
-
-    def get_reversed_line(self, line):
-        return [0 if x else 1 for x in line]
+        return [[i ^ 1 for i in row[::-1]] for row in A]
