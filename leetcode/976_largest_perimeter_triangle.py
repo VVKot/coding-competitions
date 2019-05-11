2c039d0fe8af3be055dcaa9488e8dab3ef2038c8
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-11 06:42:07 +0300

    feat: add NlogN solution

--- leetcode/976_largest_perimeter_triangle.py
+++ leetcode/976_largest_perimeter_triangle.py
@@ -2,15 +2,11 @@ from typing import List
 
 
 class Solution:
+
     def largestPerimeter(self, A: List[int]) -> int:
         A.sort(reverse=True)
-        result = 0
         N = len(A)
-        for i in range(2, N):
-            big = A[i-2]
-            small = A[i-1]
-            for j in range(i, N):
-                if A[j] + small > big:
-                    result = max(result, A[j] + small + big)
-                    break
-        return result
+        for i in range(N-2):
+            if A[i] < A[i+1] + A[i+2]:
+                return A[i] + A[i+1] + A[i+2]
+        return 0
