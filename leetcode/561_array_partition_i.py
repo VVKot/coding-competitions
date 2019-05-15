09c176146586c436507b5e9bea5582440f31873d
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-15 06:56:50 +0300

    refactor: add pythonic solution

--- leetcode/561_array_partition_i.py
+++ leetcode/561_array_partition_i.py
@@ -2,9 +2,6 @@ from typing import List
 
 
 class Solution:
+
     def arrayPairSum(self, nums: List[int]) -> int:
-        result = 0
-        for i, num in enumerate(sorted(nums)):
-            if i % 2 == 0:
-                result += num
-        return result
+        return sum(sorted(nums)[::2])
