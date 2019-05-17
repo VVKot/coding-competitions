0d14bb1e15c97be9b80977c8e674c73c3dcf2181
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-17 21:08:54 +0300

    docs: add typings

--- leetcode/83_remove_duplicates_from_sorted_list.py
+++ leetcode/83_remove_duplicates_from_sorted_list.py
@@ -1,18 +1,16 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
 
-class Solution(object):
-    def deleteDuplicates(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
         curr = head
         while curr and curr.next:
-            if curr.next.val == curr.val:
+            if curr.val == curr.next.val:
                 curr.next = curr.next.next
             else:
                 curr = curr.next
