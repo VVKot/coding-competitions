583fa2cd7c2d826fe1f33784e9b508ae4d7d7ba2
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-19 08:18:59 +0300

    feat: add solution for 669

---
+++ leetcode/669_trim_a_binary_search_tree.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class TreeNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def trimBST(self, root: TreeNode, L: int, R: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        root.left = self.trimBST(root.left, L, R)
+        return root
