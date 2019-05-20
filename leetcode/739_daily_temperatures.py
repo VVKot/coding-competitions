9799b3173c7e56fbf8faf16b0a0b15e3bc445da1
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-20 20:51:45 +0300

    docs: add typings

--- leetcode/739_daily_temperatures.py
+++ leetcode/739_daily_temperatures.py
@@ -1,12 +1,13 @@
-class Solution(object):
-    def dailyTemperatures(self, T):
-        result = [0] * len(T)
-        stack = []
-        for day, temp in enumerate(T):
+from typing import List
+
+
+class Solution:
+
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        stack, result = [], [0] * len(T)  # type: List[int], List[int]
+        for i, temp in enumerate(T):
             while stack and T[stack[-1]] < temp:
-                curr = stack.pop()
-                result[cur] = day - curr
-            stack.append(day)
-
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            stack.append(i)
         return result
-
