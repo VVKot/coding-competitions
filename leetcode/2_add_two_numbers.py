7ab44ca043c59a789dbdae7aa14af16b17c5d93c
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-21 20:24:47 +0300

    docs: add typings

--- leetcode/2_add_two_numbers.py
+++ leetcode/2_add_two_numbers.py
@@ -1,11 +1,13 @@
-# Definition for singly-linked list.
 class ListNode:
+
     def __init__(self, x):
         self.val = x
         self.next = None
 
+
 class Solution:
-    def addTwoNumbers(self, l1, l2):
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         carry = 0
         root = n = ListNode(0)
         while l1 or l2 or carry:
