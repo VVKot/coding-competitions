49ad055748cdbb7fc098e16ccc0d49202c158165
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-22 06:15:36 +0300

    feat: add linear time solution

--- leetcode/905_sort_array_by_parity.py
+++ leetcode/905_sort_array_by_parity.py
@@ -4,5 +4,10 @@ from typing import List
 class Solution:
 
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        A.sort(key=lambda x: x & 1)
+        i, j, N = 0, 0, len(A)
+        while i < N:
+            if not A[i] & 1:
+                A[i], A[j] = A[j], A[i]
+                j += 1
+            i += 1
         return A
