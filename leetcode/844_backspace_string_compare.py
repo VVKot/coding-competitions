9dc0c6be70ac3d48a77ff76ba4937ffca4dab67b
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-23 21:52:15 +0300

    feat: add constant memory solution

--- leetcode/844_backspace_string_compare.py
+++ leetcode/844_backspace_string_compare.py
@@ -1,16 +1,27 @@
 class Solution:
 
-    def backspaceCompare(self, S: str, T: str) -> bool:
-        S = self.get_res(S)
-        T = self.get_res(T)
-        return S == T
+    BACKSPACE_CHARACTER = '#'
 
-    def get_res(self, seq):
-        stack = []
-        for ch in seq:
-            if ch == '#':
-                if stack:
-                    stack.pop()
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        len_s, len_t = len(S), len(T)
+        i, j = len_s - 1, len_t - 1
+        while i > -1 or j > -1:
+            i, at_i = self.get_next(S, i)
+            j, at_j = self.get_next(T, j)
+            if at_i != at_j:
+                return False
+        return i == j
+
+    def get_next(self, seq, index):
+        backspace_count = 0
+        while index >= 0:
+            curr = seq[index]
+            if curr == Solution.BACKSPACE_CHARACTER:
+                backspace_count += 1
             else:
-                stack.append(ch)
-        return ''.join(stack)
+                if backspace_count == 0:
+                    new_index = index - 1
+                    return new_index, curr
+                backspace_count -= 1
+            index -= 1
+        return -1, Solution.BACKSPACE_CHARACTER
