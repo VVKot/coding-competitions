7601611544728bb6b69628c522c6ca4389f2c625
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-26 19:58:50 +0300

    feat: add dp solution

---
+++ leetcode/740_delete_and_earn.py
@@ -0,0 +1,16 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        N, nums_counter = len(nums), Counter(nums)
+        prevprev, prev = (float('inf'), 0), (float('inf'), 0)
+        for num, count in sorted(nums_counter.items()):
+            curr = num * count
+            if num - 1 == prev[0]:
+                curr = max(curr + prevprev[1], prev[1])
+            else:
+                curr += prev[1]
+            prevprev, prev = prev, (num, curr)
+        return prev[1]
