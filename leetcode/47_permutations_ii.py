da49d0300ef24f122081cea20caec6a90037e4fb
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-26 19:51:14 +0300

    docs: add typings

--- leetcode/47_permutations_ii.py
+++ leetcode/47_permutations_ii.py
@@ -1,13 +1,19 @@
+from typing import List
+
+
 class Solution:
-    def permuteUnique(self, nums):
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         result = []
         visited = [False] * len(nums)
+
         def backtrack(curr, rest, seen):
             if len(curr) == len(rest):
                 result.append(curr)
             else:
                 for i, num in enumerate(rest):
-                    if seen[i] or (i > 0 and rest[i-1] == num and not seen[i-1]):
+                    if seen[i]:
+                        continue
+                    if i > 0 and rest[i-1] == num and not seen[i-1]:
                         continue
                     seen[i] = True
                     backtrack(curr + [num], rest, seen)
