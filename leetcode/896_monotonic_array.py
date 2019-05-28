519b65117d71c27b4b09f98e5fd61db17ebb0b2b
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-28 05:34:20 +0300

    feat: add comparing for both possibilities

--- leetcode/896_monotonic_array.py
+++ leetcode/896_monotonic_array.py
@@ -3,15 +3,9 @@ from typing import List
 
 class Solution:
     def isMonotonic(self, A: List[int]) -> bool:
-        is_bigger, seen_first = False, False
+        inc, dec = True, True
         for i in range(1, len(A)):
             curr, prev = A[i], A[i-1]
-            if curr == prev:
-                continue
-            if not seen_first:
-                seen_first = True
-                is_bigger = curr > prev
-            else:
-                if (curr > prev) ^ is_bigger:
-                    return False
-        return True
+            inc &= curr >= prev
+            dec &= curr <= prev
+        return inc or dec
