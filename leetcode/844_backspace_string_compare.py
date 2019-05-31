bcd9c372ada7cf5e8c1264e54cfa62c3ec29edb0
VVKot <volodymyr.kot.ua@gmail.com>
2019-05-31 05:44:04 +0300

    refactor: make code more pythonic

--- leetcode/844_backspace_string_compare.py
+++ leetcode/844_backspace_string_compare.py
@@ -9,10 +9,7 @@ class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
         s_gen = self.get_next(S)
         t_gen = self.get_next(T)
-        for s_ch, t_ch in zip_longest(s_gen, t_gen):
-            if s_ch != t_ch:
-                return False
-        return True
+        return all(s == t for s, t in zip_longest(s_gen, t_gen))
 
     def get_next(self, seq: str) -> Generator[str, None, None]:
         backspace_count = 0
