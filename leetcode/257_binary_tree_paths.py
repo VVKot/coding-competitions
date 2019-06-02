0126a690f08c027a878e51e4620d9d766bdb5896
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-02 21:48:21 +0300

    feat: add solution for 257

--- leetcode/257_binary_tree_paths.py
+++ leetcode/257_binary_tree_paths.py
@@ -1,18 +1,32 @@
+from typing import List
+
+
+class TreeNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
 class Solution:
-    def binaryTreePaths(self, root):
-        if not root:
-            return []
+
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
         result = []
-        stack = [(root, str(root.val))]
+        stack = []
+        if root:
+            stack.append([root])
         while stack:
-            curr, path = stack.pop()
+            path = stack.pop()
+            curr = path[-1]
             if not curr.left and not curr.right:
-                result.append(path)
+                result.append(self.get_formated_path(path))
             else:
                 if curr.left:
-                    new_path = "{}->{}".format(path, curr.left.val)
-                    stack.append((curr.left, new_path))
+                    stack.append(path + [curr.left])
                 if curr.right:
-                    new_path = "{}->{}".format(path, curr.right.val)
-                    stack.append((curr.right, new_path))
+                    stack.append(path + [curr.right])
         return result
+
+    def get_formated_path(self, path):
+        return '->'.join(str(node.val) for node in path)
