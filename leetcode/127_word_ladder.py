bac277cbbc18e0d5a3ff9d9b6430172eda2f47fd
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-02 09:28:46 +0300

    feat: add memory optimized solution

--- leetcode/127_word_ladder.py
+++ leetcode/127_word_ladder.py
@@ -1,30 +1,34 @@
-from collections import deque, defaultdict
+import string
+import collections
+from typing import List
 
 
-class Solution(object):
-
-    def get_word_without_char(self, word, i):
-        return word[:i] + "_" + word[i+1:]
-
-    def construct_dict(self, words):
-        similar_words = defaultdict(list)
-        for word in words:
-            for i in range(len(word)):
-                new_word = self.get_word_without_char(word, i)
-                similar_words[new_word].append(word)
-        return similar_words
-
-    def ladderLength(self, beginWord, endWord, wordList):
-        sim_words = self.construct_dict(wordList)
-        stack = deque([(beginWord, 1)])
-        while stack:
-            curr, path = stack.popleft()
+class Solution:
+    def ladderLength(self,
+                     beginWord: str,
+                     endWord: str,
+                     wordList: List[str]) -> int:
+        wordList = set(wordList)
+        if endWord not in wordList:
+            return 0
+        q = collections.deque([(beginWord, 1)])
+        while q:
+            curr, dist = q.popleft()
             if curr == endWord:
-                return path
-            for i in range(len(curr)):
-                new_curr = self.get_word_without_char(curr, i)
-                words = sim_words[new_curr]
-                for word in words:
-                    stack.append((word, path+1))
-                sim_words[new_curr] = []
+                return dist
+            near = self.get_near(wordList, curr)
+            for word in near:
+                wordList.remove(word)
+                q.append((word, dist + 1))
         return 0
+
+    def get_near(self, words, curr):
+        result = []
+        for i, ch in enumerate(curr):
+            for new_ch in string.ascii_lowercase:
+                if ch == new_ch:
+                    continue
+                new_word = curr[:i] + new_ch + curr[i+1:]
+                if new_word in words:
+                    result.append(new_word)
+        return result
