b72e876f3f34264b2355d91ee842e002e0b9a99c
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-03 05:55:39 +0300

    refactor: shorter version

--- leetcode/56_merge_intervals.py
+++ leetcode/56_merge_intervals.py
@@ -4,14 +4,10 @@ from typing import List
 class Solution:
 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        merged = []  # type: List[List[int]]
-        intervals.sort()
-        for start, end in intervals:
-            if not merged:
-                merged.append([start, end])
-            prev_end = merged[-1][1]
-            if prev_end < start:
-                merged.append([start, end])
+        result = []  # type: List[List[int]]
+        for start, end in sorted(intervals):
+            if result and start <= result[-1][1]:
+                result[-1][1] = max(result[-1][1], end)
             else:
-                merged[-1][1] = max(prev_end, end)
-        return merged
+                result.append([start, end])
+        return result
