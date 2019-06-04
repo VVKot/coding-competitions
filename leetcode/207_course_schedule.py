776951a92f3fbdc675c5cf4fc1ca142134a5f254
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-04 07:22:50 +0300

    refactor: use stack

--- leetcode/207_course_schedule.py
+++ leetcode/207_course_schedule.py
@@ -1,25 +1,23 @@
-from collections import deque
+import collections
+from typing import List, DefaultDict
 
 
 class Solution:
 
-    def canFinish(self, numCourses, prerequisites):
-        courses = [[] for _ in range(numCourses)]
-        in_degrees = [0] * numCourses
-        for after, pre in prerequisites:
-            courses[pre].append(after)
-            in_degrees[after] += 1
-        to_study = deque()
-        for i, num in enumerate(in_degrees):
-            if not num:
-                to_study.append(i)
-        studied = 0
-        while to_study:
-            curr = to_study.popleft()
-            studied += 1
-            opened = courses[curr]
-            for c in opened:
-                in_degrees[c] -= 1
-            if not in_degrees[c]:
-                to_study.append(c)
-        return studied == numCourses
+    def canFinish(self,
+                  numCourses: int,
+                  prerequisites: List[List[int]]) -> bool:
+        num_prereq = [0 for _ in range(numCourses)]
+        opens_courses = \
+            collections.defaultdict(list)  # type: DefaultDict[int, List[int]]
+        for c, pre in prerequisites:
+            num_prereq[c] += 1
+            opens_courses[pre].append(c)
+        available = [i for i, count in enumerate(num_prereq) if not count]
+        while available:
+            curr = available.pop()
+            for course in opens_courses[curr]:
+                num_prereq[course] -= 1
+                if not num_prereq[course]:
+                    available.append(course)
+        return not any(num_prereq)
