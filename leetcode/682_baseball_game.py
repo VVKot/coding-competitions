8301d08444101ca392dd6851f5e700a961d80dde
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-05 06:24:49 +0300

    feat: add solution for 682

---
+++ leetcode/682_baseball_game.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+
+    def calPoints(self, ops: List[str]) -> int:
+        scores = []  # type: List[int]
+        for op in ops:
+            if op == '+':
+                score = sum(scores[-2:])
+                scores.append(score)
+            elif op == 'D':
+                score = scores[-1] * 2
+                scores.append(score)
+            elif op == 'C':
+                scores.pop()
+            else:
+                score = int(op)
+                scores.append(score)
+        return sum(scores)
