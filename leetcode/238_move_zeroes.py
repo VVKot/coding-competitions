4461a64490e33137964e2fa205555c5b4712ff07
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-06 06:32:10 +0300

    feat: add optimized solution

--- leetcode/238_move_zeroes.py
+++ leetcode/238_move_zeroes.py
@@ -3,9 +3,6 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        read, write, N = 0, 0, len(nums)
-        while read < N:
-            if nums[read]:
-                nums[read], nums[write] = nums[write], nums[read]
-                write += 1
-            read += 1
+        zeroes = nums.count(0)
+        nums[:] = [i for i in nums if i]
+        nums += [0] * zeroes
