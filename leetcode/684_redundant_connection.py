64f726344dc954e1d13d50be20b47c47870082a6
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-07 19:59:29 +0300

    feat: implement proper union find

--- leetcode/684_redundant_connection.py
+++ leetcode/684_redundant_connection.py
@@ -1,22 +1,40 @@
+from typing import List
+
+
 class Solution:
-    def findRedundantConnection(self, edges):
-        N = len(edges) + 1
-        parent = [None] * N
 
-        def find(x):
-            if parent[x] is None:
-                return x
-            parent[x] = find(parent[x])
-            return parent[x]
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parents, ranks = {}, {}
 
-        def union(x, y):
-            root_x = find(x)
-            root_y = find(y)
-            if root_x == root_y:
-                return False
-            parent[root_x] = root_y
-            return True
+        def insert(node):
+            parents[node] = node
+            ranks[node] = 1
 
-        for x, y in edges:
-            if not union(x, y):
-                return [x, y]
+        def find(node):
+            curr_parent = parents[node]
+            if curr_parent == node:
+                return node
+            parents[node] = find(curr_parent)
+            return parents[node]
+
+        def union(node1, node2):
+            rank1, rank2 = ranks[node1], ranks[node2]
+            if rank1 == rank2:
+                ranks[node1] += 1
+                parents[node2] = node1
+            elif rank1 > rank2:
+                parents[node2] = node1
+            else:
+                parents[node1] = node2
+
+        for s, e in edges:
+            if s not in parents:
+                insert(s)
+            if e not in parents:
+                insert(e)
+            s_parent, e_parent = find(s), find(e)
+            if s_parent == e_parent:
+                return list(sorted([s, e]))
+            else:
+                union(s_parent, e_parent)
+        return [-1, -1]
