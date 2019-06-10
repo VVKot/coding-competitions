b2085870e77957dce8e2c193aa474fc19107e306
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-10 13:01:27 +0300

    feat: add solution for 837

--- leetcode/837_new_21_game.py
+++ leetcode/837_new_21_game.py
@@ -1,16 +1,15 @@
 class Solution:
 
     def new21Game(self, N: int, K: int, W: int) -> float:
-        if K == 0 or N > K + W:
+        if K == 0 or N >= K + W:
             return 1.0
-        dp = [0.0 for _ in range(K + W)]
+        dp = [0.0 for _ in range(N + 1)]
         dp[0] = 1.0
-        for i in range(1, K + W):
-            dp[i] = dp[i - 1]
-            if i <= W:
-                dp[i] += dp[i - 1] / W
-            else:
-                dp[i] += (dp[i - 1] - dp[i - W - 1]) / W
-            if i > K:
-                dp[i] -= (dp[i - 1] - dp[K - 1]) / W
-        return dp[N] - dp[K - 1]
+        wsum = 1.0
+        for i in range(1, N + 1):
+            dp[i] = wsum / W
+            if i < K:
+                wsum += dp[i]
+            if i >= W:
+                wsum -= dp[i - W]
+        return sum(dp[K:])
