7bf247ff0e44ddee61dd6db2e840c28b42b298fa
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-11 17:35:11 +0300

    feat: add constant memory solution

--- leetcode/23_merge_k_sorted_lists.py
+++ leetcode/23_merge_k_sorted_lists.py
@@ -1,5 +1,4 @@
-import heapq
-from typing import List
+from typing import List, Optional
 
 
 class ListNode:
@@ -11,20 +10,31 @@ class ListNode:
 
 class Solution:
 
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        dummy = curr = ListNode(-1)
+    def mergeKLists(self, lists: List[ListNode]) -> Optional[ListNode]:
         if not any(lists):
-            return dummy.next
-        nums = []
-        for i, node in enumerate(lists):
-            if node:
-                nums.append((node.val, i, node))
-        heapq.heapify(nums)
-        while nums:
-            _, list_idx, node = heapq.heappop(nums)
-            curr.next = node
+            return None
+
+        N = len(lists)
+        interval = 1
+        while interval < N:
+            for i in range(0, N - interval, interval * 2):
+                lists[i] = self.merge_lists(lists[i],
+                                            lists[i + interval])
+            interval *= 2
+        return lists[0]
+
+    def merge_lists(self, head1, head2):
+        curr = dummy = ListNode(-1)
+        while head1 and head2:
+            if head1.val < head2.val:
+                curr.next = head1
+                head1 = head1.next
+            else:
+                curr.next = head2
+                head2 = head2.next
             curr = curr.next
-            next_node = node.next
-            if next_node:
-                heapq.heappush(nums, (next_node.val, list_idx, next_node))
+        if head1:
+            curr.next = head1
+        if head2:
+            curr.next = head2
         return dummy.next
