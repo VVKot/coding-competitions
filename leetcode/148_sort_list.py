3a097012d1e0638bb3c44f6ccfd374273569f87f
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-11 18:39:08 +0300

    feat: add optimal solution

--- leetcode/148_sort_list.py
+++ leetcode/148_sort_list.py
@@ -10,29 +10,47 @@ class Solution:
     def sortList(self, head: ListNode) -> ListNode:
         if not head or not head.next:
             return head
-        before_mid = self.get_before_mid(head)
-        mid = before_mid.next
-        before_mid.next = None
-        left = self.sortList(head)
-        right = self.sortList(mid)
-        return self.merge_lists(left, right)
-
-    def get_before_mid(self, head: ListNode) -> ListNode:
-        slow, fast = head, head
-        while fast and fast.next and fast.next.next:
-            slow, fast = slow.next, fast.next.next
-        return slow
-
-    def merge_lists(self, left: ListNode, right: ListNode) -> ListNode:
-        curr = dummy = ListNode(-1)
-        while left and right:
-            if left.val < right.val:
-                curr.next, left = left, left.next
-            else:
-                curr.next, right = right, right.next
-            curr = curr.next
-        if left:
-            curr.next = left
-        if right:
-            curr.next = right
+        list_length = self.get_len(head)
+        dummy = ListNode(-1)
+        dummy.next = head
+        step = 1
+        while step < list_length:
+            curr = dummy.next
+            tail = dummy
+            while curr:
+                left = curr
+                right = self.split(left, step)
+                curr = self.split(right, step)
+                tail = self.merge(left, right, tail)
+            step <<= 1
         return dummy.next
+
+    def get_len(self, head):
+        list_length, curr = 0, head
+        while curr:
+            list_length += 1
+            curr = curr.next
+        return list_length
+
+    def split(self, head: ListNode, n: int) -> ListNode:
+        i = 1
+        while head and i < n:
+            head = head.next
+            i += 1
+        if not head:
+            return head
+        after = head.next
+        head.next = None
+        return after
+
+    def merge(self, l1: ListNode, l2: ListNode, head: ListNode) -> ListNode:
+        while l1 and l2:
+            if l1.val < l2.val:
+                head.next, l1 = l1, l1.next
+            else:
+                head.next, l2 = l2, l2.next
+            head = head.next
+        head.next = l1 or l2
+        while head.next:
+            head = head.next
+        return head
