442c4c8edabb3c807a4073dbb38466b4052f2044
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-14 17:25:06 +0300

    feat: improve space complexity

--- leetcode/765_couples_holding_hands.py
+++ leetcode/765_couples_holding_hands.py
@@ -4,25 +4,18 @@ from typing import List
 class Solution:
     def minSwapsCouples(self, row: List[int]) -> int:
         N = len(row) // 2
-        unions = N
-        parents = list(range(N))
+        parents = {}
 
-        def find(i):
-            if i == parents[i]:
-                return i
-            parents[i] = find(parents[i])
-            return parents[i]
-
-        def union(s, e):
-            parent_s = find(s)
-            parent_e = find(e)
-            if parent_s != parent_e:
-                nonlocal unions
-                unions -= 1
-                parents[parent_e] = parent_s
+        def union(a, b):
+            min_, max_ = min(a, b), max(a, b)
+            if min_ != max_:
+                if min_ in parents:
+                    union(parents[min_], max_)
+                else:
+                    parents[min_] = max_
 
         for i in range(N):
             a, b = row[2*i], row[2*i + 1]
             union(a//2, b//2)
 
-        return N - unions
+        return len(parents)
