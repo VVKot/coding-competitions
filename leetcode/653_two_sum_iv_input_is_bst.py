f0e4ef7d1563e43834d2f3725785de2f6527bb37
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-24 08:05:00 +0300

    feat: add optimized solution

--- leetcode/653_two_sum_iv_input_is_bst.py
+++ leetcode/653_two_sum_iv_input_is_bst.py
@@ -1,3 +1,6 @@
+from typing import Set
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -6,18 +9,21 @@ class TreeNode:
 
 
 class Solution:
+
     def findTarget(self, root: TreeNode, k: int) -> bool:
-        if not root:
-            return False
+        seen = set()  # type: Set[int]
         stack = [root]
-        seen = set()
         while stack:
             curr = stack.pop()
             if not curr:
                 continue
             if curr.val in seen:
                 return True
-            seen.add(k-curr.val)
-            stack.append(curr.right)
-            stack.append(curr.left)
+            seen.add(k - curr.val)
+            if curr.val / 2 > k:
+                stack.append(curr.right)
+                stack.append(curr.left)
+            else:
+                stack.append(curr.left)
+                stack.append(curr.right)
         return False
