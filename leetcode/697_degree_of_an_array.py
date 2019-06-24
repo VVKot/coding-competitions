821b8cac03c5e054add2b48e752395060a91bddf
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-24 07:08:43 +0300

    feat: add linear time solution

--- leetcode/697_degree_of_an_array.py
+++ leetcode/697_degree_of_an_array.py
@@ -1,25 +1,20 @@
-import collections
-from typing import List
+from typing import List, Dict
 
 
 class Solution:
 
     def findShortestSubArray(self, nums: List[int]) -> int:
-        counts = collections.Counter(nums)
-        max_count = counts.most_common(1)[0][1]
-        sublists_len = []
-        for val, count in counts.most_common():
-            if count != max_count:
-                break
-            sublist_len = self.get_sublist_len(nums, val)
-            sublists_len.append(sublist_len)
-        return min(sublists_len)
-
-    def get_sublist_len(self, nums: List[int], val: int) -> int:
-        first, last = -1, -1
+        counts = {}  # type: Dict[int, List[int]]
         for i, num in enumerate(nums):
-            if num == val:
-                if first == -1:
-                    first = i
-                last = i
-        return last - first + 1
+            if num in counts:
+                counts[num][0] += 1
+                counts[num][2] = i
+            else:
+                counts[num] = [1, i, i]
+        max_count = max(count for count, _, _ in counts.values())
+        sublists_len = []
+        for count, first, last in counts.values():
+            if count == max_count:
+                sublist_len = last - first + 1
+                sublists_len.append(sublist_len)
+        return min(sublists_len)
