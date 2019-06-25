4c5b01168ef2b1b286934e058ad529de99bfeb1b
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-25 06:05:31 +0300

    feat: add optimal solution

--- leetcode/796_rotate_string.py
+++ leetcode/796_rotate_string.py
@@ -1,12 +1,4 @@
 class Solution:
 
     def rotateString(self, A: str, B: str) -> bool:
-        if len(A) != len(B):
-            return False
-        if A == B:
-            return True
-        for i in range(len(A)):
-            rotated = A[i:] + A[:i]
-            if rotated == B:
-                return True
-        return False
+        return len(A) == len(B) and B in A + A
