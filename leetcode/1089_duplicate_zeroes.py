c9c4175171be923f6492b9213b849de4cdfe4f52
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-27 06:15:13 +0300

    feat: add more consice solution

---
+++ leetcode/1089_duplicate_zeroes.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple
+
+
+class Solution:
+
+    def duplicateZeros(self, arr: List[int]) -> None:
+        N = len(arr)
+        shift = arr.count(0)
+        for i in reversed(range(N)):
+            if i + shift < N:
+                arr[i + shift] = arr[i]
+            if not arr[i]:
+                shift -= 1
+                if i + shift < N:
+                    arr[i + shift] = arr[i]
