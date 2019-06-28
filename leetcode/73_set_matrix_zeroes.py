ef9896fa2096f0a2b2e0a7cd65095a4a1600357e
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-28 15:41:09 +0300

    feat: add constant memory solution

--- leetcode/73_set_matrix_zeroes.py
+++ leetcode/73_set_matrix_zeroes.py
@@ -6,14 +6,18 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         H = len(matrix)
         W = len(matrix[0])
-        rows = [1] * H
-        cols = [1] * W
+        col1 = 1
         for y in range(H):
             for x in range(W):
                 if not matrix[y][x]:
-                    rows[y] = 0
-                    cols[x] = 0
-        for y in range(H):
-            for x in range(W):
-                if not rows[y] or not cols[x]:
+                    if x == 0:
+                        col1 = 0
+                    else:
+                        matrix[0][x] = matrix[y][0] = 0
+        for y in reversed(range(H)):
+            for x in reversed(range(W)):
+                if x == 0:
+                    if col1 == 0:
+                        matrix[y][x] = 0
+                elif matrix[0][x] == 0 or matrix[y][0] == 0:
                     matrix[y][x] = 0
