3e3099f8f9de003bc68bd06c26f8d2ce2d6b280c
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-29 14:11:47 +0300

    feat: add optimal solution

--- leetcode/258_add_digits.py
+++ leetcode/258_add_digits.py
@@ -1,10 +1,6 @@
 class Solution:
 
     def addDigits(self, num: int) -> int:
-        while num >= 10:
-            digit_sum = 0
-            while num:
-                num, mod = divmod(num, 10)
-                digit_sum += mod
-            num = digit_sum
-        return num
+        if not num:
+            return 0
+        return num % 9 or 9
