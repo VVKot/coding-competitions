960576620fb3fb431bc37d83f833663f0741588d
VVKot <volodymyr.kot.ua@gmail.com>
2019-06-30 14:18:23 +0300

    docs: clarify variable names

--- leetcode/496_next_greater_element_i.py
+++ leetcode/496_next_greater_element_i.py
@@ -1,16 +1,17 @@
-from typing import List, Dict
+from typing import List
 
 
 class Solution:
+
     def nextGreaterElement(self,
                            nums1: List[int],
                            nums2: List[int]) -> List[int]:
-        greater, stack = {}, []  # type: Dict[int, int], List[int]
+        greater = {num: -1 for num in nums1}
+        stack = []  # type: List[int]
         for num in nums2:
             while stack and stack[-1] < num:
-                small = stack.pop()
-                greater[small] = num
+                smaller = stack.pop()
+                if smaller in greater:
+                    greater[smaller] = num
             stack.append(num)
-        for num in stack:
-            greater[num] = -1
-        return [greater[x] for x in nums1]
+        return [greater[num] for num in nums1]
