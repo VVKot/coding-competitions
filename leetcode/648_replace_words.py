47cf1d6bacb83969550677a84f2b95046f2d4c61
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-02 21:29:47 +0300

    feat: add trie solution

--- leetcode/648_replace_words.py
+++ leetcode/648_replace_words.py
@@ -1,16 +1,39 @@
 from typing import List
 
 
+class RootTrie:
+
+    END = '*'
+
+    def __init__(self, roots: List[str]) -> None:
+        self.trie = dict()
+        for root in roots:
+            curr_trie = self.trie
+            for ch in root:
+                if ch not in curr_trie:
+                    curr_trie[ch] = dict()
+                curr_trie = curr_trie[ch]
+            curr_trie[self.END] = root
+            curr_trie = self.trie
+
+    def get_shortest_root(self, word: str) -> str:
+        default_root = ""
+        curr_trie = self.trie
+        for ch in word:
+            if self.END in curr_trie:
+                return curr_trie[self.END]
+            if ch not in curr_trie:
+                return default_root
+            curr_trie = curr_trie[ch]
+        return curr_trie[self.END] if self.END in curr_trie else default_root
+
+
 class Solution:
 
     def replaceWords(self, roots: List[str], sentence: str) -> str:
-        roots.sort()
+        trie = RootTrie(roots)
         result = []
         for word in sentence.split():
-            for root in roots:
-                if word.startswith(root):
-                    result.append(root)
-                    break
-            else:
-                result.append(word)
+            curr_root = trie.get_shortest_root(word) or word
+            result.append(curr_root)
         return ' '.join(result)
