19337af91c5144ef7f67de03ea1fb48838d21ac6
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-04 21:09:06 +0300

    feat: add optimal solution

--- leetcode/345_reverse_vowels_in_string.py
+++ leetcode/345_reverse_vowels_in_string.py
@@ -2,22 +2,15 @@ class Solution:
 
     def reverseVowels(self, s: str) -> str:
         chars = list(s)
-        char_len = len(chars)
-        left, right = 0, char_len - 1
-        while left < char_len and not self.is_vowel(chars[left]):
-            left += 1
-        while right >= 0 and not self.is_vowel(chars[right]):
-            right -= 1
-
+        left, right = 0, len(chars) - 1
+        vowels = set('aeiouAEIOU')
         while left < right:
-            chars[left], chars[right] = chars[right], chars[left]
-            left += 1
-            right -= 1
-            while left < char_len and not self.is_vowel(chars[left]):
+            if chars[left] in vowels and chars[right] in vowels:
+                chars[left], chars[right] = chars[right], chars[left]
                 left += 1
-            while right >= 0 and not self.is_vowel(chars[right]):
+                right -= 1
+            if not chars[left] in vowels:
+                left += 1
+            if not chars[right] in vowels:
                 right -= 1
         return ''.join(chars)
-
-    def is_vowel(self, ch: str) -> bool:
-        return ch.lower() in {'a', 'e', 'i', 'o', 'u'}
