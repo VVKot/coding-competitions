d80a1db4772e7f3d3314f2938112fbeeba258293
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-05 07:13:46 +0300

    docs: annotate complements map

--- leetcode/1010_pairs_of_songs_with_total_duration_divisible_by_60.py
+++ leetcode/1010_pairs_of_songs_with_total_duration_divisible_by_60.py
@@ -1,12 +1,12 @@
 import collections
-from typing import List
+from typing import List, DefaultDict
 
 
 class Solution:
 
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         pair_count = 0
-        comp = collections.defaultdict(int)
+        comp = collections.defaultdict(int)  # type: DefaultDict[int, int]
         for t in time:
             pair_count += comp[-t % 60]
             comp[t % 60] += 1
