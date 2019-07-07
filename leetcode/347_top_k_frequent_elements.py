710543acdb17b73a6ec4ff12cb65bf186b2c466f
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-07 14:43:13 +0300

    feat: add optimal NlogK solution

--- leetcode/347_top_k_frequent_elements.py
+++ leetcode/347_top_k_frequent_elements.py
@@ -1,7 +1,16 @@
 import collections
-from typing import List
+import heapq
+from typing import List, Tuple
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return list(map(lambda x: x[0], collections.Counter(nums).most_common(k)))
+        counts = collections.Counter(nums)
+        result = []  # type: List[Tuple[int, int]]
+        for val, count in counts.items():
+            item = (count, val)
+            if len(result) == k:
+                heapq.heappushpop(result, item)
+            else:
+                heapq.heappush(result, item)
+        return [val for _, val in result]
