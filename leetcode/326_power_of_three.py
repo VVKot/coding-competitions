c91ef69f78ad376132dbae9de9da3770206d567e
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-08 21:52:27 +0300

    feat: add optimized solution

--- leetcode/326_power_of_three.py
+++ leetcode/326_power_of_three.py
@@ -1,23 +1,9 @@
-import string
-
 
 class Solution:
 
-    digs = string.digits
-
-    def int2base(self, x: int, base: int) -> str:
-        if x == 0:
-            return self.digs[0]
-        digits = []
-        while x:
-            x, mod = divmod(x, base)
-            digits.append(str(mod))
-        digits.reverse()
-        return ''.join(digits)
-
     def isPowerOfThree(self, n: int) -> bool:
-        if n <= 0:
+        if n < 1:
             return False
-        in_thriary_system = self.int2base(n, 3)
-        return in_thriary_system.count('1') == 1 and \
-            in_thriary_system.count('2') == 0
+        while not n % 3:
+            n //= 3
+        return n == 1
