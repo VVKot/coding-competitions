3a5f3f5f3121c2a47aa006c115751220357a62d1
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-09 06:33:44 +0300

    feat: add one path solution

--- leetcode/645_set_mismatch.py
+++ leetcode/645_set_mismatch.py
@@ -1,27 +1,17 @@
-import functools
-import operator
 from typing import List
 
 
 class Solution:
 
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        N = len(nums)
-        total = functools.reduce(operator.xor, nums)
-        total = functools.reduce(operator.xor, range(1, N+1), total)
-        rightmost_set = total & ~(total - 1)
-        xor1 = xor2 = 0
-        for num in nums:
-            if num & rightmost_set:
-                xor1 ^= num
+        missing = duplicated = 0
+        for i, num in enumerate(nums):
+            val = abs(num)
+            duplicated ^= (i+1) ^ val
+            index = val - 1
+            if nums[index] < 0:
+                missing = val
             else:
-                xor2 ^= num
-        for num in range(1, N+1):
-            if num & rightmost_set:
-                xor1 ^= num
-            else:
-                xor2 ^= num
-        for num in nums:
-            if num == xor1:
-                return [xor1, xor2]
-        return [xor2, xor1]
+                nums[index] = -nums[index]
+        duplicated ^= missing
+        return [missing, duplicated]
