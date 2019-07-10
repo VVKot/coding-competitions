999b0d3448a3f1defcb193fcd0d9fb58f730a2b1
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-10 21:46:21 +0300

    feat: add optimal solution

--- leetcode/88_merge_sorted_array.py
+++ leetcode/88_merge_sorted_array.py
@@ -1,3 +1,4 @@
+from sys import maxsize
 from typing import List
 
 
@@ -7,6 +8,20 @@ class Solution:
               nums1: List[int],
               m: int,
               nums2: List[int], n: int) -> None:
-        for i in range(n):
-            nums1[i+m] = nums2[i]
-        nums1[:m+n] = sorted(nums1[:m+n])
+        write_pointer = m + n - 1
+        nums1_pointer = m - 1
+        nums2_pointer = n - 1
+        while nums1_pointer >= 0 or nums2_pointer >= 0:
+            first = nums1[nums1_pointer] \
+                if nums1_pointer >= 0 \
+                else maxsize
+            second = nums2[nums2_pointer] \
+                if nums2_pointer >= 0 \
+                else maxsize
+            if first > second:
+                nums1[write_pointer] = first
+                nums1_pointer -= 1
+            else:
+                nums1[write_pointer] = second
+                nums2_pointer -= 1
+            write_pointer -= 1
