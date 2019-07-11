8f56d06c242d89279a5fe4ffefc3e550d750c5e4
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-11 21:30:06 +0300

    feat: add more consice code

--- leetcode/572_subtree_of_another_tree.py
+++ leetcode/572_subtree_of_another_tree.py
@@ -9,21 +9,15 @@ class TreeNode:
 class Solution:
 
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        if not s or not t:
+        if not s:
             return False
-        if s.val == t.val:
-            is_subtree = self.is_equal(s, t)
-            if is_subtree:
-                return True
-        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        return self.is_equal(s, t) or self.isSubtree(s.left, t) or \
+            self.isSubtree(s.right, t)
 
     def is_equal(self, s: TreeNode, t: TreeNode) -> bool:
         if not s and not t:
             return True
-        if s and t:
-            if s.val != t.val:
-                return False
-            return self.is_equal(s.left, t.left) and \
-                self.is_equal(s.right, t.right)
-        else:
+        if not s or not t:
             return False
+        return s.val == t.val and self.is_equal(s.left, t.left) and \
+            self.is_equal(s.right, t.right)
