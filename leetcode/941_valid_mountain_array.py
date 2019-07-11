19767a822ab754204bddeff243444dd4b75ff31b
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-11 08:07:05 +0300

    feat: add optimal one-pass solution

--- leetcode/941_valid_mountain_array.py
+++ leetcode/941_valid_mountain_array.py
@@ -4,18 +4,15 @@ from typing import List
 class Solution:
 
     def validMountainArray(self, A: List[int]) -> bool:
-        N = len(A)
-        if N < 3:
+        last = len(A) - 1
+        if last < 2:
             return False
-        max_height = max(A)
-        max_index = A.index(max_height)
-        if max_index == 0 or max_index == N-1:
+        peak = 0
+        while peak < last and A[peak] < A[peak+1]:
+            peak += 1
+        if peak == 0 or peak == last:
             return False
-        for i, height in enumerate(A):
-            if i < max_index:
-                if height >= A[i+1]:
-                    return False
-            elif i > max_index:
-                if height >= A[i-1]:
-                    return False
-        return True
+        downhill = peak
+        while downhill < last and A[downhill] > A[downhill+1]:
+            downhill += 1
+        return downhill == last
