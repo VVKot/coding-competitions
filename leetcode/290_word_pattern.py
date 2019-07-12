7140d6829d4dff31d4f4ddb277cc204104712e4b
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-12 06:21:02 +0300

    feat: add solution using only sets

--- leetcode/290_word_pattern.py
+++ leetcode/290_word_pattern.py
@@ -1,21 +1,9 @@
-from typing import Dict, Set
-
-
 class Solution:
 
     def wordPattern(self, pattern: str, sequence: str) -> bool:
         words = sequence.split()
         if len(words) != len(pattern):
             return False
-        mapping = {}  # type: Dict[str, str]
-        used_words = set()  # type: Set[str]
-        for letter, word in zip(pattern, words):
-            if letter in mapping:
-                if mapping[letter] != word:
-                    return False
-            else:
-                if word in used_words:
-                    return False
-                mapping[letter] = word
-                used_words.add(word)
-        return True
+        if len(set(words)) != len(set(pattern)):
+            return False
+        return len(set(zip(words, pattern))) == len(set(pattern))
