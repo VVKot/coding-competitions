db3e8b00e6a426d5f2dda6d12b45fa407745cdda
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-13 08:20:16 +0300

    docs: add typings

--- leetcode/235_lowest_common_ancestor_of_bst.py
+++ leetcode/235_lowest_common_ancestor_of_bst.py
@@ -1,9 +1,21 @@
+class TreeNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
 class Solution:
-    def lowestCommonAncestor(self, root, p, q):
-        small, big = sorted([p.val, q.val])
-        while not small <= root.val <= big:
-            if small > root.val:
-                root = root.right
-            else:
+
+    def lowestCommonAncestor(self,
+                             root: TreeNode,
+                             p: TreeNode,
+                             q: TreeNode) -> TreeNode:
+        smaller, greater = sorted([p.val, q.val])
+        while not smaller <= root.val <= greater:
+            if root.val > greater:
                 root = root.left
+            else:
+                root = root.right
         return root
