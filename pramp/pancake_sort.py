bfc53d1a2476f9e2342c192e71bae5ebbc0fb7bb
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-13 07:36:41 +0300

    feat: add solution for Pancake sort

--- pramp/pancake_sort.py
+++ pramp/pancake_sort.py
@@ -1,12 +1,25 @@
 def flip(arr, k):
-    arr[:k] = reversed(arr[:k])
+    left, right = 0, k-1
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
 
 
 def pancake_sort(arr):
     num_elements = len(arr)
     for i in range(num_elements-1):
-        max_elem = max(arr[:num_elements-i])
-        flip_index = arr.index(max_elem)
+        flip_index = get_max_index_in_prefix(arr, num_elements-i)
         flip(arr, flip_index+1)
         flip(arr, num_elements-i)
     return arr
+
+
+def get_max_index_in_prefix(arr, amount_to_consider):
+    max_elem, max_index = float('-inf'), -1
+    for i in range(amount_to_consider):
+        num = arr[i]
+        if num > max_elem:
+            max_elem = num
+            max_index = i
+    return max_index
