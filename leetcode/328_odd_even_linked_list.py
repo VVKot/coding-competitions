3f34c735cb9563092501acb3b811ca8cdec99484
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-13 20:57:04 +0300

    refactor: add more intuitive solution

--- leetcode/328_odd_even_linked_list.py
+++ leetcode/328_odd_even_linked_list.py
@@ -10,15 +10,10 @@ class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
         if not head:
             return head
-        event_start = head.next
-        curr = odd_end = head
-        index = 1
-        while curr and curr.next:
-            if index & 1:
-                odd_end = curr.next.next or curr
-            index += 1
-            next_node = curr.next
-            curr.next = curr.next.next
-            curr = next_node
-        odd_end.next = event_start
+        even_head = head.next
+        odd, even = head, head.next
+        while even and even.next:
+            odd.next, odd = even.next, even.next
+            even.next, even = odd.next, odd.next
+        odd.next = even_head
         return head
