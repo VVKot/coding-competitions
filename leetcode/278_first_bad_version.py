d91156aa49c01b8a9b0255a52a56abc7a6ea6052
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-19 19:17:27 +0300

    feat: add solution for 278

---
+++ leetcode/278_first_bad_version.py
@@ -0,0 +1,15 @@
+def isBadVersion(n: int) -> bool:
+    return True  # mock, proper implementation is defined by LC
+
+
+class Solution:
+
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+        while left != right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
