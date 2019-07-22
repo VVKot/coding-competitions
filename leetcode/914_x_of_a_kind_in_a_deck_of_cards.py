4919ec6e0a4e0809bcceec2a1dc329bbb68dbf3c
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-22 21:13:18 +0300

    feat: add optimal one-liner

--- leetcode/914_x_of_a_kind_in_a_deck_of_cards.py
+++ leetcode/914_x_of_a_kind_in_a_deck_of_cards.py
@@ -1,18 +1,10 @@
-import collections
+from collections import Counter
+from functools import reduce
+from fractions import gcd
 from typing import List
 
 
 class Solution:
 
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        if len(deck) < 2:
-            return False
-        card_counts = collections.Counter(deck)
-        min_count = min(card_counts.values())
-        for group_count in range(2, min_count+1):
-            for count in card_counts.values():
-                if count % group_count != 0:
-                    break
-            else:
-                return True
-        return False
+        return reduce(gcd, Counter(deck).values()) >= 2
