4e7f28f3a41221c9d5dc49db75c95c03a52f7d5e
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-28 18:06:16 +0300

    feat: add optimal log time solution

--- leetcode/343_integer_break.py
+++ leetcode/343_integer_break.py
@@ -1,10 +1,13 @@
 class Solution:
 
     def integerBreak(self, n: int) -> int:
-        dp = [0] * (n+1)
-        dp[1] = 1
-        for i in range(2, n+1):
-            for j in range(1, i):
-                max_prev_product = max(dp[i-j], i-j)
-                dp[i] = max(dp[i], max_prev_product * j)
-        return dp[n]
+        if n == 2:
+            return 1
+        elif n == 3:
+            return 2
+        elif not n % 3:
+            return 3 ** (n//3)
+        elif n % 3 == 1:
+            return 2 * 2 * 3 ** ((n-4)//3)
+        else:
+            return 2 * 3 ** ((n-2)//3)
