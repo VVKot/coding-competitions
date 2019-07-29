a0b46bb0d512f581495607b98f942a71b55b7d57
VVKot <volodymyr.kot.ua@gmail.com>
2019-07-29 21:18:34 +0300

    feat: add smarter merge

--- leetcode/763_partition_labels.py
+++ leetcode/763_partition_labels.py
@@ -1,24 +1,15 @@
-from typing import Dict, List
+from typing import List
 
 
 class Solution:
 
     def partitionLabels(self, S: str) -> List[int]:
-        positions = {}  # type: Dict[str, List[int]]
+        result = []
+        last = {ch: i for i, ch in enumerate(S)}
+        start = j = 0
         for i, ch in enumerate(S):
-            if ch in positions:
-                positions[ch][1] = i
-            else:
-                positions[ch] = [i, i]
-        intervals = list(sorted(positions.values()))
-        i, j, N = 0, 0, len(intervals)
-        result_intervals = []
-        while i < N:
-            start, end = intervals[i]
-            j += 1
-            while j < N and end > intervals[j][0]:
-                end = max(end, intervals[j][1])
-                j += 1
-            result_intervals.append(end-start+1)
-            i = j
-        return result_intervals
+            j = max(j, last[ch])
+            if i == j:
+                result.append(i-start+1)
+                start = i + 1
+        return result
