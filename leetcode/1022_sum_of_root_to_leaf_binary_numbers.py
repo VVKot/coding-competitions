11cae3f15f57c6e6c9420540785904504d9d4acd
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-02 20:59:06 +0300

    feat: don't store all paths

--- leetcode/1022_sum_of_root_to_leaf_binary_numbers.py
+++ leetcode/1022_sum_of_root_to_leaf_binary_numbers.py
@@ -9,15 +9,15 @@ class TreeNode:
 class Solution:
 
     def sumRootToLeaf(self, root: TreeNode) -> int:
-        paths = []
+        self.result = 0
+        self.process_all_paths(root, "")
+        return self.result
 
-        def process_all_paths(node, path):
-            if node:
-                path += str(node.val)
-                if not node.right and not node.left:
-                    paths.append(path)
-                else:
-                    process_all_paths(node.left, path)
-                    process_all_paths(node.right, path)
-        process_all_paths(root, "")
-        return sum(int(path, 2) for path in paths) if paths else 0
+    def process_all_paths(self, node: TreeNode, path: str) -> None:
+        if node:
+            path += str(node.val)
+            if not node.right and not node.left:
+                self.result += int(path, 2)
+            else:
+                self.process_all_paths(node.left, path)
+                self.process_all_paths(node.right, path)
