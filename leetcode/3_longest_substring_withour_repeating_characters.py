f2f94e3f5bc83694360bc4752a38e2f0b24b315b
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-04 18:06:01 +0300

    feat: use set instead of dictionary

--- leetcode/3_longest_substring_withour_repeating_characters.py
+++ leetcode/3_longest_substring_withour_repeating_characters.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Set
 
 
 class Solution:
@@ -6,12 +6,12 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         result = 0
         left = 0
-        win_char = {}  # type: Dict[str, int]
-        for right, curr in enumerate(s):
-            if curr in win_char:
-                prev_occ = win_char[curr]
-                if prev_occ >= left:
-                    left = prev_occ + 1
-            win_char[curr] = right
+        substring = set()  # type: Set[str]
+        for right, ch in enumerate(s):
+            if ch in substring:
+                while ch in substring:
+                    substring.remove(s[left])
+                    left += 1
+            substring.add(ch)
             result = max(result, right-left+1)
         return result
