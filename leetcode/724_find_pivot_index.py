748643083f6b0dfa6637a808fe7deb43e16714f1
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-05 21:21:43 +0300

    feat: add optimal solution using constant memory

--- leetcode/724_find_pivot_index.py
+++ leetcode/724_find_pivot_index.py
@@ -4,13 +4,11 @@ from typing import List
 class Solution:
 
     def pivotIndex(self, nums: List[int]) -> int:
-        prefix = [0]
+        left = 0
+        right = sum(nums)
         for i, num in enumerate(nums):
-            prefix.append(prefix[-1] + num)
-        N = len(nums)
-        for i, num in enumerate(nums):
-            before = prefix[i]
-            after = prefix[N] - prefix[i+1]
-            if before == after:
+            right -= num
+            if left == right:
                 return i
+            left += num
         return -1
