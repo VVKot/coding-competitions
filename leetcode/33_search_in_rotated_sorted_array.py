0ef5e54cb2b4afb5ae64470bd044ac3eeae76aa1
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-06 21:39:17 +0300

    docs: add typings, clarify variable names

--- leetcode/33_search_in_rotated_sorted_array.py
+++ leetcode/33_search_in_rotated_sorted_array.py
@@ -5,13 +5,13 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if not nums:
             return -1
-        max_index = self.get_max_index(nums, target)
+        max_index = self.get_pivot(nums, target)
         if nums[0] <= target <= nums[max_index]:
             return self.binary_search(nums, 0, max_index, target)
         else:
             return self.binary_search(nums, max_index+1, len(nums)-1, target)
 
-    def get_max_index(self, nums, target):
+    def get_pivot(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
         while left != right:
             mid = (left+right) // 2
@@ -21,7 +21,11 @@ class Solution:
                 right = mid
         return left
 
-    def binary_search(self, nums, left, right, target):
+    def binary_search(self,
+                      nums: List[int],
+                      left: int,
+                      right: int,
+                      target: int) -> int:
         while left <= right:
             mid = (left+right) // 2
             if nums[mid] == target:
