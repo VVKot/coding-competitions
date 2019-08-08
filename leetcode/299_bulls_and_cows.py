6b9c51998df83398d5b8474e3566399c3a9b07bc
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-08 05:52:01 +0300

    feat: add optimal solution in one path

--- leetcode/299_bulls_and_cows.py
+++ leetcode/299_bulls_and_cows.py
@@ -5,15 +5,16 @@ from typing import Counter
 class Solution:
 
     def getHint(self, secret: str, guess: str) -> str:
-        bulls = 0
-        secret_count = collections.Counter()  # type: Counter[str]
-        guess_count = collections.Counter()  # type: Counter[str]
+        bulls = cows = 0
+        seen = collections.Counter()  # type: Counter[str]
         for s, g in zip(secret, guess):
             if s == g:
                 bulls += 1
             else:
-                secret_count[s] += 1
-                guess_count[g] += 1
-        common_digits_count = secret_count & guess_count
-        cows = sum(common_digits_count.values())
+                if seen[s] < 0:
+                    cows += 1
+                if seen[g] > 0:
+                    cows += 1
+                seen[s] += 1
+                seen[g] -= 1
         return "{0}A{1}B".format(bulls, cows)
