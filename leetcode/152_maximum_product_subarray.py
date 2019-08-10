09e56997559ff676962549aad9226861613e3036
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-10 17:01:28 +0300

    refactor: clarify variable names

--- leetcode/152_maximum_product_subarray.py
+++ leetcode/152_maximum_product_subarray.py
@@ -4,12 +4,12 @@ from typing import List
 class Solution:
 
     def maxProduct(self, nums: List[int]) -> int:
-        pmax = pmin = res = nums[0]
+        overall_max = curr_max = curr_min = nums[0]
         for i in range(1, len(nums)):
             num = nums[i]
             if num < 0:
-                pmax, pmin = pmin, pmax
-            pmax = max(num, pmax * num)
-            pmin = min(num, pmin * num)
-            res = max(res, pmax)
-        return res
+                curr_max, curr_min = curr_min, curr_max
+            curr_max = max(num, curr_max * num)
+            curr_min = min(num, curr_min * num)
+            overall_max = max(overall_max, curr_max)
+        return overall_max
