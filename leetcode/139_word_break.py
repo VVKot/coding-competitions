cf501e353570255497d3678f92bdb9a00e588372
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-10 17:09:45 +0300

    feat: add memoized backtracking solution

--- leetcode/139_word_break.py
+++ leetcode/139_word_break.py
@@ -1,26 +1,26 @@
-from typing import Dict
+from typing import Dict, List
 
 
 class Solution:
 
-    def word_break_rec(self, word):
-        if not word:
-            return True
-        if word in self.dp:
-            return self.dp[word]
-        N = len(word)
-        for i in range(N):
-            curr = word[0:i+1]
-            if curr in self.word_set:
-                suffix = word[i+1:]
-                result = self.word_break_rec(suffix)
-                if suffix not in self.dp:
-                    self.dp[suffix] = result
-                if result:
-                    return True
-        return False
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words = set(wordDict)
+        N = len(s)
+        dp = {}  # type: Dict[int, bool]
 
-    def wordBreak(self, s, wordDict):
-        self.word_set = set(wordDict)
-        self.dp = {}  # type: Dict[str, bool]
-        return self.word_break_rec(s)
+        def can_construct(start):
+            if start == N:
+                return True
+            if start in dp:
+                return dp[start]
+            for i in range(start, N):
+                curr_word = s[start:i+1]
+                if curr_word in words:
+                    result = can_construct(i+1)
+                    if result:
+                        dp[start] = True
+                        return dp[start]
+            dp[start] = False
+            return dp[start]
+
+        return can_construct(0)
