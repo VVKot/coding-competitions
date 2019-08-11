123f664e70fa7d915a99c86a6df0570fa6030f6d
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-11 05:19:25 +0300

    feat: add optimized solution for 1150

--- leetcode/1150_check_if_number_is_majority_element_in_a_sorted_array.py
+++ leetcode/1150_check_if_number_is_majority_element_in_a_sorted_array.py
@@ -4,4 +4,10 @@ from typing import List
 class Solution:
 
     def isMajorityElement(self, nums: List[int], target: int) -> bool:
-        return nums.count(target) > len(nums) // 2
+        target_count = 0
+        for num in nums:
+            if num == target:
+                target_count += 1
+            elif num > target:
+                break
+        return target_count > len(nums) // 2
