6f1ff3d58836237d185acaec988bf9935828050b
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-11 15:21:58 +0300

    feat: improve time and space complexity to linear

--- leetcode/105_construct_binary_tree_from_inorder_and_preorder_traversal.py
+++ leetcode/105_construct_binary_tree_from_inorder_and_preorder_traversal.py
@@ -13,10 +13,26 @@ class Solution:
     def buildTree(self,
                   preorder: List[int],
                   inorder: List[int]) -> Optional[TreeNode]:
-        if inorder:
-            ind = inorder.index(preorder.pop(0))
-            root = TreeNode(inorder[ind])
-            root.left = self.buildTree(preorder, inorder[0:ind])
-            root.right = self.buildTree(preorder, inorder[ind+1:])
-            return root
-        return None
+        if not preorder:
+            return None
+        self.in_indices = {num: i for i, num in enumerate(inorder)}
+        self.pre_index = 0
+        return self.build_rec(0, len(preorder) - 1, preorder)
+
+    def build_rec(self,
+                  left: int,
+                  right: int,
+                  preorder: List[int]) -> TreeNode:
+        val = preorder[self.pre_index]
+        node = TreeNode(val)
+        if left == right:
+            return node
+        inorder_index = self.in_indices[val]
+        if left != inorder_index:
+            self.pre_index += 1
+            node.left = self.build_rec(left, inorder_index-1, preorder)
+
+        if right != inorder_index:
+            self.pre_index += 1
+            node.right = self.build_rec(inorder_index+1, right, preorder)
+        return node
