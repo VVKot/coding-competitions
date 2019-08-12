bba7abd0a9f8bd5eda8eaf3c7b56162d79413711
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-12 21:41:42 +0300

    feat: add solution for 18

---
+++ leetcode/18_4sum.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+        def findNsum(l: int,
+                     r: int,
+                     target: int,
+                     N: int,
+                     result: List[int],
+                     results: List[List[int]]):
+            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:
+                return
+            if N == 2:
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:
+                for i in range(l, r+1):
+                    if i == l or (i > l and nums[i-1] != nums[i]):
+                        findNsum(i+1, r, target -
+                                 nums[i], N-1, result+[nums[i]], results)
+
+        nums.sort()
+        four_sums = []  # type: List[List[int]]
+        findNsum(0, len(nums)-1, target, 4, [], four_sums)
+        return four_sums
