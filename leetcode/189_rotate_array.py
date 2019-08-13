4d2c17e78a7b512be7513210264a57721cf675b7
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-13 06:05:41 +0300

    feat: add solution using reverse

--- leetcode/189_rotate_array.py
+++ leetcode/189_rotate_array.py
@@ -4,5 +4,14 @@ from typing import List
 class Solution:
 
     def rotate(self, nums: List[int], k: int) -> None:
-        k %= len(nums)
-        nums[k:], nums[:k] = nums[:-k], nums[-k:]
+        N = len(nums)
+        k %= N
+        self.reverse(nums, 0, N-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, N-1)
+
+    def reverse(self, nums: List, left: int, right: int) -> None:
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
