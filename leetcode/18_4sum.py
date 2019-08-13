29bf1c6e85138dc143d0fd16780412dae8e04482
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-13 06:05:05 +0300

    refactor: clarify variable names

--- leetcode/18_4sum.py
+++ leetcode/18_4sum.py
@@ -5,33 +5,35 @@ class Solution:
 
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
 
-        def findNsum(l: int,
-                     r: int,
+        def find_n_sum(left: int,
+                     right: int,
                      target: int,
                      N: int,
                      result: List[int],
                      results: List[List[int]]):
-            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:
+            if right-left+1 < N or N < 2:
+                return
+            if target < nums[left]*N or target > nums[right]*N:
                 return
             if N == 2:
-                while l < r:
-                    s = nums[l] + nums[r]
+                while left < right:
+                    s = nums[left] + nums[right]
                     if s == target:
-                        results.append(result + [nums[l], nums[r]])
-                        l += 1
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1
+                        results.append(result + [nums[left], nums[right]])
+                        left += 1
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1
                     elif s < target:
-                        l += 1
+                        left += 1
                     else:
-                        r -= 1
+                        right -= 1
             else:
-                for i in range(l, r+1):
-                    if i == l or (i > l and nums[i-1] != nums[i]):
-                        findNsum(i+1, r, target -
+                for i in range(left, right+1):
+                    if i == left or (i > left and nums[i-1] != nums[i]):
+                        find_n_sum(i+1, right, target -
                                  nums[i], N-1, result+[nums[i]], results)
 
         nums.sort()
         four_sums = []  # type: List[List[int]]
-        findNsum(0, len(nums)-1, target, 4, [], four_sums)
+        find_n_sum(0, len(nums)-1, target, 4, [], four_sums)
         return four_sums
