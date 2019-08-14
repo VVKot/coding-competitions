63191447ddfe06eccaf0bee80565d92829e7cfca
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-14 20:53:21 +0300

    refactor: add solution with better space complexity

--- leetcode/530_minimum_absolute_distance_in_bst.py
+++ leetcode/530_minimum_absolute_distance_in_bst.py
@@ -1,3 +1,7 @@
+from sys import maxsize as maxint
+from typing import List
+
+
 class TreeNode:
 
     def __init__(self, x):
@@ -9,13 +13,16 @@ class TreeNode:
 class Solution:
 
     def getMinimumDifference(self, root: TreeNode) -> int:
-        values = []
-
-        def inorder(node: TreeNode) -> None:
-            if node.left:
-                inorder(node.left)
-            values.append(node.val)
-            if node.right:
-                inorder(node.right)
-        inorder(root)
-        return min(abs(a - b) for a, b in zip(values, values[1:]))
+        stack = []  # type: List[TreeNode]
+        prev = -maxint
+        min_diff = maxint
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            curr = stack.pop()
+            min_diff = min(min_diff, curr.val-prev)
+            prev = curr.val
+            if curr.right:
+                root = curr.right
+        return min_diff
