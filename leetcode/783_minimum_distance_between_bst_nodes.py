da98a74e1cdabd01e299259a6cef78863c3e7cea
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-14 20:51:05 +0300

    feat: add solution for 783

---
+++ leetcode/783_minimum_distance_between_bst_nodes.py
@@ -0,0 +1,28 @@
+from sys import maxsize as maxint
+from typing import List
+
+
+class TreeNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def minDiffInBST(self, root: TreeNode) -> int:
+        stack = []  # type: List[TreeNode]
+        prev = -maxint
+        min_diff = maxint
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            curr = stack.pop()
+            min_diff = min(min_diff, curr.val-prev)
+            prev = curr.val
+            if curr.right:
+                root = curr.right
+        return min_diff
