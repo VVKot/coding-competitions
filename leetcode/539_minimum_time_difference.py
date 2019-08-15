23a419f4b34509b6c9ac999690c11848b4ffc10a
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-15 20:29:46 +0300

    feat: improve space complexity

--- leetcode/539_minimum_time_difference.py
+++ leetcode/539_minimum_time_difference.py
@@ -1,5 +1,4 @@
-import sys
-from typing import List
+from typing import List, Set
 
 
 class Solution:
@@ -8,27 +7,27 @@ class Solution:
     HOURS_IN_DAY = 24
     MINS_IN_DAY = HOURS_IN_DAY * MINS_IN_HOUR
 
-    def get_mins(self, time: str) -> int:
-        hours, mins = time.split(":")
-        return int(hours) * self.MINS_IN_HOUR + int(mins)
-
     def findMinDifference(self, times: List[str]) -> int:
-        all_mins = [0] * self.MINS_IN_DAY
-        for time in times:
-            mins = self.get_mins(time)
-            all_mins[mins] += 1
-        result = sys.maxsize
-        prev, first = -1, -1
-        for i in range(self.MINS_IN_DAY):
-            num_of_times = all_mins[i]
-            if num_of_times == 1:
-                if prev == -1:
-                    prev, first = i, i
-                else:
-                    result = min(result, i-prev)
-                    prev = i
-            elif num_of_times > 1:
+        timestamps_as_minutes = set()  # type: Set[int]
+        for timestamp in times:
+            mins = self.get_mins(timestamp)
+            if mins in timestamps_as_minutes:
                 return 0
+            timestamps_as_minutes.add(mins)
+        result = self.MINS_IN_DAY
+        prev, first = -1, -1
+        for time in range(self.MINS_IN_DAY):
+            if time not in timestamps_as_minutes:
+                continue
+            if prev == -1:
+                prev = first = time
+            else:
+                result = min(result, time-prev)
+                prev = time
         over_midnight = self.MINS_IN_DAY - prev + first
         result = min(result, over_midnight)
         return result
+
+    def get_mins(self, timestamp: str) -> int:
+        hours, mins = map(int, timestamp.split(":"))
+        return hours * self.MINS_IN_HOUR + mins
