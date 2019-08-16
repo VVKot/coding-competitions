a456cddab6e29d0119a1daae1bf0cae5cb161e1d
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-16 06:25:15 +0300

    feat: add solution without division

--- leetcode/238_product_of_array_except_self.py
+++ leetcode/238_product_of_array_except_self.py
@@ -1,5 +1,3 @@
-import operator
-import functools
 from typing import List
 
 
@@ -7,15 +5,13 @@ class Solution:
 
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         N = len(nums)
-        zero_count = nums.count(0)
-        if zero_count > 1:
-            return [0] * N
-        elif zero_count == 1:
-            total = 1
-            for num in nums:
-                if num != 0:
-                    total *= num
-            return [total if num == 0 else 0 for num in nums]
-        else:
-            total = functools.reduce(operator.mul, nums)
-            return [total//num for num in nums]
+        products = [1] * N
+        curr = nums[0]
+        for i in range(1, N):
+            products[i] *= curr
+            curr *= nums[i]
+        curr = nums[-1]
+        for i in reversed(range(0, N-1)):
+            products[i] *= curr
+            curr *= nums[i]
+        return products
