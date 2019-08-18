00f33c5e5a0c04296f6c6987fbf19c3f67b38d84
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-18 17:48:08 +0300

    refactor: simplify code

--- leetcode/24_swap_nodes_in_pairs.py
+++ leetcode/24_swap_nodes_in_pairs.py
@@ -1,17 +1,17 @@
-# Definition for singly-linked list.
 class ListNode:
+
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
 class Solution:
+
     def swapPairs(self, head: ListNode) -> ListNode:
-        # prev a b b.next
-        prev, prev.next = self, head
-        while prev.next and prev.next.next:
-            first = prev.next
-            second = first.next
-            prev.next, first.next, second.next = second, second.next, first
-            prev = first
-        return self.next
+        dummy = prev = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            prev.next = head.next
+            head.next.next, head.next = head, head.next.next
+            prev, head = head, head.next
+        return dummy.next
