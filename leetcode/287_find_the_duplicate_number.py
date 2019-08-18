7b2b4cfc2d44677cfa0223cb35983f6b9c0a9e81
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-18 13:06:27 +0300

    reafactor: clarify variable names

--- leetcode/287_find_the_duplicate_number.py
+++ leetcode/287_find_the_duplicate_number.py
@@ -2,14 +2,15 @@ from typing import List
 
 
 class Solution:
+
     def findDuplicate(self, nums: List[int]) -> int:
         slow = nums[0]
         fast = nums[nums[0]]
         while slow != fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
-        fast = 0
-        while slow != fast:
+        start = 0
+        while start != slow:
             slow = nums[slow]
-            fast = nums[fast]
-        return fast
+            start = nums[start]
+        return slow
