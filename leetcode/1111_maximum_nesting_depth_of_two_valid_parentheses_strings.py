70ef81e9f7fac3c99e0f97d172c7039aadefc66e
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-19 21:45:40 +0300

    feat: add pythonic solution

--- leetcode/1111_maximum_nesting_depth_of_two_valid_parentheses_strings.py
+++ leetcode/1111_maximum_nesting_depth_of_two_valid_parentheses_strings.py
@@ -4,27 +4,4 @@ from typing import List
 class Solution:
 
     def maxDepthAfterSplit(self, seq: str) -> List[int]:
-        half_depth = self.get_max_depth(seq) // 2
-        depth = 0
-        split = [0] * len(seq)
-        for i, ch in enumerate(seq):
-            if ch == '(':
-                depth += 1
-                if depth > half_depth:
-                    split[i] = 1
-            else:
-                if depth > half_depth:
-                    split[i] = 1
-                depth -= 1
-        return split
-
-    def get_max_depth(self, seq: str) -> int:
-        depth = 0
-        max_depth = 0
-        for ch in seq:
-            if ch == '(':
-                depth += 1
-                max_depth = max(max_depth, depth)
-            else:
-                depth -= 1
-        return max_depth
+        return [i & 1 ^ (ch == '(') for i, ch in enumerate(seq)]
