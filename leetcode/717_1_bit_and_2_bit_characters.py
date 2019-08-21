3b09e054c0c3e1197d696d0ca8ae814b34179d33
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-21 21:46:09 +0300

    refactor: simplify approach, remove redundant variable

--- leetcode/717_1_bit_and_2_bit_characters.py
+++ leetcode/717_1_bit_and_2_bit_characters.py
@@ -4,13 +4,7 @@ from typing import List
 class Solution:
 
     def isOneBitCharacter(self, bits: List[int]) -> bool:
-        last = False
         i, N = 0, len(bits)
-        while i < N:
-            if bits[i] == 1:
-                i += 2
-                last = False
-            else:
-                i += 1
-                last = True
-        return last
+        while i < N-1:
+            i += 1 + bits[i]
+        return i == N-1
