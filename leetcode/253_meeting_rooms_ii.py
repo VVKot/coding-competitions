44bc7bdc1fc17ac86364db95c08e08d49e555eee
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-23 06:30:14 +0300

    feat: add solution without heap

--- leetcode/253_meeting_rooms_ii.py
+++ leetcode/253_meeting_rooms_ii.py
@@ -1,16 +1,21 @@
-import heapq
 from typing import List
 
 
 class Solution:
 
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        total_room_count = 0
         if not intervals:
             return 0
-        intervals.sort()
-        meeting_ends = []  # type: List[int]
-        for start, end in intervals:
-            if meeting_ends and meeting_ends[0] <= start:
-                heapq.heappop(meeting_ends)
-            heapq.heappush(meeting_ends, end)
-        return len(meeting_ends)
+        N = len(intervals)
+        starts = sorted(start for start, _ in intervals)
+        ends = sorted(end for _, end in intervals)
+        start_pointer = end_pointer = 0
+        while start_pointer < N:
+            if starts[start_pointer] >= ends[end_pointer]:
+                total_room_count -= 1
+                end_pointer += 1
+
+            start_pointer += 1
+            total_room_count += 1
+        return total_room_count
