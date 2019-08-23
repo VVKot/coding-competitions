1c8e565979fa0fb5ec001259a536899df14bf071
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-23 06:09:52 +0300

    refactor: optimize solution using counter

--- leetcode/253_meeting_rooms_ii.py
+++ leetcode/253_meeting_rooms_ii.py
@@ -1,5 +1,5 @@
-from typing import List
-from sys import maxsize as maxint
+import collections
+from typing import Counter, List
 
 
 class Solution:
@@ -8,17 +8,12 @@ class Solution:
         total_room_count = 0
         if not intervals:
             return total_room_count
-        min_start = maxint
-        max_end = -maxint
+        room_deltas = collections.Counter()  # type: Counter[int]
         for start, end in intervals:
-            min_start = min(min_start, start)
-            max_end = max(max_end, end)
-        currently_booked_rooms = [0] * (max_end - min_start + 1)
-        for start, end in intervals:
-            currently_booked_rooms[start-min_start] += 1
-            currently_booked_rooms[end-min_start] -= 1
+            room_deltas[start] += 1
+            room_deltas[end] -= 1
         running_total = 0
-        for room_count_delta in currently_booked_rooms:
+        for timestamp in sorted(room_deltas):
             total_room_count = max(total_room_count, running_total)
-            running_total += room_count_delta
+            running_total += room_deltas[timestamp]
         return total_room_count
