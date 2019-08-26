ffbb9cde77bdce88457e8aa10aa09fe0d01bcbb9
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-26 06:41:21 +0300

    feat: add more optimal solution using library functions

--- leetcode/1170_compare_strings_by_frequency_of_the_smallest_character.py
+++ leetcode/1170_compare_strings_by_frequency_of_the_smallest_character.py
@@ -7,19 +7,9 @@ class Solution:
     def numSmallerByFrequency(self,
                               queries: List[str],
                               words: List[str]) -> List[int]:
-        words_frequency = [self.get_word_frequency(w) for w in words]
-        words_frequency.sort()
-        N = len(words_frequency)
-        smaller_than_num_words = []
-        for query in queries:
-            query_frequency = self.get_word_frequency(query)
-            index = bisect.bisect(words_frequency, query_frequency)
-            smaller_than_num_words.append(N-index)
-        return smaller_than_num_words
+        words_frequency = sorted(self.get_freq(w) for w in words)
+        return [len(words) - bisect.bisect(words_frequency, self.get_freq(q))
+                for q in queries]
 
-    def get_word_frequency(self, seq):
-        N = len(seq)
-        smallest = seq[0]
-        for i in range(1, N):
-            smallest = min(smallest, seq[i])
-        return seq.count(smallest)
+    def get_freq(self, seq):
+        return seq.count(min(seq))
