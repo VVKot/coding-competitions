c86c9a22a39c34b6147d9dadcbd9271482af898d
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-27 05:57:40 +0300

    feat: add optimization for the case where node has children

--- leetcode/285_inorder_successor_in_bst.py
+++ leetcode/285_inorder_successor_in_bst.py
@@ -12,6 +12,12 @@ class TreeNode:
 class Solution:
 
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> TreeNode:
+        if p.right:
+            p = p.right
+            while p.left:
+                p = p.left
+            return p
+
         stack = []  # type: List[TreeNode]
         prev = float('-inf')
         while stack or root:
