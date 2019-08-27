637436c4e3ade3963dece0e8fada7b40b627bc49
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-27 08:06:20 +0300

    feat: add BFS solution

--- leetcode/200_number_of_islands.py
+++ leetcode/200_number_of_islands.py
@@ -1,28 +1,38 @@
+import collections
+from typing import List, Tuple
+
+
 class Solution:
-    def get_adj_nodes(self, grid, r, c, y, x):
-        adj = []
-        for yy, xx in ((y+1, x), (y-1, x), (y, x+1), (y, x-1)):
-            if 0 <= yy < r and 0 <= xx < c and grid[yy][xx] == '1':
-                adj.append((yy, xx))
-        return adj
 
-    def dfs(self, grid, r, c, y, x):
-        stack = [(y, x)]
-        while stack:
-            curr_y, curr_x = stack.pop()
-            grid[curr_y][curr_x] = "*"
-            adj_nodes = self.get_adj_nodes(grid, r, c, curr_y, curr_x)
-            stack.extend(adj_nodes)
+    WATER, LAND = '0', '1'
 
-    def numIslands(self, grid):
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not any(grid):
             return 0
-        rows = len(grid)
-        columns = len(grid[0])
+        self.grid = grid
         result = 0
-        for y in range(rows):
-            for x in range(columns):
-                if grid[y][x] == '1':
-                    self.dfs(grid, rows, columns, y, x)
+        for y, row in enumerate(grid):
+            for x, mark in enumerate(row):
+                if mark == self.LAND:
+                    self.bfs(y, x)
                     result += 1
         return result
+
+    def bfs(self, y: int, x: int) -> None:
+        self.grid[y][x] = self.WATER
+        q = collections.deque([(y, x)])
+        while q:
+            curr_y, curr_x = q.popleft()
+            for yy, xx in self.get_adj_nodes(curr_y, curr_x):
+                self.grid[yy][xx] = self.WATER
+                q.append((yy, xx))
+
+    def get_adj_nodes(self, y: int, x: int) -> List[Tuple[int, int]]:
+        r = len(self.grid)
+        c = len(self.grid[0])
+        adj = []
+        for yy, xx in ((y+1, x), (y-1, x), (y, x+1), (y, x-1)):
+            if 0 <= yy < r and 0 <= xx < c:
+                if self.grid[yy][xx] == self.LAND:
+                    adj.append((yy, xx))
+        return adj
