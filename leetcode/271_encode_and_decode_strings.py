2c8a16a00cd235449fcab980a80f0229e0573462
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-27 06:36:54 +0300

    feat: add compression solution

--- leetcode/271_encode_and_decode_strings.py
+++ leetcode/271_encode_and_decode_strings.py
@@ -1,21 +1,21 @@
 class Codec:
 
     def encode(self, strs):
-        """Encodes a list of strings to a single string.
-
-        :type strs: List[str]
-        :rtype: str
-        """
-        if not strs:
-            return chr(258)
-        return chr(257).join(strs)
+        return ''.join(self.len_to_str(len(s)) + s.encode('utf-8')
+                       for s in strs)
 
     def decode(self, s):
-        """Decodes a single string to a list of strings.
+        strs = []
+        i, N = 0, len(s)
+        while i < N:
+            curr_len = self.str_to_len(s[i:i+4])
+            i += 4
+            strs.append(s[i:i+curr_len])
+            i += curr_len
+        return strs
 
-        :type s: str
-        :rtype: List[str]
-        """
-        if s == chr(258):
-            return []
-        return s.split(chr(257))
+    def len_to_str(self, val):
+        return format(val, '04d')
+
+    def str_to_len(self, s):
+        return int(s)
