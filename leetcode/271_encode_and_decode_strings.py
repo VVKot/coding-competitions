8e1612584a3b840b0870cecd1b31a010403b9a80
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-27 06:15:58 +0300

    feat: add trivial solution for 271

---
+++ leetcode/271_encode_and_decode_strings.py
@@ -0,0 +1,21 @@
+class Codec:
+
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
+
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return chr(258)
+        return chr(257).join(strs)
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+
+        :type s: str
+        :rtype: List[str]
+        """
+        if s == chr(258):
+            return []
+        return s.split(chr(257))
