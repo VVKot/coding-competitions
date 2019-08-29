85f9c139e3a3c693401c24b8f9491492384147e2
VVKot <volodymyr.kot.ua@gmail.com>
2019-08-29 05:21:11 +0300

    feat: optimize using binary search

--- leetcode/1064_fixed_point.py
+++ leetcode/1064_fixed_point.py
@@ -1,10 +1,13 @@
+import bisect
 from typing import List
 
 
 class Solution:
 
     def fixedPoint(self, A: List[int]) -> int:
-        for i, num in enumerate(A):
-            if i == num:
+        i = bisect.bisect(A, 0) - 1
+        while i < len(A):
+            if i == A[i]:
                 return i
+            i += 1
         return -1
