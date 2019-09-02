1bd01c8924dd2be33193348a330052db9731e8df
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-02 06:20:10 +0300

    feat: improve time complexity using sieve of Eratosthenes

--- leetcode/1175_prime_arrangements.py
+++ leetcode/1175_prime_arrangements.py
@@ -6,15 +6,18 @@ class Solution:
     MAX_NUM = 10**9 + 7
 
     def numPrimeArrangements(self, n: int) -> int:
-        prime_count = 0
-        for i in range(2, n+1):
-            if self.is_prime(i):
-                prime_count += 1
+        prime_count = self.get_prime_count(n)
         return math.factorial(prime_count) * \
             math.factorial(n-prime_count) % self.MAX_NUM
 
-    def is_prime(self, val: int) -> bool:
-        for i in range(2, val):
-            if val % i == 0:
-                return False
-        return True
+    def get_prime_count(self, val: int) -> int:
+        is_prime = [True] * (val+1)
+        is_prime[0] = is_prime[1] = False
+        max_divisor = math.ceil(math.sqrt(val)) + 1
+        for i in range(2, max_divisor):
+            if is_prime[i]:
+                j = i*i
+                while j <= val:
+                    is_prime[j] = False
+                    j += i
+        return sum(is_prime)
