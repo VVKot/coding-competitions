808b35a1d5f08901a2c113994c7a170eca55b07a
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2019-09-03 22:10:36 +0200

    feat: add solution for 1176

--- leetcode/1176_diet_plan_performance.py
+++ leetcode/1176_diet_plan_performance.py
@@ -1,3 +1,14 @@
 class Solution:
 
-    
+    def dietPlanPerformance(self, calories: List[int], k: int, lower: int, upper: int) -> int:
+        res = i = 0
+        T = sum(calories[i:i+k])
+        while i+k <= len(calories):
+            if T < lower:
+                res -= 1
+            elif T > upper:
+                res += 1
+            end = min(len(calories)-1, i+k)
+            T -= calories[i]
+            T += calories[end]
+            i += 1
