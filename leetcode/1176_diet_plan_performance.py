324044b23d7523f5c38cd12925caac75e6f669e8
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-06 17:36:53 +0300

    style: fix indentation

--- leetcode/1176_diet_plan_performance.py
+++ leetcode/1176_diet_plan_performance.py
@@ -1,14 +1,23 @@
+from typing import List
+
+
 class Solution:
 
-    def dietPlanPerformance(self, calories: List[int], k: int, lower: int, upper: int) -> int:
-        res = i = 0
-        T = sum(calories[i:i+k])
-        while i+k <= len(calories):
-            if T < lower:
-                res -= 1
-            elif T > upper:
-                res += 1
-            end = min(len(calories)-1, i+k)
-            T -= calories[i]
-            T += calories[end]
+    def dietPlanPerformance(self,
+                            calories: List[int],
+                            k: int,
+                            lower: int,
+                            upper: int) -> int:
+        total = i = 0
+        N = len(calories)
+        running_calories_sum = sum(calories[i:i+k])
+        while i+k <= N:
+            if running_calories_sum < lower:
+                total -= 1
+            elif running_calories_sum > upper:
+                total += 1
+            end = min(N-1, i+k)
+            running_calories_sum -= calories[i]
+            running_calories_sum += calories[end]
             i += 1
+        return total
