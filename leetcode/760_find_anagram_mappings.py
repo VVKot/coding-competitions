3748fe78893c345345355265c2b89bfb105f9db5
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-06 17:40:24 +0300

    docs: add typings

--- leetcode/760_find_anagram_mappings.py
+++ leetcode/760_find_anagram_mappings.py
@@ -1,11 +1,11 @@
 import collections
-from typing import List
+from typing import Dict, List
 
 
 class Solution:
 
     def anagramMappings(self, A: List[int], B: List[int]) -> List[int]:
-        nums = collections.defaultdict(list)
+        nums = collections.defaultdict(list)  # type: Dict[int, List[int]]
         for i, num in enumerate(B):
             nums[num].append(i)
         result = []
