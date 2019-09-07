e9a3e97b8b1e2aabedc70ad20365181b639f0c79
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-07 21:41:09 +0300

    feat: add constant memory solution

--- leetcode/581_shortest_unsorted_continuos_subarray.py
+++ leetcode/581_shortest_unsorted_continuos_subarray.py
@@ -5,19 +5,29 @@ class Solution:
 
     def findUnsortedSubarray(self, nums: List[int]) -> int:
         N = len(nums)
-        left = N
-        stack = []  # type: List[int]
-        for i, num in enumerate(nums):
-            while stack and nums[stack[-1]] > num:
-                left = min(left, stack.pop())
-            stack.append(i)
-        right = 0
-        stack = []
-        for i in reversed(range(N)):
-            num = nums[i]
-            while stack and nums[stack[-1]] < num:
-                right = max(right, stack.pop())
-            stack.append(i)
-        if right < left:
+        min_num = max_num = float('inf')
+
+        for i in range(1, N):
+            if nums[i] < nums[i-1]:
+                min_num = min(nums[i:])
+                break
+
+        for i in reversed(range(N-1)):
+            if nums[i] > nums[i+1]:
+                max_num = max(nums[:i+1])
+                break
+
+        if min_num == float('inf'):
             return 0
+        left = right = 0
+
+        for i in range(N):
+            if nums[i] > min_num:
+                left = i
+                break
+
+        for i in reversed(range(N)):
+            if nums[i] < max_num:
+                right = i
+                break
         return right-left+1
