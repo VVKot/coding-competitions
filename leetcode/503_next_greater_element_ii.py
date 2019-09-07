0d9c228af64433ec9c3251255354404c41933da9
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-07 15:46:21 +0300

    refactor: clarify variable names, add typings

--- leetcode/503_next_greater_element_ii.py
+++ leetcode/503_next_greater_element_ii.py
@@ -1,12 +1,19 @@
+from typing import List
+
+
 class Solution:
-    def nextGreaterElements(self, A):
-        stack, res = [], [-1] * len(A)
-        for i in range(len(A)):
-            while stack and (A[stack[-1]] < A[i]):
-                res[stack.pop()] = A[i]
-            stack.append(i)
-        for i in range(len(A)):
-            while stack and (A[stack[-1]] < A[i]):
-                res[stack.pop()] = A[i]
-            stack.append(i)
-        return res
+
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        N = len(nums)
+        result = [-1] * N
+        nums_to_process = []  # type: List[int]
+        for i, num in enumerate(nums):
+            while nums_to_process and nums[nums_to_process[-1]] < num:
+                result[nums_to_process.pop()] = num
+            nums_to_process.append(i)
+        for num in nums:
+            if not nums_to_process:
+                break
+            while nums_to_process and nums[nums_to_process[-1]] < num:
+                result[nums_to_process.pop()] = num
+        return result
