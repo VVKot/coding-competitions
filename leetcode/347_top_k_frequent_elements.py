2cbfa68d60b923fa10dc68e51683e941919cf2df
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-08 14:35:00 +0300

    feat: add linear time solution using bucket sort

--- leetcode/347_top_k_frequent_elements.py
+++ leetcode/347_top_k_frequent_elements.py
@@ -1,16 +1,12 @@
 import collections
-import heapq
-from typing import List, Tuple
+import itertools
+from typing import List
 
 
 class Solution:
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = collections.Counter(nums)
-        result = []  # type: List[Tuple[int, int]]
-        for val, count in counts.items():
-            item = (count, val)
-            if len(result) == k:
-                heapq.heappushpop(result, item)
-            else:
-                heapq.heappush(result, item)
-        return [val for _, val in result]
+        buckets = [[] for _ in nums]
+        for num, freq in collections.Counter(nums).items():
+            buckets[-freq].append(num)
+        return list(itertools.chain(*buckets))[:k]
