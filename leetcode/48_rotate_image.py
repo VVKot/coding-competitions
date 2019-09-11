ba721c08581a6ab16c054ff1f903f94a825c3035
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-11 19:12:38 +0300

    feat: add in-place solution

--- leetcode/48_rotate_image.py
+++ leetcode/48_rotate_image.py
@@ -4,4 +4,12 @@ from typing import List
 class Solution:
 
     def rotate(self, matrix: List[List[int]]) -> None:
-        matrix[:] = zip(*matrix[::-1])
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for x in range(cols):
+            for y in range(rows//2):
+                matrix[y][x], matrix[rows-1-y][x] = \
+                    matrix[rows-1-y][x], matrix[y][x]
+        for y in range(rows):
+            for x in range(y+1, cols):
+                matrix[y][x], matrix[x][y] = matrix[x][y], matrix[y][x]
