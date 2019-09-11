e5f237ec8bf09e6ec04097bd53f783487729b533
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-11 05:23:03 +0300

    feat: easier way to generate all permutations

--- leetcode/301_remove_invalid_parenthesis.py
+++ leetcode/301_remove_invalid_parenthesis.py
@@ -4,33 +4,12 @@ from typing import List
 class Solution:
 
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        invalid_right = curr_left = 0
-        for ch in s:
-            if ch == '(':
-                curr_left += 1
-            elif ch == ')':
-                if curr_left > 0:
-                    curr_left -= 1
-                else:
-                    invalid_right += 1
-        invalid_left = curr_left
-        results = set()
-
-        def helper(curr, position, left, right):
-            if not left and not right:
-                result = curr + s[position:]
-                if self.is_valid(result):
-                    results.add(result)
-                return
-            if position >= len(s):
-                return
-            if left and s[position] == '(':
-                helper(curr, position+1, left-1, right)
-            if right and s[position] == ')':
-                helper(curr, position+1, left, right-1)
-            helper(curr+s[position], position+1, left, right)
-        helper('', 0, invalid_left, invalid_right)
-        return list(results)
+        level = {s}
+        while True:
+            valid = list(filter(self.is_valid, level))
+            if valid:
+                return valid
+            level = {s[:i] + s[i+1:] for s in level for i in range(len(s))}
 
     def is_valid(self, s: str) -> bool:
         left = 0
