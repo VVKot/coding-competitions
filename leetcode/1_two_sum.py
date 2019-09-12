be300f0271c5536217e4db12883fcf40251dc98f
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-12 06:32:08 +0300

    refactor: fill complements on the fly

--- leetcode/1_two_sum.py
+++ leetcode/1_two_sum.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import Dict, List
 
 
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complement = {target - num: i for i, num in enumerate(nums)}
-        for j, num in enumerate(nums):
-            if num in complement and complement[num] != j:
-                return [j, complement[num]]
+        complements = {}  # type: Dict[int, int]
+        for i, num in enumerate(nums):
+            complement = target-num
+            if complement in complements:
+                return [complements[complement], i]
+            complements[num] = i
         return [-1, -1]
