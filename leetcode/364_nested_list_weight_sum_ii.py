0cb1bfa03ec921b8857da2eaaa5a527efc55eb42
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-12 06:20:07 +0300

    feat: add brute force solution

---
+++ leetcode/364_nested_list_weight_sum_ii.py
@@ -0,0 +1,45 @@
+from typing import List
+
+"""
+This is the interface that allows for creating nested lists.
+You should not implement it, or speculate about its implementation
+"""
+
+
+class NestedInteger:
+
+    def __init__(self, value=None):
+        pass
+
+    def isInteger(self):
+        pass
+
+    def add(self, elem):
+        pass
+
+    def setInteger(self, value):
+        pass
+
+    def getInteger(self):
+        pass
+
+    def getList(self):
+        pass
+
+
+class Solution:
+
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        depth = 0
+        temp = nestedList[:]
+        while temp:
+            temp = sum([x.getList() for x in temp if not x.isInteger()], [])
+            depth += 1
+        result = 0
+        while nestedList:
+            result += depth * sum([x.getInteger() for x in nestedList
+                                   if x.isInteger()])
+            nestedList = sum([x.getList() for x in nestedList
+                              if not x.isInteger()], [])
+            depth -= 1
+        return result
