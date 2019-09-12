a2398051f37d0ee678775713f4a44870f40f15ca
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-12 07:26:14 +0300

    refactor: extract function for clarity

--- leetcode/680_valid_palindrome_ii.py
+++ leetcode/680_valid_palindrome_ii.py
@@ -1,9 +1,22 @@
 class Solution:
-    def validPalindrome(self, s):
-        left, right = 0, len(s) - 1
+
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
         while left < right:
             if s[left] != s[right]:
-                no_right, no_left = s[left:right], s[left + 1:right + 1]
-                return no_right == no_right[::-1] or no_left == no_left[::-1]
-            left, right = left + 1, right - 1
+                no_left = s[left+1:right+1]
+                no_right = s[left:right]
+                return self.is_palindrome(no_left) or \
+                    self.is_palindrome(no_right)
+            left += 1
+            right -= 1
+        return True
+
+    def is_palindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
         return True
