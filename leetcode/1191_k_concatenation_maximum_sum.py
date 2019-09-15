d680e454a8861ffb8d0bcf17debe71d283bf4647
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-15 17:13:10 +0300

    feat: add more consice solution

--- leetcode/1191_k_concatenation_maximum_sum.py
+++ leetcode/1191_k_concatenation_maximum_sum.py
@@ -6,37 +6,26 @@ class Solution:
     MAX_SUM = 10**9 + 7
 
     def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
-        result = 0
-        if all(i <= 0 for i in arr):
-            return result
-
-        max_subarray_sum = curr = 0
-        for num in arr:
-            if curr + num > 0:
-                curr += num
-                max_subarray_sum = max(max_subarray_sum, curr)
-            else:
-                curr = 0
-
+        max_subarray_sum = self._get_max_subarray_sum(arr)
         array_sum = sum(arr)
-        all_arrays_sum = array_sum * k
-
+        total_concatendated_sum = array_sum * k
         if k == 1:
-            return max(max_subarray_sum, all_arrays_sum) % self.MAX_SUM
+            return max(max_subarray_sum,
+                       total_concatendated_sum) % self.MAX_SUM
 
-        max_left_prefix = max_right_prefix = 0
-        curr_left = curr_right = 0
-        N = len(arr)
-        for num in arr:
-            curr_left += num
-            max_left_prefix = max(max_left_prefix, curr_left)
-        for num in reversed(arr):
-            curr_right += num
-            max_right_prefix = max(max_right_prefix, curr_right)
-
-        max_with_prefix = max_left_prefix + \
-            max_right_prefix + max(array_sum * (k-2), 0)
+        max_overlapped_subarray_sum = self._get_max_subarray_sum(arr + arr)
+        max_overlapped_subarray_sum += max(0, array_sum * (k-2))
 
         return max([max_subarray_sum,
-                    all_arrays_sum,
-                    max_with_prefix]) % self.MAX_SUM
+                    total_concatendated_sum,
+                    max_overlapped_subarray_sum]) % self.MAX_SUM
+
+    def _get_max_subarray_sum(self, nums: List) -> int:
+        max_sum = curr_sum = 0
+        for num in nums:
+            curr_sum += num
+            if curr_sum > 0:
+                max_sum = max(max_sum, curr_sum)
+            else:
+                curr_sum = 0
+        return max_sum
