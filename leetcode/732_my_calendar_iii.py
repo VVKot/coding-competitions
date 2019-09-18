e34957988beeb731a1d2c78a512f075f709b2587
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-18 21:07:50 +0300

    refactor: extract methods, clarify variable names

--- leetcode/732_my_calendar_iii.py
+++ leetcode/732_my_calendar_iii.py
@@ -1,17 +1,19 @@
-from bisect import insort
+import bisect
 
 
 class MyCalendarThree:
 
     def __init__(self):
-        self.booked = []
+        self.bookings = []
 
-    def book(self, start, end):
-        insort(self.booked, (start, 1))
-        insort(self.booked, (end, -1))
-        result = 0
-        cum_sum = 0
-        for t, val in self.booked:
-            cum_sum += val
-            result = max(result, cum_sum)
-        return result
+    def book(self, start: int, end: int) -> int:
+        bisect.insort(self.bookings, (start, 1))
+        bisect.insort(self.bookings, (end, -1))
+        return self._get_max_bookings()
+
+    def _get_max_bookings(self) -> int:
+        max_bookings = bookings = 0
+        for _, booking_diff in self.bookings:
+            bookings += booking_diff
+            max_bookings = max(max_bookings, bookings)
+        return max_bookings
