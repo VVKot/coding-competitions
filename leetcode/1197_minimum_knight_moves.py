77883512e9bbdbc3bf1e285c938a405e40285646
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-22 18:01:07 +0300

    feat: add math constant time solution

--- leetcode/1197_minimum_knight_moves.py
+++ leetcode/1197_minimum_knight_moves.py
@@ -1,16 +1,11 @@
 class Solution:
 
     def minKnightMoves(self, x: int, y: int) -> int:
-        moves = [(2, 1), (-2, 1), (2, -1), (-2, -1),
-                 (1, 2), (1, -2), (-1, 2), (-1, -2)]
         x, y = abs(x), abs(y)
-        stack = [(0, 0, 0)]
-        distances = {(0, 0): 0}
-        for r, c, d in stack:
-            for dr, dc in moves:
-                curr_r, curr_c = r+dr, c+dc
-                if -2 <= curr_r <= x+2 and -2 <= curr_c <= y+2:
-                    if (curr_r, curr_c) not in distances:
-                        distances[(curr_r, curr_c)] = d+1
-                        stack.append((curr_r, curr_c, d+1))
-        return distances[(x, y)]
+        if x+y == 1:
+            return 3
+        if x == y == 2:
+            return 4
+        step = max((y+1)//2, (x+1)//2, (x+y+2)//3)
+        step += (step ^ x ^ y) & 1
+        return step
