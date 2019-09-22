6ba8ebf4ba54c18ef6a1ae4cf1b08f1eb9008b3c
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-22 18:07:01 +0300

    feat: add solution based on Huffman encoding

---
+++ leetcode/1199_minimum_time_to_build_blocks.py
@@ -0,0 +1,12 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def minBuildTime(self, blocks: List[int], split: int) -> int:
+        heapq.heapify(blocks)
+        while len(block) > 1:
+            _ = heapq.heappop(blocks)
+            last = heapq.heappop(blocks)
+            heapq.heappush(blocks, last+split)
+        return heapq.heappop(blocks)
