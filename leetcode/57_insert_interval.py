01c59eb1a7bd7bd36e70652014d698a615105f64
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-22 21:00:19 +0300

    feat: add solution with merge at the end

--- leetcode/57_insert_interval.py
+++ leetcode/57_insert_interval.py
@@ -6,21 +6,14 @@ class Solution:
     def insert(self,
                intervals: List[List[int]],
                newInterval: List[int]) -> List[List[int]]:
-        result = []  # type: List[List[int]]
-
-        def merge(start, end):
-            if result and result[-1][1] >= start:
-                result[-1][1] = max(end, result[-1][1])
-            else:
-                result.append([start, end])
-
         l, r = newInterval
-        interval_merged = False
-        for i, j in intervals:
-            if not interval_merged and i > l:
-                interval_merged = True
-                merge(l, r)
-            merge(i, j)
-        if not interval_merged:
-            merge(l, r)
-        return result
+        before, after = [], []
+        for start, end in intervals:
+            if end < l:
+                before.append([start, end])
+            elif start > r:
+                after.append([start, end])
+            else:
+                l = min(l, start)
+                r = max(r, end)
+        return before + [[l, r]] + after
