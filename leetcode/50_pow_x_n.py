4eb0334e9cdb512737e1f9507296471ba8cd0164
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-26 05:52:03 +0300

    feat: add clean log time solution

--- leetcode/50_pow_x_n.py
+++ leetcode/50_pow_x_n.py
@@ -3,25 +3,13 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
-        if x == 0:
-            return 0
-        power = 1
         if n < 0:
             x = 1/x
-            n = -n
-        power_values = {1: x}
-        while power < n:
-            power *= 2
+            n = -n + 1
+        result = 1.0
+        while n:
+            if n & 1:
+                result *= x
             x *= x
-            power_values[power] = x
-        if power == n:
-            return x
-        power //= 2
-        remainder = n - power
-        x = power_values[power]
-        while remainder:
-            power //= 2
-            if power <= remainder:
-                remainder -= power
-                x *= power_values[power]
-        return x
+            n >>= 1
+        return result
