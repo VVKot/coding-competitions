a6c630b75c0a668431831fdd2154f0466266fa30
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-29 13:38:46 +0300

    feat: add stack solution

--- leetcode/1209_remove_all_adjacent_duplicates_in_string_ii.py
+++ leetcode/1209_remove_all_adjacent_duplicates_in_string_ii.py
@@ -1,16 +1,12 @@
 class Solution:
 
     def removeDuplicates(self, s: str, k: int) -> str:
-        stack = []
+        stack = [['#', 0]]
         for ch in s:
-            if not stack:
-                stack.append([ch, 1])
+            if stack[-1][0] == ch:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
             else:
-                prev_ch, prev_count = stack[-1]
-                if prev_ch != ch:
-                    stack.append([ch, 1])
-                else:
-                    stack[-1][1] += 1
-                    if stack[-1][1] == k:
-                        stack.pop()
+                stack.append([ch, 1])
         return ''.join(ch * count for ch, count in stack)
