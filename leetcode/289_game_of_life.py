13bbcd3e60b85ec554c86b40afc889e73b876c76
VVKot <volodymyr.kot.ua@gmail.com>
2019-09-29 13:21:19 +0300

    feat: add solution without offset

--- leetcode/289_game_of_life.py
+++ leetcode/289_game_of_life.py
@@ -3,42 +3,43 @@ from typing import List
 
 class Solution:
 
-    OFFSET = 10
-    DEAD, ALIVE = range(2)
+    DEAD, ALIVE, BECAME_DEAD, BECAME_ALIVE = range(4)
 
     def gameOfLife(self, board: List[List[int]]) -> None:
-        self.count_neighbors(board)
-        self.get_next_generation(board)
+        self.determine_new_state(board)
+        self.unify_states(board)
 
-    def count_neighbors(self, board: List[List[int]]) -> None:
+    def determine_new_state(self, board: List[List[int]]) -> None:
         for y, row in enumerate(board):
             for x, val in enumerate(row):
-                alive = self.get_alive(board, y, x)
-                if val == self.ALIVE:
-                    alive += self.OFFSET
-                board[y][x] = alive
-
-    def get_next_generation(self, board: List[List[int]]) -> None:
-        for y, row in enumerate(board):
-            for x, val in enumerate(row):
+                alive_neighbors = self.get_alive(board, y, x)
                 state = self.DEAD
-                if val >= self.OFFSET:
-                    val -= self.OFFSET
-                    if val in (2, 3):
+                if val == self.ALIVE:
+                    if alive_neighbors in (2, 3):
                         state = self.ALIVE
+                    else:
+                        state = self.BECAME_DEAD
                 else:
-                    if val == 3:
-                        state = self.ALIVE
+                    if alive_neighbors == 3:
+                        state = self.BECAME_ALIVE
                 board[y][x] = state
 
+    def unify_states(self, board: List[List[int]]) -> None:
+        for y, row in enumerate(board):
+            for x, val in enumerate(row):
+                if val == self.BECAME_ALIVE:
+                    board[y][x] = self.ALIVE
+                if val == self.BECAME_DEAD:
+                    board[y][x] = self.DEAD
+
     def get_alive(self, board: List[List[int]], y: int, x: int) -> int:
+        neighbors = [(y-1, x-1), (y-1, x), (y-1, x+1), (y, x-1),
+                     (y, x+1), (y+1, x-1), (y+1, x), (y+1, x+1)]
         alive = 0
-        for curr_y, curr_x in [(y-1, x-1), (y-1, x), (y-1, x+1), (y, x-1)]:
+        for curr_y, curr_x in neighbors:
             if self.is_valid(board, curr_y, curr_x):
-                alive += board[curr_y][curr_x] >= self.OFFSET
-        for curr_y, curr_x in [(y, x+1), (y+1, x-1), (y+1, x), (y+1, x+1)]:
-            if self.is_valid(board, curr_y, curr_x):
-                alive += board[curr_y][curr_x] == self.ALIVE
+                alive += board[curr_y][curr_x] in (self.ALIVE,
+                                                   self.BECAME_DEAD)
         return alive
 
     def is_valid(self, board: List[List[int]], y: int, x: int) -> bool:
