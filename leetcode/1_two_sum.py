74de8fab071370fca7d17d0ab8af95a7113b1611
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2019-09-30 18:25:36 +0300

    refactor: simplify code

--- leetcode/1_two_sum.py
+++ leetcode/1_two_sum.py
@@ -1,13 +1,12 @@
-from typing import Dict, List
+from typing import List
 
 
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complements = {}  # type: Dict[int, int]
+        complements = dict()
         for i, num in enumerate(nums):
-            complement = target-num
-            if complement in complements:
-                return [complements[complement], i]
-            complements[num] = i
+            if num in complements:
+                return [complements[num], i]
+            complements[target-num] = i
         return [-1, -1]
