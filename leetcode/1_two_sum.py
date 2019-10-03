3be83315428e9a11bc3b2039b31e58e9be59a8c4
VVKot <volodymyr.kot.ua@gmail.com>
2019-10-03 13:26:57 +0300

    docs: add typings

--- leetcode/1_two_sum.py
+++ leetcode/1_two_sum.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import Dict, List
 
 
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complements = dict()
+        complements = dict()  # type: Dict[int, int]
         for i, num in enumerate(nums):
             if num in complements:
                 return [complements[num], i]
