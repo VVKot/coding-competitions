29f869236ed4365db2b47f945ae04078ed666263
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2019-10-04 21:52:03 +0200

    chore: comment class for LC submissions

--- leetcode/206_reverse_linked_list.py
+++ leetcode/206_reverse_linked_list.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
 
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+#class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
 
 
 class Solution:
