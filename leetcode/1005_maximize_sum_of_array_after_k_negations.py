07abced8083005dc92d4f73a4f3d2f4bba56a3d8
VVKot <volodymyr.kot.ua@gmail.com>
2019-10-13 08:32:46 +0300

    feat: add heap solution

--- leetcode/1005_maximize_sum_of_array_after_k_negations.py
+++ leetcode/1005_maximize_sum_of_array_after_k_negations.py
@@ -1,16 +1,11 @@
+import heapq
 from typing import List
 
 
 class Solution:
 
     def largestSumAfterKNegations(self, A: List[int], K: int) -> int:
-        A.sort()
-        i = 0
-        while K and A[i] <= 0:
-            A[i] = -A[i]
-            K -= 1
-            i += 1
-        total = sum(A)
-        if K and K & 1:
-            total -= min(A) * 2
-        return total
+        heapq.heapify(A)
+        for _ in range(K):
+            heapq.heappush(A, -heapq.heappop(A))
+        return sum(A)
