26798f0267821a5fa1da6835228fa17a2f1f7a23
VVKot <volodymyr.kot.ua@gmail.com>
2019-10-16 05:38:23 +0300

    refactor: change variable names for clarity

--- leetcode/1224_maximum_equal_frequency.py
+++ leetcode/1224_maximum_equal_frequency.py
@@ -5,35 +5,31 @@ from typing import Counter, List
 class Solution:
 
     def maxEqualFreq(self, nums: List[int]) -> int:
-        num_frequencies = collections.Counter()  # type: Counter[int]
-        frequencies_count = collections.Counter()  # type: Counter[int]
-        max_prefix_size = 0
+        number_freq = collections.Counter()  # type: Counter[int]
+        freq_count = collections.Counter()  # type: Counter[int]
+        max_prefix_size = max_freq = 0
         for i, num in enumerate(nums):
-            prev = num_frequencies[num]
-            num_frequencies[num] += 1
-            self._update_frequency_count(frequencies_count, num, prev)
-            if self._is_valid_prefix(frequencies_count):
+            self._update_frequency_count(number_freq, freq_count, num)
+            max_freq = max(max_freq, number_freq[num])
+            if self._is_valid_prefix(freq_count, max_freq, i):
                 max_prefix_size = i + 1
         return max_prefix_size
 
     def _update_frequency_count(self,
-                                frequencies_count: Counter[int],
-                                num: int,
-                                prev_count: int) -> None:
-        if prev_count > 0:
-            frequencies_count[prev_count] -= 1
-        if frequencies_count[prev_count] == 0:
-            del frequencies_count[prev_count]
-        frequencies_count[prev_count+1] += 1
+                                number_freq: Counter[int],
+                                freq_count: Counter[int],
+                                num: int) -> None:
+        number_freq[num] += 1
+        freq_count[number_freq[num]-1] -= 1
+        freq_count[number_freq[num]] += 1
 
-    def _is_valid_prefix(self, frequencies_count: Counter[int]) -> bool:
-        if len(frequencies_count) > 2:
-            return False
-        if len(frequencies_count) == 1:
-            [a] = frequencies_count.keys()
-            [b] = frequencies_count.values()
-            return a == 1 or b == 1
-        [(low_freq, low_count), (hi_freq, hi_count)] = \
-            sorted(frequencies_count.items())
-        return (low_freq == 1 and low_count == 1) or \
-            (hi_freq - low_freq == 1 and hi_count == 1)
+    def _is_valid_prefix(self,
+                         freq_count: Counter[int],
+                         max_freq: int,
+                         curr_index: int) -> bool:
+        all_numbers_appear_once = max_freq == 1
+        one_number_appears_once = max_freq * freq_count[max_freq] == curr_index
+        one_number_appears_max_frequency_times = \
+            (max_freq-1) * (freq_count[max_freq-1]+1) == curr_index
+        return all_numbers_appear_once or one_number_appears_once or \
+            one_number_appears_max_frequency_times
