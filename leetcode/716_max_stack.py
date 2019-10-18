e6b98f5acb7ee266899706d916d2291d07a42e69
VVKot <volodymyr.kot.ua@gmail.com>
2019-10-18 22:05:11 +0300

    feat: add linear time solution

---
+++ leetcode/716_max_stack.py
@@ -0,0 +1,20 @@
+class MaxStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def peekMax(self) -> int:
+        return max(self.stack)
+
+    def popMax(self) -> int:
+        max_index = self.stack[::-1].index(self.peekMax())
+        return self.stack.pop(len(self.stack) - 1 - max_index)
