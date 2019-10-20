35003b1e16f9b7c23e005fac1773dfe1dee074bd
VVKot <volodymyr.kot.ua@gmail.com>
2019-10-20 12:14:48 +0300

    feat: add binary search solution

--- leetcode/1228_missing_number_in_arithmetic_progression.py
+++ leetcode/1228_missing_number_in_arithmetic_progression.py
@@ -4,5 +4,13 @@ from typing import List
 class Solution:
 
     def missingNumber(self, arr: List[int]) -> int:
-        total = (arr[0] + arr[-1]) * (len(arr) + 1) // 2
-        return total - sum(arr)
+        n = len(arr)
+        d = (arr[-1] - arr[0]) // n
+        left, right = 0, n
+        while left < right:
+            mid = (left + right) // 2
+            if arr[mid] == arr[0] + d * mid:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[0] + d * left
