80b137954e31e55ab56c69698a873307e782d980
VVKot <volodymyr.kot.ua@gmail.com>
2019-10-25 20:31:48 +0300

    feat: improve memory complexity

--- leetcode/359_logger_rate_limiter.py
+++ leetcode/359_logger_rate_limiter.py
@@ -1,16 +1,19 @@
-import collections
-
-
 class Logger:
 
     PRINT_INTERVAL = 10
 
     def __init__(self):
-        self.can_print_on = collections.defaultdict(int)
+        self.buckets = [-1 for _ in range(self.PRINT_INTERVAL)]
+        self.sets = [set() for _ in range(self.PRINT_INTERVAL)]
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        allowed_timestamp = self.can_print_on[message]
-        if timestamp < allowed_timestamp:
-            return False
-        self.can_print_on[message] = timestamp + self.PRINT_INTERVAL
+        bucket_index = timestamp % self.PRINT_INTERVAL
+        if self.buckets[bucket_index] != timestamp:
+            self.sets[bucket_index].clear()
+            self.buckets[bucket_index] = timestamp
+        for i, bucket_timestamp in enumerate(self.buckets):
+            if timestamp - bucket_timestamp < self.PRINT_INTERVAL:
+                if message in self.sets[i]:
+                    return False
+        self.sets[bucket_index].add(message)
         return True
