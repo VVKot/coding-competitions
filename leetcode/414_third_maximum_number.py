3d6419c3773ea85ee8ea6422d81700f7e0cbdf38
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-06 06:49:48 +0200

    feat: add linear time solution

--- leetcode/414_third_maximum_number.py
+++ leetcode/414_third_maximum_number.py
@@ -1,9 +1,14 @@
+import heapq
 from typing import List
 
 
 class Solution:
 
     def thirdMax(self, nums: List[int]) -> int:
-        nums = list(set(nums))
-        nums.sort()
-        return nums[-3] if len(nums) >= 3 else nums[-1]
+        max_nums = []
+        for num in set(nums):
+            if len(max_nums) < 3:
+                heapq.heappush(max_nums, num)
+            else:
+                heapq.heappushpop(max_nums, num)
+        return max_nums[0] if len(max_nums) == 3 else max(max_nums)
