d1fb9daba78f9e6c255a52320143c5453036012b
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-09 19:16:26 +0200

    docs: add explanation and complexities

--- leetcode/1_two_sum.py
+++ leetcode/1_two_sum.py
@@ -1,12 +1,22 @@
+"""
+T: O(N)
+S: O(N)
+
+Remember numbers we have seen so far alongside their indices.
+Return a sorted pair of indices when there is a match.
+"""
+
+
 from typing import Dict, List
 
 
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complements = dict()  # type: Dict[int, int]
+        seen = {}  # type: Dict[int, int]
         for i, num in enumerate(nums):
-            if num in complements:
-                return [complements[num], i]
-            complements[target-num] = i
+            other = target-num
+            if other in seen:
+                return sorted([i, seen[other]])
+            seen[num] = i
         return [-1, -1]
