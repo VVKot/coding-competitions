79800a69530c12125847c3e80820da43ce1eb389
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-12 21:05:52 +0200

    feat: compess the comparison into a one lambda

--- leetcode/937_reorder_data_in_log_files.py
+++ leetcode/937_reorder_data_in_log_files.py
@@ -2,29 +2,19 @@
 T: O(NlogN)
 S: O(N)
 
-First, split logs into separate groups.
-After that, letter logs are sorted by words,
-and by the identifier in the case of a tie.
+Compress requirements into the one lambda.
+All that we have to check is the first letter of the first word.
 """
 
 
-from typing import List
+from typing import List, Tuple
 
 
 class Solution:
 
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        letter_logs = []
-        digit_logs = []
-        for log in logs:
-            if log.split()[1].isnumeric():
-                digit_logs.append(log)
-            else:
-                letter_logs.append(log)
-        letter_logs.sort(key=self.sort_lambda)
-        return letter_logs + digit_logs
+        return sorted(logs, key=self.sort_lambda)
 
-    def sort_lambda(self, log: str) -> List[str]:
-        words = log.split()
-        words.append(words.pop(0))
-        return words
+    def sort_lambda(self, log: str) -> Tuple[int, str, str]:
+        identifier, words = log.split(' ', 1)
+        return (0, words, identifier) if words[0].isalpha() else (1, '', '')
