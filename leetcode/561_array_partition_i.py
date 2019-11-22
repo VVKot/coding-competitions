9e4b697df74859cfda14bf9d825c441cf5db1a64
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-22 22:24:46 +0200

    docs: add complexities and description

--- leetcode/561_array_partition_i.py
+++ leetcode/561_array_partition_i.py
@@ -1,3 +1,12 @@
+"""
+T: O(NlogN)
+S: O(N) - sorted returns an iterator, but Python sorting works in linear memory
+
+We can determine the best solution greedily. We have to group numbers in
+sorted order and take every one of them which is at the odd position.
+"""
+
+
 from typing import List
 
 
