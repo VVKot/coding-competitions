ea2ef8e3c08e208149cd2fa4058b3afe300b3dc8
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-24 16:58:29 +0200

    feat: add more concise solution

--- leetcode/1122_relative_sort_array.py
+++ leetcode/1122_relative_sort_array.py
@@ -1,15 +1,22 @@
-import collections
+"""
+T: O(NlogN)
+S: O(N) - only because Python sorting works in linear time
+
+We are exploiting constraints to create an elegant solution.
+First, we create a relative order map for every number in the second list.
+Any other number should come after it, so we just add the maximum value
+of a number to the key for the sorting.
+"""
+
+
 from typing import List
 
 
 class Solution:
 
+    MAX_NUM_VALUE = 1000
+
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        result = []
-        counts = collections.Counter(arr1)
-        for num in arr2:
-            result.extend([num] * counts[num])
-            del counts[num]
-        for num in sorted(counts.keys()):
-            result.extend([num] * counts[num])
-        return result
+        nums_order = {num: i for i, num in enumerate(arr2)}
+        return sorted(arr1,
+                      key=lambda x: nums_order.get(x, self.MAX_NUM_VALUE+x))
