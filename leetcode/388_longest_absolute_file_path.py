a7ff6d1b6e78aed856e29451eb3b1ccbd399833c
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-24 18:38:48 +0200

    feat: add concise solution

--- leetcode/388_longest_absolute_file_path.py
+++ leetcode/388_longest_absolute_file_path.py
@@ -2,44 +2,23 @@
 T: O(N)
 S: O(N)
 
-Firstly, we find current filename and level at which this file is located.
-Secondly, we prune the current path if we jumped to a level closer to the root.
-After that, we only have to extend current path and compare it to the longest
-path we have seen so far if current element is a file.
+Firstly, get the depth of the current element by counting tabs.
+Secondly, we record the path length to the dictionary using depth as a key.
+After that, we only have to extend the current path and compare it to the
+longest path we have seen so far if the current element is a file.
 """
-
-
-from typing import List, Tuple
 
 
 class Solution:
 
-    CARRIAGE_RETURN, TAB, DOT, SLASH = '\n', '\t', '.', '/'
+    TAB, DOT = '\t', '.'
 
     def lengthLongestPath(self, fs_path: str) -> int:
-        longest_path_len = 0
-        curr_path = []  # type: List[str]
-        for path_part in self.get_path_parts(fs_path):
-            file_name, file_level = self.get_file_info(path_part)
-            if file_level < len(curr_path):
-                curr_path = curr_path[:file_level]
-            curr_path.append(file_name)
-            if self.is_file(file_name):
-                longest_path_len = max(longest_path_len,
-                                       self.get_path_len(curr_path))
-        return longest_path_len
-
-    def get_path_parts(self, fs_path: str) -> List[str]:
-        return fs_path.split(self.CARRIAGE_RETURN)
-
-    def get_file_info(self, path_part: str) -> Tuple[str, int]:
-        curr_row = path_part.split(self.TAB)
-        curr_level = len(curr_row) - 1
-        file_name = curr_row[0] if curr_level == 0 else curr_row[-1]
-        return file_name, curr_level
-
-    def is_file(self, file_name: str) -> bool:
-        return self.DOT in file_name
-
-    def get_path_len(self, curr_path: List[str]) -> int:
-        return len(self.SLASH.join(curr_path))
+        longest = 0
+        path_lens = {-1: 0}
+        for path in fs_path.splitlines():
+            depth = path.count(self.TAB)
+            path_lens[depth] = path_lens[depth - 1] + len(path) - depth
+            if self.DOT in path:
+                longest = max(longest, path_lens[depth] + depth)
+        return longest
