312181eeef98b2c5ee5732e0fbd806e0706acda7
VVKot <volodymyr.kot.ua@gmail.com>
2019-11-30 13:57:53 +0200

    docs: add explanation

--- leetcode/225_implement_stack_using_queues.py
+++ leetcode/225_implement_stack_using_queues.py
@@ -1,24 +1,38 @@
+"""
+T:
+    push O(N)
+    pop O(1)
+    top O(1)
+    empty O(1)
+S: O(N)
+
+The only operation that needs to be linear in time is the push.
+We convert the queue to a stack every time a new number comes in.
+To do that, we have to rotate the queue of length L L-1 times.
+"""
+
+
 import collections
 
 
 class MyStack:
 
     def __init__(self):
-        self.queue = collections.deque()
+        self.stack = collections.deque()
 
     def push(self, x: int) -> None:
-        self.queue.appendleft(x)
-        self._convert_to_stack()
-
-    def _convert_to_stack(self):
-        for _ in range(len(self.queue)-1):
-            self.queue.appendleft(self.queue.pop())
+        self.stack.append(x)
+        self._rotate(len(self.stack)-1)
 
     def pop(self) -> int:
-        return self.queue.pop()
+        return self.stack.popleft()
 
     def top(self) -> int:
-        return self.queue[-1]
+        return self.stack[0]
 
     def empty(self) -> bool:
-        return not self.queue
+        return not self.stack
+
+    def _rotate(self, times: int) -> None:
+        for _ in range(times):
+            self.stack.append(self.stack.popleft())
