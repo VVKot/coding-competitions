d97fa9d3eb6b409820fd9524c0b288b673bce33a
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-01 08:46:16 +0200

    refactor: simplify solution

--- leetcode/1272_remove_interval.py
+++ leetcode/1272_remove_interval.py
@@ -17,22 +17,13 @@ class Solution:
                        intervals: List[List[int]],
                        toBeRemoved: List[int]) -> List[List[int]]:
         processed_intervals = []
-        for interval in intervals:
-            for reduced_interval in self.get_reduced_intervals(interval,
-                                                               toBeRemoved):
-                processed_intervals.append(reduced_interval)
+        remove_start, remove_end = toBeRemoved
+        for start, end in intervals:
+            if remove_end <= start or remove_start >= end:
+                processed_intervals.append([start, end])
+            else:
+                if remove_start > start:
+                    processed_intervals.append([start, remove_start])
+                if remove_end < end:
+                    processed_intervals.append([remove_end, end])
         return processed_intervals
-
-    def get_reduced_intervals(self,
-                              interval: List[int],
-                              to_remove: List[int]) -> List[List[int]]:
-        start, end = interval
-        remove_start, remove_end = to_remove
-        if remove_end <= start or remove_start >= end:
-            return [interval]
-        reduced = []
-        if remove_start > start:
-            reduced.append([start, remove_start])
-        if remove_end < end:
-            reduced.append([remove_end, end])
-        return reduced
