fc53975977f1362e11a82740443286858cf03473
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-02 06:18:22 +0200

    docs: add explanation and complexity analysis

--- leetcode/543_diameter_of_binary_tree.py
+++ leetcode/543_diameter_of_binary_tree.py
@@ -1,4 +1,16 @@
-from typing import Dict
+"""
+T: O(N)
+S: O(N)
+
+A good example of making a recursive solution into an iterative one.
+For each node, we need to get to know the depths of their left and right
+branches. Using this information, we find the diameter for the current node
+and current node depth. The maximum seen diameter is the maximum diameter
+of the tree.
+"""
+
+
+from typing import Dict, Optional
 
 
 class TreeNode:
@@ -11,21 +23,22 @@ class TreeNode:
 class Solution:
 
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        max_diameter = 0
         if not root:
-            return 0
-        res = 0
-        stack = [root]
-        vals = {}  # type: Dict[TreeNode, int]
-        while stack:
-            curr = stack[-1]
-            if curr.left and curr.left not in vals:
-                stack.append(curr.left)
-            elif curr.right and curr.right not in vals:
-                stack.append(curr.right)
+            return max_diameter
+        node_depths = {None: 0}  # type: Dict[Optional[TreeNode], int]
+        nodes_to_process = [root]
+        while nodes_to_process:
+            curr = nodes_to_process[-1]
+            left, right = curr.left, curr.right
+            if left and left not in node_depths:
+                nodes_to_process.append(left)
+            elif right and right not in node_depths:
+                nodes_to_process.append(right)
             else:
-                head = stack.pop()
-                left = vals[head.left] if head.left in vals else 0
-                right = vals[head.right] if head.right in vals else 0
-                vals[head] = 1 + max(left, right)
-                res = max(res, left + right)
-        return res
+                _ = nodes_to_process.pop()
+                left_depth = node_depths[left]
+                right_depth = node_depths[right]
+                node_depths[curr] = 1 + max(left_depth, right_depth)
+                max_diameter = max(max_diameter, left_depth + right_depth)
+        return max_diameter
