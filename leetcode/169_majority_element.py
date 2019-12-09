9645a05564d8d56f024bdf4f6c0a9819c38086e3
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-09 06:10:52 +0200

    docs: add complexity analysis and explanation

--- leetcode/169_majority_element.py
+++ leetcode/169_majority_element.py
@@ -1,15 +1,22 @@
+"""
+T: O(N)
+S: O(1)
+
+Remember the last most common element, and update its count according to the
+current element. When the count becomes zero, update the majority element.
+In the end, the last most common item is the most common overall
+"""
+
 from typing import List
 
 
 class Solution:
 
     def majorityElement(self, nums: List[int]) -> int:
-        major, count = 0, 0
+        majority_element = 0
+        majority_count = 0
         for num in nums:
-            if not count:
-                major, count = num, 1
-            elif num == major:
-                count += 1
-            else:
-                count -= 1
-        return major
+            if majority_count == 0:
+                majority_element = num
+            majority_count += 1 if majority_element == num else -1
+        return majority_element
