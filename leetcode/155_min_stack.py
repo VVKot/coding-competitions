7849c27984515f5b300bf0915152f75aed89e3b5
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-13 06:01:40 +0200

    docs: add explanations and complexity analysis

--- leetcode/155_min_stack.py
+++ leetcode/155_min_stack.py
@@ -1,3 +1,14 @@
+"""
+T: O(1) everything
+S: O(N)
+
+The only trick case is storing a minimum number. The basic idea is to store it
+on the push and modify on pop, but that will lead to the linear runtime of pop.
+To avoid that, when we add new minimum number X, we also record previously seen
+minimum number Y >= X, and assign it to a global minimum if X gets popped.
+"""
+
+
 class MinStack:
 
     def __init__(self):
