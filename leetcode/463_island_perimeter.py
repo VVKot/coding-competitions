85676c4ab37e3d9e3ad2cf8da2c6c8abea487e98
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-14 13:13:07 +0200

    docs: add explanation

--- leetcode/463_island_perimeter.py
+++ leetcode/463_island_perimeter.py
@@ -1,3 +1,13 @@
+"""
+T: O(R*C)
+S: O(1)
+
+One of the multiple ways to solve this problem in constant space.
+We count the total number of parts of the island and their neighbors to the
+right and to the bottom. Each island part has a perimeter of four, and
+connection takes up two units of length.
+"""
+
 from typing import List
 
 
@@ -15,11 +25,11 @@ class Solution:
             for x, val in enumerate(row):
                 if val == self.LAND:
                     land_count += 1
-                    if y < rows-1:
-                        if grid[y+1][x] == self.LAND:
+                    if y < rows - 1:
+                        if grid[y + 1][x] == self.LAND:
                             neighbor_count += 1
-                    if x < cols-1:
-                        if grid[y][x+1] == self.LAND:
+                    if x < cols - 1:
+                        if grid[y][x + 1] == self.LAND:
                             neighbor_count += 1
         return land_count * self.EDGES_PER_CELL - \
             neighbor_count * self.EDGES_PER_NEIGHBOR_CONNECTION
