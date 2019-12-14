04cf94cca161f9f8732a69661cde71201d82748f
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-14 17:59:16 +0200

    docs: add explanation

--- leetcode/33_search_in_rotated_sorted_array.py
+++ leetcode/33_search_in_rotated_sorted_array.py
@@ -1,37 +1,45 @@
+"""
+T: O(logN)
+S: O(logN)
+
+The solution a is two-step process. First, we find the smallest number in the
+list. After that, what's left is to do the binary search on the appropriate
+half of the list
+"""
+
 from typing import List
 
 
 class Solution:
+
     def search(self, nums: List[int], target: int) -> int:
         if not nums:
             return -1
-        max_index = self.get_pivot(nums, target)
-        if nums[0] <= target <= nums[max_index]:
-            return self.binary_search(nums, 0, max_index, target)
+        min_number_index = self.get_min_index(nums)
+        if nums[min_number_index] <= target <= nums[-1]:
+            return self.binary_search(nums, target, min_number_index,
+                                      len(nums) - 1)
         else:
-            return self.binary_search(nums, max_index+1, len(nums)-1, target)
+            return self.binary_search(nums, target, 0, min_number_index - 1)
 
-    def get_pivot(self, nums: List[int], target: int) -> int:
+    def get_min_index(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
-        while left != right:
-            mid = (left+right) // 2
-            if nums[mid] > nums[left]:
-                left = mid
+        while left < right:
+            mid = (left + right) // 2
+            if nums[0] <= nums[mid]:
+                left = mid + 1
             else:
                 right = mid
         return left
 
-    def binary_search(self,
-                      nums: List[int],
-                      left: int,
-                      right: int,
-                      target: int) -> int:
+    def binary_search(self, nums: List[int], target: int, left: int,
+                      right: int) -> int:
         while left <= right:
-            mid = (left+right) // 2
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] < target:
-                left = mid + 1
-            else:
+            elif nums[mid] > target:
                 right = mid - 1
+            else:
+                left = mid + 1
         return -1
