97403124bddcd8fc244fa5c9ba47196ee0f31e38
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-14 18:34:44 +0200

    feat: add one-pass solution

--- leetcode/33_search_in_rotated_sorted_array.py
+++ leetcode/33_search_in_rotated_sorted_array.py
@@ -2,9 +2,13 @@
 T: O(logN)
 S: O(logN)
 
-The solution a is two-step process. First, we find the smallest number in the
-list. After that, what's left is to do the binary search on the appropriate
-half of the list
+The one-pass solution to the problem. It can be done through observing the
+invariances. We start the usual binary search, and if we found the appropriate
+number we just return the index. Otherwise, there are two cases to consider:
+1) nums[lo] <= nums[mid], part to the left of the mid is sorted, and if the
+target can be in the sorted part, we go there, otherwise, we go right
+2) otherwise the right part is sorted, and if the target is possibly in it,
+we go right, otherwise, we go left
 """
 
 from typing import List
@@ -13,33 +17,19 @@ from typing import List
 class Solution:
 
     def search(self, nums: List[int], target: int) -> int:
-        if not nums:
-            return -1
-        min_number_index = self.get_min_index(nums)
-        if nums[min_number_index] <= target <= nums[-1]:
-            return self.binary_search(nums, target, min_number_index,
-                                      len(nums) - 1)
-        else:
-            return self.binary_search(nums, target, 0, min_number_index - 1)
-
-    def get_min_index(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
-            if nums[0] <= nums[mid]:
-                left = mid + 1
-            else:
-                right = mid
-        return left
-
-    def binary_search(self, nums: List[int], target: int, left: int,
-                      right: int) -> int:
-        while left <= right:
-            mid = (left + right) // 2
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                right = mid - 1
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
             else:
-                left = mid + 1
+                if nums[mid] < target <= nums[-1]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
         return -1
