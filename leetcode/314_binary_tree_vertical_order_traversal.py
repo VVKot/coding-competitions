3a7ac1823a83503f8b871a48267f676249491af9
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-15 11:27:16 +0200

    feat: make solution linear in time

--- leetcode/314_binary_tree_vertical_order_traversal.py
+++ leetcode/314_binary_tree_vertical_order_traversal.py
@@ -1,11 +1,11 @@
 """
-T: O(NlogN)
+T: O(N)
 S: O(N)
 
 The gist of the solution is to determine a correct index for each node.
 Since we are doing BFS, also known as level traversal, we can simply put
 the node to the appropriate bucket with the nodes of the same vertical index.
-In the end, we have to sort the dictionary to achieve a correct result.
+In the end, we use recorded minimum and maximum indices to get a proper order.
 """
 
 import collections
@@ -27,12 +27,17 @@ class Solution:
             return []
         column_order = collections.defaultdict(
             list)  # type: Dict[int, List[int]]
-        nodes_queue = collections.deque([(root, 0)])
+        curr_index = min_index = max_index = 0
+        nodes_queue = collections.deque([(root, curr_index)])
         while nodes_queue:
             curr, index = nodes_queue.popleft()
             if not curr:
                 continue
+            min_index = min(min_index, index)
+            max_index = max(max_index, index)
             column_order[index].append(curr.val)
             nodes_queue.append((curr.left, index - 1))
             nodes_queue.append((curr.right, index + 1))
-        return [column_order[index] for index in sorted(column_order.keys())]
+        return [
+            column_order[index] for index in range(min_index, max_index + 1)
+        ]
