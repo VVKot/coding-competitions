5cfbe73257c403c432282c6e20fac3f11e032f02
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-16 05:34:42 +0200

    docs: add explanation and complexities

--- leetcode/496_next_greater_element_i.py
+++ leetcode/496_next_greater_element_i.py
@@ -1,17 +1,26 @@
-from typing import List
+"""
+T: O(N)
+S: O(N)
+
+We use a descending stack to remember the numbers that we have seen that do not
+have the next greater element so far. Once we see an element that is greater
+than some of the stack, we pop those elements and add a current element to the
+stack. In this way, we maintain the invariant of only descending elements
+in the stack.
+"""
+import collections
+from typing import Dict, List
 
 
 class Solution:
 
-    def nextGreaterElement(self,
-                           nums1: List[int],
+    def nextGreaterElement(self, nums1: List[int],
                            nums2: List[int]) -> List[int]:
-        greater = {num: -1 for num in nums1}
+        next_greater = collections.defaultdict(
+            lambda: -1)  # type: Dict[int, int]
         stack = []  # type: List[int]
         for num in nums2:
             while stack and stack[-1] < num:
-                smaller = stack.pop()
-                if smaller in greater:
-                    greater[smaller] = num
+                next_greater[stack.pop()] = num
             stack.append(num)
-        return [greater[num] for num in nums1]
+        return [next_greater[num] for num in nums1]
