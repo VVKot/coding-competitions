59050b9aa31f08aa1d1cfaa7152e577a783fa132
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-20 05:27:18 +0200

    docs: add explanation

--- leetcode/80_remove_duplicates_from_sorted_array_ii.py
+++ leetcode/80_remove_duplicates_from_sorted_array_ii.py
@@ -1,17 +1,27 @@
+"""
+T: O(N)
+S: O(1)
+
+We can overwrite the array using constant space by remembering the last insert
+position. After that, the only thing that we should check is the number of the
+same elements that we have seen so far.
+"""
+
 from typing import List
 
 
 class Solution:
 
     def removeDuplicates(self, nums: List[int]) -> int:
-        prev = float('inf')
-        write = same_count = 0
+        insert_idx = 0
+        prev_num = 0
+        prev_count = 0
         for num in nums:
-            if num == prev:
-                same_count += 1
-            else:
-                prev = num
-                same_count = 1
-            nums[write] = num
-            write += 1 if same_count < 3 else 0
-        return write
+            if num != prev_num:
+                prev_num = num
+                prev_count = 0
+            prev_count += 1
+            if prev_count <= 2:
+                nums[insert_idx] = num
+                insert_idx += 1
+        return insert_idx
