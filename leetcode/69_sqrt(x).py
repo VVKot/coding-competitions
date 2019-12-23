a8e7a2bb512acb6e1c10d07aa2afadee3c319ab7
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-23 06:01:20 +0200

    feat: add binary search solution

--- leetcode/69_sqrt(x).py
+++ leetcode/69_sqrt(x).py
@@ -1,9 +1,22 @@
+"""
+T: O(logN)
+S: O(1)
+
+Binary search the correct number. Since we are rounding the result down, we
+should only skip the current number if it is too high.
+"""
+
+
 class Solution:
 
-    def mySqrt(self, x):
-        if not x:
-            return x
-        r = x
-        while r*r > x:
-            r = (r + x//r) // 2
-        return int(r)
+    def mySqrt(self, x: int) -> int:
+        lo, hi = 0, x
+        sqrt = 0
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if mid**2 > x:
+                hi = mid - 1
+            else:
+                sqrt = mid
+                lo = mid + 1
+        return sqrt
