09be3f3598adeef900bfd55b8d09a059f5ce0067
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-25 06:04:35 +0200

    docs: add explanation

--- leetcode/575_distributing_candies.py
+++ leetcode/575_distributing_candies.py
@@ -1,7 +1,18 @@
+"""
+T: O(N)
+S: O(N)
+
+The result is bounded by two variables. Firstly, the kid can not get more than
+half of all candy. Secondly, they can not get more kinds of candies that are
+present. The minimum of those constraints is the answer.
+"""
+
 from typing import List
 
 
 class Solution:
+
     def distributeCandies(self, candies: List[int]) -> int:
-        half, diff = len(candies) // 2, len(set(candies))
-        return min(half, diff)
+        candies_per_kid = len(candies) // 2
+        candies_kind_count = len(set(candies))
+        return min(candies_per_kid, candies_kind_count)
