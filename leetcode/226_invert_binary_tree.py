6db498f9a336199c9668ed8a86dc3fe1b8eea54d
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-25 05:46:20 +0200

    docs: add description
    Add complexitites and also description. Also improve variable names

--- leetcode/226_invert_binary_tree.py
+++ leetcode/226_invert_binary_tree.py
@@ -1,3 +1,11 @@
+"""
+T: O(N)
+S: O(N)
+
+Just do as the description says.
+"""
+
+
 class TreeNode:
 
     def __init__(self, x):
@@ -9,10 +17,11 @@ class TreeNode:
 class Solution:
 
     def invertTree(self, root: TreeNode) -> TreeNode:
-        stack = [root]
-        while stack:
-            curr = stack.pop()
+        nodes_to_process = [root]
+        while nodes_to_process:
+            curr = nodes_to_process.pop()
             if curr:
                 curr.left, curr.right = curr.right, curr.left
-                stack += curr.left, curr.right
+                nodes_to_process.append(curr.left)
+                nodes_to_process.append(curr.right)
         return root
