5d776f802003e4a86cc7e3d7d400dbf286c5b65d
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-26 06:03:57 +0200

    docs: add explanation

--- leetcode/153_find_minimum_in_rotated_sorted_array.py
+++ leetcode/153_find_minimum_in_rotated_sorted_array.py
@@ -1,15 +1,27 @@
+"""
+T: O(logN)
+S: O(1)
+
+A classic binary search problem. Only conditions are changed a bit.
+1) If the current element is greater than the right boundary, we should go to
+the right. That is because when this condition holds, the pivot element is in
+between the current element and the right boundary.
+2) Otherwise, we can conclude that the part of the array between the current
+and the right is sorted, so we want to go to the left. We should be careful to
+not exclude mid in this case, since it is might be the answer.
+"""
+
 from typing import List
 
 
 class Solution:
 
     def findMin(self, nums: List[int]) -> int:
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
         while left != right:
-            mid = (left + right) // 2
-            if nums[right] < nums[mid]:
-                left = mid+1
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
             else:
                 right = mid
-        return nums[left]
+        return nums[right]
