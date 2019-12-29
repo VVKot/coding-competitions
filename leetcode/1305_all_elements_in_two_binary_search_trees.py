2d842e9005a02a51398b06ccade631bad2356e52
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-29 14:22:23 +0100

    feat: add linear time solution

---
+++ leetcode/1305_all_elements_in_two_binary_search_trees.py
@@ -0,0 +1,43 @@
+"""
+T: O(N+M)
+S: O(N+M) - additional
+
+Do two inorder traversals in parallel. On each iteration select stack with the
+minimum last element.
+"""
+
+from typign import List
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        all_elements = []
+        stack1, stack2 = [], []  # type: List[int], List[int]
+        while any([root1, root2, stack1, stack2]):
+            stack1, root1 = self.push_left(stack1, root1)
+            stack2, root2 = self.push_left(stack2, root2)
+            smallest_stack = self.get_smallest_stack(stack1, stack2)
+            curr = smallest_stack.pop()
+            all_elements.append(curr.val)
+            if smallest_stack is stack1:
+                root1 = curr.right
+            else:
+                root2 = curr.right
+        return all_elements
+    
+    def push_left(self, stack: List[int], root: int) -> List[int], int:
+        while root:
+            stack.append(root)
+            root = root.left
+        return stack, root
+
+    def get_smallest_stack(self, stack1: List[int], stack2: List[int]) -> List[int]:
+        if stack1 and stack2:
+            return stack1 if stack1[-1].val <= stack2[-1].val else stack2
+        return stack1 or stack2
