7882425b3fc6e8b96685a5bdb244e48de24a0835
VVKot <volodymyr.kot.ua@gmail.com>
2019-12-30 06:29:59 +0100

    refactor: upsolve

--- leetcode/69_sqrt(x).py
+++ leetcode/69_sqrt(x).py
@@ -14,9 +14,9 @@ class Solution:
         sqrt = 0
         while lo <= hi:
             mid = lo + (hi - lo) // 2
-            if mid**2 > x:
-                hi = mid - 1
-            else:
+            if mid**2 <= x:
                 sqrt = mid
                 lo = mid + 1
+            else:
+                hi = mid - 1
         return sqrt
