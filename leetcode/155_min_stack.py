bcdb8c0d899eb68da7d322512b2ca3d52baea6a5
VVKot <volodymyr.kot.ua@gmail.com>
2020-01-10 06:17:20 +0100

    refactor: clarify variable names

--- leetcode/155_min_stack.py
+++ leetcode/155_min_stack.py
@@ -13,20 +13,21 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min_num = float('inf')
+        self.min = float('inf')
 
     def push(self, x: int) -> None:
-        if x <= self.min_num:
-            self.stack.append(self.min_num)
-            self.min_num = x
+        if x <= self.min:
+            self.stack.append(self.min)
+            self.min = x
         self.stack.append(x)
 
     def pop(self) -> None:
-        if self.stack.pop() == self.min_num:
-            self.min_num = self.stack.pop()
+        top = self.stack.pop()
+        if top == self.min:
+            self.min = self.stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.min_num
+        return self.min
