cac9b0cad4f5145f10a2c60074b8a33ee71babd1
VVKot <volodymyr.kot.ua@gmail.com>
2020-01-15 07:22:23 +0000

    feat: improve time complexity

--- leetcode/522_longest_uncommon_subsequence_ii.py
+++ leetcode/522_longest_uncommon_subsequence_ii.py
@@ -1,12 +1,11 @@
 """
-T: O(N**2 * 2**K)
-S: O(K)
+T: O(N**2 * K)
+S: O(1)
 
 Brute-force solution which computes every possible subsequence starting from
 the largest and compares it to the every word.
 """
 
-import itertools
 from typing import List
 
 
@@ -14,30 +13,24 @@ class Solution:
 
     def findLUSlength(self, strs: List[str]) -> int:
         strs.sort(key=len, reverse=True)
-        max_len = -1
-        for i, word in enumerate(strs):
-            for subseq in self.get_subseqs(word):
-                if max_len >= len(subseq):
+        for i, subseq in enumerate(strs):
+            is_subseq_of_any = False
+            for j, word in enumerate(strs):
+                if i == j:
+                    continue
+                if len(word) < len(subseq):
                     break
-                for j, str_to_check in enumerate(strs):
-                    if i == j:
-                        continue
-                    if self.is_subseq(subseq, str_to_check):
-                        break
-                else:
-                    max_len = max(max_len, len(subseq))
-        return max_len
+                if self.is_subseq(subseq, word):
+                    is_subseq_of_any = True
+                    break
+            if not is_subseq_of_any:
+                return len(subseq)
+        return -1
 
-    def get_subseqs(self, word: str):
-        for seq_len in reversed(range(1, len(word) + 1)):
-            for seq in itertools.combinations(word, seq_len):
-                yield seq
-
-    def is_subseq(self, maybe_subseq: List[str], word: str) -> bool:
-        read_pointer = 0
-        for char in word:
-            if char == maybe_subseq[read_pointer]:
-                read_pointer += 1
-                if read_pointer == len(maybe_subseq):
-                    return True
-        return False
+    def is_subseq(self, subseq: str, word: str) -> bool:
+        i = j = 0
+        while i < len(subseq) and j < len(word):
+            if subseq[i] == word[j]:
+                i += 1
+            j += 1
+        return i == len(subseq)
