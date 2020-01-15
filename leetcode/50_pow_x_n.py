8974bae6a61fac9c47f7aa729ac63475a8397a06
VVKot <volodymyr.kot.ua@gmail.com>
2020-01-15 17:50:11 +0000

    docs: upsolve and document

--- leetcode/50_pow_x_n.py
+++ leetcode/50_pow_x_n.py
@@ -1,10 +1,19 @@
+"""
+T: O(logN)
+S: O(1)
+
+We do better than brute force by finding the powers of two in the required
+power and multiplying the result by appropriate numbers.
+"""
+
+
 class Solution:
 
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
         if n < 0:
-            x = 1/x
+            x = 1 / x
             n = -n + 1
         result = 1.0
         while n:
