4a530390da89d7dca1aa73d5c3cf911d9ed51886
VVKot <volodymyr.kot.ua@gmail.com>
2020-01-19 15:33:25 +0000

    refactor: make solution more concise

--- leetcode/286_walls_and_gates.py
+++ leetcode/286_walls_and_gates.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List, Tuple
 
 
@@ -6,32 +7,22 @@ class Solution:
     GATE, EMPTY = 0, 2147483647
 
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        rooms_to_process = self.find_rooms_near_gates(rooms)
-        curr_distance = 1
+        rooms_to_process = collections.deque(self.find_gates(rooms))
         while rooms_to_process:
-            next_rooms_to_process = []
-            for y, x in rooms_to_process:
-                node = rooms[y][x]
-                if node != self.EMPTY:
+            y, x = rooms_to_process.popleft()
+            for near_y, near_x in self.get_neighbors(rooms, y, x):
+                if rooms[near_y][near_x] != self.EMPTY:
                     continue
-                rooms[y][x] = curr_distance
-                neighbors = self.get_neighbors(rooms, y, x)
-                next_rooms_to_process.extend(neighbors)
-            curr_distance += 1
-            rooms_to_process = next_rooms_to_process
+                rooms_to_process.append((near_y, near_x))
+                rooms[near_y][near_x] = rooms[y][x] + 1
 
-    def find_rooms_near_gates(self,
-                              rooms: List[List[int]]) -> List[Tuple[int, int]]:
-        rooms_near_gates = []
+    def find_gates(self, rooms: List[List[int]]) -> List[Tuple[int, int]]:
+        gates = []
         for y, row in enumerate(rooms):
             for x, val in enumerate(row):
-                if val != self.EMPTY:
-                    continue
-                for near_y, near_x in self.get_neighbors(rooms, y, x):
-                    if rooms[near_y][near_x] == self.GATE:
-                        rooms_near_gates.append((y, x))
-                        break
-        return rooms_near_gates
+                if val == self.GATE:
+                    gates.append((y, x))
+        return gates
 
     def get_neighbors(self, rooms: List[List[int]], y: int,
                       x: int) -> List[Tuple[int, int]]:
