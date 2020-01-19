c45b6776f08fae1b229d377334bb3f55a1d386b4
VVKot <volodymyr.kot.ua@gmail.com>
2020-01-19 06:53:01 +0000

    feat: improve time complexity
    Add solution based on counting in- and out-degress. Add documentation

--- leetcode/997_find_the_town_judge.py
+++ leetcode/997_find_the_town_judge.py
@@ -1,17 +1,27 @@
-from collections import defaultdict
+"""
+T: O(N+T)
+S: O(N)
+
+We can represent this as a graph problem. The judge is the person that trusts
+nobody(i.e. out-degree of 0) and the person that is trusted by everybody(i.e.
+in-degree of N-1). So, we create a counter for each citizen and decrement it
+if the person trusts somebody, and increment if it is trusted by somebody. The
+person that ends up with a count of N-1 is a judge since the counter is simply
+in-degree minus out-degree.
+"""
+
+import collections
+from typing import Counter, List
 
 
 class Solution:
-    def findJudge(self, N, trust):
-        truct_chain = defaultdict(set)
-        for k, v in trust:
-            truct_chain[k].add(v)
-        for cand in range(1, N+1):
-            if not truct_chain[cand]:
-                for other in range(1, N+1):
-                    if other == cand:
-                        continue
-                    if not cand in truct_chain[other]:
-                        return -1
-                return cand
+
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        trust_count = collections.Counter()  # type: Counter[int]
+        for who, trusts_whom in trust:
+            trust_count[who] -= 1
+            trust_count[trusts_whom] += 1
+        for i in range(1, N + 1):
+            if trust_count[i] == N - 1:
+                return i
         return -1
