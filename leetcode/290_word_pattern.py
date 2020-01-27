91b9835d0b744a7d10430cc6ce6d4daf2a11417b
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2020-01-27 06:38:31 -0800

    docs: add complexities and explanation

--- leetcode/290_word_pattern.py
+++ leetcode/290_word_pattern.py
@@ -1,3 +1,15 @@
+"""
+T: O(P + S)
+S: O(P + S)
+
+This solution is based on the following observations:
+1. If number of words is different from the length of pattern
+2. Or if number of unique words is different from number of unique letters
+3. Or if number of unique mappings is different from number of unique letters
+the answer is false. Otherwise, it is true, no matter the exact mapping from
+letters to words.
+"""
+
 class Solution:
 
     def wordPattern(self, pattern: str, sequence: str) -> bool:
