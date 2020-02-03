64f8271c1816647da2c1eb3ae026baa8412e6739
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2020-02-03 06:48:37 -0500

    refactor: remove redundant check

--- leetcode/766_toeplitz_matrix.py
+++ leetcode/766_toeplitz_matrix.py
@@ -1,11 +1,13 @@
+from typing import List
+
+
 class Solution:
-    def isToeplitzMatrix(self, matrix):
-        if not any(matrix):
-            return False
+
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         for y, row in enumerate(matrix):
-            for x, val in enumerate(row):
+            for x, curr in enumerate(row):
                 if y != 0 and x != 0:
                     prev = matrix[y-1][x-1]
-                    if prev != val:
+                    if prev != curr:
                         return False
         return True
