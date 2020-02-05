9bbb4d083fd32ab503b290f0882c132d70fa6542
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2020-02-05 06:30:46 -0500

    docs: add explanation

--- leetcode/1221_split_a_string_in_balanced_strings.py
+++ leetcode/1221_split_a_string_in_balanced_strings.py
@@ -1,3 +1,13 @@
+"""
+T: O(N)
+S: O(1)
+
+We use the fact that initial string is balanced. If the previous found
+interval is balanced, then the next interval is either balanced or empty.
+It it wasn't so - the whole string would not be balanced, which contradicts
+the inital propmt
+"""
+
 class Solution:
 
     def balancedStringSplit(self, s: str) -> int:
