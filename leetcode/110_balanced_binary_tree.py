84cb2a4afd7aaf2888822c9abcaa4891bb602d70
Volodymyr Kot <volodymyr.kot.ua@gmail.com>
2020-02-07 06:36:04 -0500

    docs: add explanation

--- leetcode/110_balanced_binary_tree.py
+++ leetcode/110_balanced_binary_tree.py
@@ -1,3 +1,11 @@
+"""
+T: O(N)
+S: O(N)
+
+Simple recursive solution based on determining the depth of subtrees.
+If some subtree is inbalanced - mark its depth as such and propagate it.
+"""
+
 class TreeNode:
 
     def __init__(self, x):
@@ -8,14 +16,16 @@ class TreeNode:
 
 class Solution:
 
+    INBALANCED_MARK = -1
+
     def isBalanced(self, root: TreeNode) -> bool:
-        return self.get_depth(root) != -1
+        return self.get_depth(root) != self.INBALANCED_MARK
 
     def get_depth(self, root: TreeNode) -> int:
         if root is None:
             return 0
         left = self.get_depth(root.left)
         right = self.get_depth(root.right)
-        if left == -1 or right == -1 or abs(left - right) > 1:
-            return -1
+        if left == self.INBALANCED_MARK or right == self.INBALANCED_MARK or abs(left - right) > 1:
+            return self.INBALANCED_MARK
         return 1 + max(left, right)
