63a5b1d9e2c3b1324221169201e4ea43159dd670
VVKot <volodymyr.kot.ua@gmail.com>
2020-02-10 06:39:54 +0000

    docs: add explanation

--- leetcode/941_valid_mountain_array.py
+++ leetcode/941_valid_mountain_array.py
@@ -1,3 +1,15 @@
+"""
+T: O(N)
+S: O(1)
+
+For an array to be a mountain it has to satisfy these conditions:
+1. Has an ascent
+2. Has a descent from the end of the ascent
+3. Both ascent and descent only happen once
+First, we find a possible peak(end of the ascent), then we check if there is
+a descent starting on that peak and ending at the end of the array.
+"""
+
 from typing import List
 
 
@@ -6,11 +18,11 @@ class Solution:
     def validMountainArray(self, A: List[int]) -> bool:
         end_index = len(A) - 1
         peak = 0
-        while peak < end_index and A[peak] < A[peak+1]:
+        while peak < end_index and A[peak] < A[peak + 1]:
             peak += 1
         if peak == 0 or peak == end_index:
             return False
-        downhill = peak
-        while downhill < end_index and A[downhill] > A[downhill+1]:
-            downhill += 1
-        return downhill == end_index
+        descent = peak
+        while descent < end_index and A[descent] > A[descent + 1]:
+            descent += 1
+        return descent == end_index
