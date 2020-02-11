7b09969c1b2e94b6db2de3253f88b1d6bd2bfd18
VVKot <volodymyr.kot.ua@gmail.com>
2020-02-11 06:13:26 +0000

    docs: add explanation

--- leetcode/884_ uncommon_words_from_two_sentences.py
+++ leetcode/884_ uncommon_words_from_two_sentences.py
@@ -1,5 +1,18 @@
+"""
+T: O(A + B)
+S: O(A + B)
+
+The words we are looking for are only present once across both sequences. Count
+the words and find those that are unique.
+"""
+
+import collections
+from typing import List
+
+
 class Solution:
 
     def uncommonFromSentences(self, A: str, B: str) -> List[str]:
-        all_words = collections.Counter(A.split()) + collections.Counter(B.split())
+        all_words = collections.Counter(A.split()) + \
+            collections.Counter(B.split())
         return [word for word, count in all_words.items() if count == 1]
