1ab0390de055990cd75df42c953af13e6578e189
VVKot <volodymyr.kot.ua@gmail.com>
2020-02-12 06:00:39 +0000

    docs: add explanation

--- leetcode/804_unique_morse_code_words.py
+++ leetcode/804_unique_morse_code_words.py
@@ -1,16 +1,24 @@
+"""
+T: O(N)
+S: O(N)
+
+Just do as the description says, find unique representations and count them.
+"""
+
 from typing import List
 
 
 class Solution:
 
-    MORSE_WORDS = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
-                   "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
-                   "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
-                   "-.--", "--.."]
+    MORSE_WORDS = [
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+        "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+        "..-", "...-", ".--", "-..-", "-.--", "--.."
+    ]
 
     def get_morse_word(self, word: str) -> str:
         return "".join([self.MORSE_WORDS[ord(ch) - ord("a")] for ch in word])
 
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        words = {self.get_morse_word(word) for word in words}
-        return len(words)
+        unique_words = {self.get_morse_word(word) for word in words}
+        return len(unique_words)
