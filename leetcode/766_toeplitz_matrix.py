a504a99081d040c055bd0d811113b9d4f677029c
VVKot <volodymyr.kot.ua@gmail.com>
2020-02-14 06:14:38 +0000

    docs: add explanation

--- leetcode/766_toeplitz_matrix.py
+++ leetcode/766_toeplitz_matrix.py
@@ -1,3 +1,11 @@
+"""
+T: O(M * N)
+S: O(1)
+
+Simply check every value that has to hold the condition against the previous
+value. With a for loop, it is possible to achieve in constant space.
+"""
+
 from typing import List
 
 
@@ -7,7 +15,7 @@ class Solution:
         for y, row in enumerate(matrix):
             for x, curr in enumerate(row):
                 if y != 0 and x != 0:
-                    prev = matrix[y-1][x-1]
+                    prev = matrix[y - 1][x - 1]
                     if prev != curr:
                         return False
         return True
