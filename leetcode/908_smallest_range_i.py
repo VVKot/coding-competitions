f800efc22b78fc18bbc2eb01e76c85e3bb5c2407
VVKot <volodymyr.kot.ua@gmail.com>
2020-02-17 05:30:53 +0000

    docs: improve explanation

--- leetcode/908_smallest_range_i.py
+++ leetcode/908_smallest_range_i.py
@@ -2,12 +2,12 @@
 T: O(N)
 S: O(1)
 
-To find out the difference between extremes in the end,
-we first find that difference in the starting array. We
-also can find out by how much we csn shrink this difference -
-by 2K, since we can add numbers up to K magnitude to both extemes.
-After that, we have to check if the distance didn't become negative
-because in that case we can make it exactly zero by applying smaller numbers.
+To find out the difference between extremes in the end, we first find that
+difference in the starting array. We also can find out by how much we can
+shrink this difference - by 2K since we can add numbers up to K magnitude to
+both extremes. After that, we have to check if the distance didn't become
+negative because in that case, we can make it exactly zero by applying smaller
+numbers.
 """
 
 from typing import List
