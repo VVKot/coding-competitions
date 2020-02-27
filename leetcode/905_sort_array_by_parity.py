1a51bb4c21c5497aac180f6c20537279bcbc686e
VVKot <volodymyr.kot.ua@gmail.com>
2020-02-27 05:54:57 +0000

    docs: add explanation

--- leetcode/905_sort_array_by_parity.py
+++ leetcode/905_sort_array_by_parity.py
@@ -1,13 +1,21 @@
+"""
+T: O(N)
+S: O(1)
+
+Use two pointers, one to look through the array, and another one to remember
+the position of the last even element. Whenever we see an even element, push it
+to the start of the array after the last known even element.
+"""
+
 from typing import List
 
 
 class Solution:
 
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        i, j, N = 0, 0, len(A)
-        while i < N:
-            if not A[i] & 1:
-                A[i], A[j] = A[j], A[i]
-                j += 1
-            i += 1
+        read, write = 0, 0
+        for read in range(len(A)):
+            if not A[read] & 1:
+                A[read], A[write] = A[write], A[read]
+                write += 1
         return A
