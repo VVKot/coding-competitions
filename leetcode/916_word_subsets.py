14d1a3b6238c83299f43eeec3b7174f4adfb325b
VVKot <volodymyr.kot.ua@gmail.com>
2020-02-28 05:42:48 +0000

    docs: add explanation

--- leetcode/916_word_subsets.py
+++ leetcode/916_word_subsets.py
@@ -1,3 +1,11 @@
+"""
+T: O(N)
+S: O(1) - not counting output space
+
+Find the overall pattern - how many of each character do we have to have in a
+word for it to be universal. After that just check every word again pattern.
+"""
+
 import collections
 from typing import Counter, List
 
@@ -6,13 +14,12 @@ class Solution:
 
     def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
         pattern = self.get_pattern(B)
-        return [a for a in A if not pattern - collections.Counter(a)]
+        return [word for word in A if not pattern - collections.Counter(word)]
 
     def get_pattern(self, B: List[str]) -> Counter[str]:
         pattern = collections.Counter()  # type: Counter[str]
-        unique_patterns = set(B)
-        for p in unique_patterns:
-            curr = collections.Counter(p)
+        for word in B:
+            curr = collections.Counter(word)
             for letter, count in curr.items():
                 pattern[letter] = max(pattern[letter], count)
         return pattern
