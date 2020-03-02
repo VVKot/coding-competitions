0d3d8848a4b94c63d00e502cdade05288611eb68
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-02 05:52:00 +0000

    docs: add explanation

--- leetcode/1047_remove_all_adjacent_duplicates_in_string.py
+++ leetcode/1047_remove_all_adjacent_duplicates_in_string.py
@@ -1,3 +1,13 @@
+"""
+T: O(N)
+S: O(N)
+
+Process every character, check if it is the same as the previous one. If so -
+don't put it into the stack and pop the last character in the stack. This way,
+our algorithm will work even when we have to do multiple removals in one go,
+i.e. for abba, because bb will never be added to the stack.
+"""
+
 from typing import List
 
 
