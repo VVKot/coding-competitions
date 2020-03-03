8763f6bbdd286dacfe4d81377d2be2fc0b480270
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-03 06:08:26 +0000

    docs: add explanation and typings

--- leetcode/1035_uncrossed_lines.py
+++ leetcode/1035_uncrossed_lines.py
@@ -1,8 +1,21 @@
+"""
+T: O(N**2)
+S: O(N**2)
+
+The question is the same as LCS. Find the maximum result for current node from
+the calculation for the previous one. If current node in bottom row does not
+match the node in the top row - consider walking through both top and bottom
+row.
+"""
+
+from typing import List
+
+
 class Solution:
 
-    def maxUncrossedLines(self, A, B):
+    def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
         dp = {}
-        a, b = len(A)-1, len(B)-1
+        a, b = len(A) - 1, len(B) - 1
 
         def helper(i, j):
             result = 0
@@ -11,9 +24,10 @@ class Solution:
             if (i, j) in dp:
                 return dp[(i, j)]
             if A[i] == B[j]:
-                result = 1 + helper(i+1, j+1)
+                result = 1 + helper(i + 1, j + 1)
             else:
-                result = max(helper(i, j+1), helper(i+1, j))
-            dp[(i, j)]=result
+                result = max(helper(i, j + 1), helper(i + 1, j))
+            dp[(i, j)] = result
             return result
+
         return helper(0, 0)
