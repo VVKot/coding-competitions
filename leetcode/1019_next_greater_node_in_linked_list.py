43775f91c0c01cf710136a3ce8b4a525bef7bd3e
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-04 06:16:25 +0000

    docs: add explanation
    Also add typings and reformat

--- leetcode/1019_next_greater_node_in_linked_list.py
+++ leetcode/1019_next_greater_node_in_linked_list.py
@@ -1,16 +1,29 @@
-# Definition for singly-linked list.
+"""
+T: O(N)
+S: O(N)
+
+Remember seen nodes. If we see a node that is greater than some of the seen
+ones - update their next larger nodes with the value of the current node.
+"""
+
+from typing import List, Tuple
+
+
 class ListNode:
+
     def __init__(self, x):
         self.val = x
         self.next = None
 
+
 class Solution:
-	def nextLargerNodes(self, head):
-		result, stack = [], []
-		while head:
-			while stack and stack[-1][1] < head.val:
-				result[stack.pop()[0]] = head.val
-			stack.append((len(result), head.val))
-			result.append(0)
-			head = head.next
-		return result
+
+    def nextLargerNodes(self, head: ListNode) -> List[int]:
+        next_larger, stack = [], []  # type: List[int], List[Tuple[int, int]]
+        while head:
+            while stack and stack[-1][1] < head.val:
+                next_larger[stack.pop()[0]] = head.val
+            stack.append((len(next_larger), head.val))
+            next_larger.append(0)
+            head = head.next
+        return next_larger
