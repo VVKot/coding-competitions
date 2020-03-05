14f8dd13c5b87af0115d7619d6e64fe96ab88134
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-05 06:59:42 +0000

    docs: add explanation

--- leetcode/1022_sum_of_root_to_leaf_binary_numbers.py
+++ leetcode/1022_sum_of_root_to_leaf_binary_numbers.py
@@ -1,3 +1,13 @@
+"""
+T: O(N)
+S: O(N)
+
+Walk down the tree and calculate all created binary numbers. Calculating the
+next number is as simple as shifting previous left(multiplying by two) and
+adding additional digit to the end using OR.
+"""
+
+
 class TreeNode:
 
     def __init__(self, x):
@@ -15,8 +25,8 @@ class Solution:
 
     def process_all_paths(self, node: TreeNode, total: int) -> None:
         if node:
-            total *= 2
-            total += node.val
+            total <<= 1
+            total |= node.val
             if not node.right and not node.left:
                 self.result += total
             else:
