043798913d18b817e47e40c4ad2cd99620a4fd0c
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-06 08:14:01 +0000

    docs: add explanation

--- leetcode/986_interval_list_intersections.py
+++ leetcode/986_interval_list_intersections.py
@@ -1,9 +1,18 @@
+"""
+T: O(A + B)
+S: O(C), where C = min(A, B)
+
+Walk through both of the lists and check if two current intervals intersect.
+If so - add them to the result. Move the pointer in the list which interval
+ends earlier.
+"""
+
 from typing import List
 
 
 class Solution:
-    def intervalIntersection(self,
-                             A: List[List[int]],
+
+    def intervalIntersection(self, A: List[List[int]],
                              B: List[List[int]]) -> List[List[int]]:
         result = []  # type: List[List[int]]
         if not A or not B:
