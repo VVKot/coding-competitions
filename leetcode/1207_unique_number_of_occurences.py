f021b0db78311bc0a63f4e031c9358352cc25820
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-07 05:46:16 +0000

    docs: add explanation

--- leetcode/1207_unique_number_of_occurences.py
+++ leetcode/1207_unique_number_of_occurences.py
@@ -1,8 +1,19 @@
+"""
+T: O(N)
+S: O(N)
+
+Find the number of occurences of the each element, find unique number of
+occurences, compare the two. If they are the same - all occurences numbers
+are unique.
+"""
+
 import collections
 from typing import List
 
 
 class Solution:
+
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        c = collections.Counter(arr).values()
-        return len(c) == len(set(c))
+        num_counts = collections.Counter(arr)
+        occurences = num_counts.values()
+        return len(occurences) == len(set(occurences))
