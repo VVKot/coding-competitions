73f7d5413eb5414a99f403c3f2f32ab761afd66e
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-08 07:01:24 +0000

    docs: add explanation

--- leetcode/1215_stepping_numbers.py
+++ leetcode/1215_stepping_numbers.py
@@ -1,3 +1,13 @@
+"""
+T: O(2**N), where N is log10 H 
+S: O(2**N), where N is log10 H 
+
+Find all stepping numbers up to high, add them to the result if they are in
+the range. Theext stepping number can be either or both of:
+1. Current + last digit minus one, if the last digit is not a zero
+2. Current + last digit plus one, if the last digit is not a nine
+"""
+
 import collections
 from typing import List
 
@@ -5,18 +15,18 @@ from typing import List
 class Solution:
 
     def countSteppingNumbers(self, low: int, high: int) -> List[int]:
-        stepping_numbers = []
+        answers = []
         possible_answers = collections.deque(range(1, 10))
         if low == 0:
-            stepping_numbers.append(0)
+            answers.append(0)
         while possible_answers:
             num = possible_answers.popleft()
             if num < high:
                 last = num % 10
                 if last > 0:
-                    possible_answers.append(num*10 + last-1)
+                    possible_answers.append(num * 10 + last - 1)
                 if last < 9:
-                    possible_answers.append(num*10 + last+1)
+                    possible_answers.append(num * 10 + last + 1)
             if low <= num <= high:
-                stepping_numbers.append(num)
-        return stepping_numbers
+                answers.append(num)
+        return answers
