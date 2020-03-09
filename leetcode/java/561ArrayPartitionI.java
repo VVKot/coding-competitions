7b549c805e5d0673d5b45bf3fd016794c319e07c
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-09 05:54:14 +0000

    feat: add java solution

---
+++ leetcode/java/561ArrayPartitionI.java
@@ -0,0 +1,20 @@
+package leetcode.java;
+
+/**
+ * T: O(NlogN) S: O(1)
+ *
+ * <p>We can determine the best solution greedily. We have to group numbers in sorted order and take
+ * minimum of every pair which are at the odd positions.
+ */
+import java.util.Arrays;
+
+class Solution {
+  public int arrayPairSum(int[] nums) {
+    Arrays.sort(nums);
+    int result = 0;
+    for (int i = 0; i < nums.length; i += 2) {
+      result += nums[i];
+    }
+    return result;
+  }
+}
