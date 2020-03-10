958891e01cd87ffdc5dc263f7431fdc981ade672
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-10 20:29:47 +0000

    Add linear time solution

---
+++ leetcode/java/1TwoSum.java
@@ -0,0 +1,24 @@
+package leetcode.java;
+
+/**
+ * T: O(N) S: O(N)
+ *
+ * <p>Remember numbers we have seen so far alongside their indices. Return a sorted pair of indices
+ * when we see a number that is complement to the previously seen one.
+ */
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> seenNumbers = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+      if (seenNumbers.containsKey(complement)) {
+        return new int[] {seenNumbers.get(complement), i};
+      }
+      seenNumbers.put(nums[i], i);
+    }
+    return new int[] {-1, -1};
+  }
+}
