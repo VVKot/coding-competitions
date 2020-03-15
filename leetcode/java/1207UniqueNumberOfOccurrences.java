c53c51347d476f7a5b9e3e8c99acbc6ff0cc911a
VVKot <volodymyr.kot.ua@gmail.com>
2020-03-15 09:12:28 +0000

    feat: add linear time solution

---
+++ leetcode/java/1207UniqueNumberOfOccurrences.java
@@ -0,0 +1,23 @@
+package leetcode.java;
+
+/**
+ * T: O(N) S: O(N)
+ *
+ * <p>Count number of occurences of each elements. If all of those numbers are unique - return true,
+ * otherwise - return false.
+ */
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+
+class Solution {
+  public boolean uniqueOccurences(int[] arr) {
+    Map<int, int> numberCount = new HashMap<>();
+    for (int number : arr) {
+      numberCount.putIfAbsent(number, 0);
+      numberCount.merge(number, 1, Integer::sum);
+    }
+    Collection<int> allNumberCounts = numberCount.values();
+    return allNumberCounts.size() == new HashSet(allNumberCounts).size();
+  }
+}
