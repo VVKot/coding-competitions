79776884485555464efe5471059f8df9d74c87b3
VVKot <volodymyr.kot.ua@gmail.com>
2020-04-12 08:04:37 +0100

    chore: fix types

--- leetcode/java/1207UniqueNumberOfOccurrences.java
+++ leetcode/java/1207UniqueNumberOfOccurrences.java
@@ -4,18 +4,20 @@
  * <p>Count number of occurences of each elements. If all of those numbers are unique - return true,
  * otherwise - return false.
  */
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 class Solution {
-  public boolean uniqueOccurences(int[] arr) {
-    Map<int, int> numberCount = new HashMap<>();
-    for (int number : arr) {
-      numberCount.putIfAbsent(number, 0);
-      numberCount.merge(number, 1, Integer::sum);
-    }
-    Collection<int> allNumberCounts = numberCount.values();
-    return allNumberCounts.size() == new HashSet(allNumberCounts).size();
-  }
+	public boolean uniqueOccurrences(int[] arr) {
+		Map<Integer, Integer> numberCount = new HashMap<>();
+		for (int number: arr) {
+			numberCount.putIfAbsent(number, 0);
+			numberCount.merge(number, 1, Integer::sum);
+		}
+		List<Integer> allNumberCounts = new ArrayList<>(numberCount.values());
+		return allNumberCounts.size() == new HashSet<Integer>(allNumberCounts).size();
+	}
 }
