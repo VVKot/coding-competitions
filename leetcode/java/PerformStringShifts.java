6806c6784d47af763aa53d08763adc1eba0c7d01
VVKot <volodymyr.kot.ua@gmail.com>
2020-04-14 19:56:09 +0100

    docs: add explanation

--- leetcode/java/PerformStringShifts.java
+++ leetcode/java/PerformStringShifts.java
@@ -1,3 +1,10 @@
+/**
+ * T: O(S + SH) S: O(S)
+ *
+ * <p>Iterate over the whole string, find overall shift. Use modulo to not perform shift that is
+ * larger than length of the array. Space complexity is still linear due to the need of creation of
+ * a new string.
+ */
 class Solution {
   public String stringShift(String s, int[][] shift) {
     int totalShift = 0;
