6ae3d92f20728d961aaec7be7b0ced218a718bfc
VVKot <volodymyr.kot.ua@gmail.com>
2020-06-02 21:13:31 +0100

    feat: add solution

---
+++ leetcode/java/237DeleteNodeInALinkedList.java
@@ -0,0 +1,12 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+      node.val = node.next.val;
+      node.next = node.next.next;
+    }
+}
