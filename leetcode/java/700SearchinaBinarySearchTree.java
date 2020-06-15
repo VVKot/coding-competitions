ac5cda8fe2947b8a442960c8fb52650e1d24fba8
VVKot <volodymyr.kot.ua@gmail.com>
2020-06-15 18:59:18 +0100

    feat: add linear time solution

---
+++ leetcode/java/700SearchinaBinarySearchTree.java
@@ -0,0 +1,33 @@
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public TreeNode searchBST(TreeNode root, int val) {
+    while (root != null) {
+      if (root.val == val) {
+        return root;
+      }
+      if (root.val > val) {
+        root = root.left;
+      } else {
+        root = root.right;
+      }
+    }
+    return null;
+  }
+}
